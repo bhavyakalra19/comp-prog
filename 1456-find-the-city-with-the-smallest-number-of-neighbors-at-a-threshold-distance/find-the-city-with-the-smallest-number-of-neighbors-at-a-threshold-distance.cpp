ea071a1e2938836b98cb2502b9a3c4a5b3102e6a
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-07-26 23:37:32 +0530

    Time: 12 ms (99.83%) | Memory: 15.4 MB (79.53%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,43 @@
+#define INF 10001
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n,vector<int>(n,INF));
+        int ans = 0;
+        int mn = INT_MAX;
+        for(int i = 0; i < edges.size(); i++){
+            dist[edges[i][0]][edges[i][1]] = edges[i][2];
+            dist[edges[i][1]][edges[i][0]] = edges[i][2];
+        }
+        // for(int i = 0; i < n; i++){
+        //     dist[i][i] = 0;
+        // }
+         for (int k = 0; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            int check = 0;
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    continue;
+                }
+                if(min(dist[i][j],dist[j][i]) <= distanceThreshold){
+                    check += 1;
+                }
+            }
+            if(check <= mn){
+                mn = check;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
