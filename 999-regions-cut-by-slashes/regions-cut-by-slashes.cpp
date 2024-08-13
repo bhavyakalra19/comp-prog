09b2b744f06c9cf7f165bfda3e76967ed21dc13d
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-08-14 01:53:50 +0530

    Time: 7 ms (69.29%) | Memory: 13.7 MB (49.01%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.cpp
@@ -1,71 +1,41 @@
 class Solution {
 public:
-    static void dfs(int v, vector<vector<int>>& adj, bitset<962>& viz) {
-        viz[v] = 1;
-        for (int w : adj[v]) {
-            if (viz[w]) continue;
-            dfs(w, adj, viz);
+    void dfs(vector<vector<int>> &check,int i, int j, int n){
+        if(i == n || i < 0 || j == n || j < 0 || check[i][j] == -1){
+            return;
         }
+        check[i][j] = -1;
+        dfs(check,i+1,j,n);
+        dfs(check,i,j+1,n);
+        dfs(check,i-1,j,n);
+        dfs(check,i,j-1,n);
     }
-    static int regionsBySlashes(vector<string>& grid) {
-        const int n = grid.size();
-        const int side = n+1; // side length of the extended grid
-        const int N = side*side; // total vertices in the extended grid
 
-        bitset<962> viz = 0; // Supports up to a 31x31 grid
-        vector<vector<int>> adj(N);
-
-        int N0 = N-side;
-        for (int i = 0; i < n; i++) {
-            adj[i].push_back(i+1);             // Top border
-            adj[i+1].push_back(i);
-
-            adj[i+N0].push_back(i+N0+1);   // Bottom border
-            adj[i+N0+1].push_back(i+N0);
-
-            adj[i*side].push_back((i+1)*side);    // Left border
-            adj[(i+1)*side].push_back(i*side);
-
-            adj[n+i*side].push_back(n+(i+1)*side); // Right border
-            adj[n+(i+1)*side].push_back(n+i*side);
-        }
-
-        int E = 4*n; // Initial edges from the borders
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '\\') {
-                    int u = i*side+j;
-                    int v = (i+1)*side+j+1;
-                    adj[u].push_back(v);
-                    adj[v].push_back(u);
-                    E++;
-                } 
-                else if (grid[i][j] == '/') {
-                    int u = i*side+j+1;
-                    int v = (i+1)*side+j;
-                    adj[u].push_back(v);
-                    adj[v].push_back(u);
-                    E++;
+    int regionsBySlashes(vector<string>& grid) {
+        int n = grid.size();
+        int ans = 0;
+        vector<vector<int>> check(n*3,vector<int>(3*n,0));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '/'){
+                    check[3*i][3*j + 2] = -1;
+                    check[3*i + 1][3*j + 1] = -1;
+                    check[3*i + 2][3*j] = -1;
+                }else if(grid[i][j] == '\\'){
+                    check[3*i][3*j] = -1;
+                    check[3*i + 1][3*j + 1] = -1;
+                    check[3*i + 2][3*j + 2] = -1;
                 }
             }
         }
-
-        int component = 0;
-        for (int i=0; i < N; i++)
-            // don't count isolated 1-vertex components
-            if (!adj[i].empty() && !viz[i]) {
-            //    cout << "\nStarting DFS for component at vertex: "<<i<<endl;
-                component++;
-                dfs(i, adj, viz);
+        for(int i = 0; i < 3*n; i++){
+            for(int j = 0; j < 3*n; j++){
+                if(check[i][j] == 0){
+                    ans += 1;
+                    dfs(check,i,j,3*n);
+                }
             }
-        
-        int V = viz.count();
-        int F = E-V+1+component;
-        
-        // output
-       // cout<<"V="<<V<<", E="<<E<<", component="<<component<<", F="<<F<< endl;
-        //print(grid);
-        return F - 1; // Final face count
+        }
+        return ans;
     }
 };
