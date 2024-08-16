3672357a1a9f96e7552ac2b93502c2283bfd6c16
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-08-16 14:13:08 +0530

    Time: 6 ms (78.46%) | Memory: 14.3 MB (21.48%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int smallestDistancePair(std::vector<int>& numbers, int k) {
-        std::sort(numbers.begin(), numbers.end());
-        int minDistance = 0, maxDistance = numbers.back() - numbers.front();
-        
-        while (minDistance < maxDistance) {
-            int midDistance = minDistance + (maxDistance - minDistance) / 2;
-            if (countPairsWithinDistance(numbers, midDistance) < k) {
-                minDistance = midDistance + 1;
-            } else {
-                maxDistance = midDistance;
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int mn = 0;
+        int mx = nums.back() - nums.front();
+        while(mn < mx){
+            int md = mn + (mx - mn)/2;
+            if(countCheck(nums, md) < k){
+                mn = md + 1;
+            }else{
+                mx = md;
             }
         }
-        
-        return minDistance;
+        return mn;
     }
 
-private:
-    int countPairsWithinDistance(const std::vector<int>& numbers, int targetDistance) {
-        int count = 0, left = 0;
-        for (int right = 1; right < numbers.size(); ++right) {
-            while (numbers[right] - numbers[left] > targetDistance) {
+    int countCheck(vector<int> nums, int md){
+        int count = 0;
+        int left = 0;
+        for(int right = 1; right < nums.size(); right++){
+            while(nums[right] - nums[left] > md){
                 ++left;
             }
             count += right - left;
