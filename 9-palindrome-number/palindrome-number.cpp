a8ae23d66369020613c49716ab6eb311bfc48ce2
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-09-03 11:35:05 +0530

    Time: 3 ms (93.41%) | Memory: 8.6 MB (37.03%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        vector<int> check;
         if(x < 0){
             return false;
         }
+        int check = x;
+        long long num = 0;
         while(x > 0){
-            check.push_back(x%10);
+            num *= 10;
+            num += (x%10);
             x /= 10;
         }
-        int n = check.size();
-        for(int i = 0; i < n/2; i++){
-            if(check[i] != check[n-i-1]){
-                return false;
-            }
-        }
-        return true;
+        
+        return num == check;
     }
 };
