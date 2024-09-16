687eda037d3cf339f789e3d9f85ff6a500a34926
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-09-16 08:57:39 +0530

    Time: 236 ms (33.33%) | Memory: 181.8 MB (33.33%) - LeetSync

--- 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
@@ -1,34 +1,16 @@
 class Solution {
-    #define ll long long
-
-public:
-    // int n;
-    // vector<vector<long long>> dp;
-    // long long getAns(vector<int> a, vector<int> b, int i, int j){
-    //     if(i == 4){
-    //         return 0;
-    //     }
-    //     if(dp[i][j] != INT_MIN){
-    //         return dp[i][j];
-    //     }
-    //     if((3 - i) == (n - j - 1)){
-    //         return dp[i][j] = (long long)a[i] * (long long)b[j]  + getAns(a,b,i+1,j+1);
-    //     }
-    //     return dp[i][j] = max((long long)a[i] * (long long)b[j] + getAns(a,b,i+1,j+1),getAns(a,b,i,j+1));
-    // }
-
-    long long maxScore(vector<int>& a, vector<int>& b) {
-        int n = b.size();
-        vector<vector<ll>> dp(n+1, vector<ll> (5, LONG_MIN));
-        for(int aidx = 0; aidx <= 3; aidx++) dp[n][aidx] = -1e18;
-        for(int bidx = 0; bidx <= n; bidx++) dp[bidx][4] = 0;
-        for(int bidx = n-1; bidx >= 0; bidx--){
-            for(int aidx = 3; aidx >= 0; aidx--){
-                ll take = (ll)a[aidx]*b[bidx] + dp[bidx+1][aidx+1];
-                ll notTake = dp[bidx+1][aidx];
-                dp[bidx][aidx] = max(take, notTake);
-            }
+    public:
+        vector<vector<long long>> dp;
+        long long dfs(int i, int j, vector<int>& a, vector<int>& b) {
+            if (i >= a.size() || j >= b.size())
+                return i == a.size() ? 0 : INT_MIN;
+            if (dp[i][j] == INT_MIN)
+                dp[i][j] = max(dfs(i, j + 1, a, b), 
+                            (long long)a[i] * b[j] + dfs(i + 1, j + 1, a, b));
+            return dp[i][j];
+        } 
+        long long maxScore(vector<int>& a, vector<int>& b) {
+            dp = vector<vector<long long>>(4, vector<long long>(b.size(), INT_MIN));
+            return dfs(0, 0, a, b);
         }
-        return dp[0][0];
-    }
 };
