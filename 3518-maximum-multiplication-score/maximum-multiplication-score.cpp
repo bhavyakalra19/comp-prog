ae4cb5eec9f61380869392de0f426bbdc298797e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-09-16 11:15:49 +0530

    Time: 171 ms (33.33%) | Memory: 179.4 MB (33.33%) - LeetSync

--- 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
+++ 3518-maximum-multiplication-score/maximum-multiplication-score.cpp
@@ -10,7 +10,23 @@ class Solution {
             return dp[i][j];
         } 
         long long maxScore(vector<int>& a, vector<int>& b) {
-            dp = vector<vector<long long>>(4, vector<long long>(b.size(), INT_MIN));
-            return dfs(0, 0, a, b);
+            // dp = vector<vector<long long>>(5, vector<long long>(b.size() + 1, INT_MIN));
+            // return dfs(0, 0, a, b);
+            int n = b.size();
+            dp = vector<vector<long long>>(5,vector<long long>(n+1,LONG_MIN));
+            for(int i = 0; i < 5; i++){
+                dp[i][0] = -1e18;
+            }
+            for(int i = 0; i <= n; i++){
+                dp[0][i] = 0;
+            }
+            for(int i = 1; i < 5; i++){
+                for(int j = 1; j <= n; j++){
+                    long long take = (long long)(a[i-1]) * (long long)(b[j-1]) + dp[i-1][j-1];
+                    long long notTake = dp[i][j-1];
+                    dp[i][j] = max(take, notTake);
+                }
+            }
+            return dp[4][n];
         }
 };
