60fc3d418106a6c02c97d54d41b33edb1850d3ac
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-09-23 14:31:38 +0530

    Time: 24 ms (90.91%) | Memory: 13.8 MB (63.64%) - LeetSync

--- 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     long long validSubstringCount(string word1, string word2) {
         vector<int> check(26,0);
-        for(int i = 0; i < word2.size(); i++){
+        int k = word2.size();
+        for(int i = 0; i < k; i++){
             check[word2[i] - 'a'] += 1;
         }
-        int n = word1.size();
+        int start = 0;
         long long ans = 0;
         vector<int> slide(26,0);
-        int start = 0;
-        int k = word2.size();
+        int n = word1.size();
         for(int i = 0; i < n; i++){
-            char curr = word1[i];
-            if(check[curr - 'a'] > 0){
-                if(slide[curr - 'a'] < check[curr - 'a']){
+            int a = word1[i] - 'a';
+            if(check[a] > 0){
+                if(slide[a] < check[a]){
                     k--;
                 }
-            } 
-            slide[curr - 'a']++;
+            }
+            slide[a]++;
             while(k == 0){
                 ans += n - i;
-                char pre = word1[start];
-                slide[pre - 'a']--;
-                if(check[pre - 'a'] > 0 && slide[pre - 'a'] < check[pre - 'a']){
-                    k++;
+                int c = word1[start] - 'a';
+                slide[c]--;
+                if(check[c] > 0){
+                    if(slide[c] < check[c]){
+                        k++;
+                    }
                 }
                 start++;
             }
