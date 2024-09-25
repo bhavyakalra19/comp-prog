a3bc8c36d8146742024743c100434a172eeb35e8
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-09-25 17:34:57 +0530

    Time: 7 ms (32.37%) | Memory: 9.8 MB (31.23%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -12,15 +12,13 @@ public:
         }
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(p[j] == '.'){
+                if(p[j] == '.' || s[i] == p[j]){
                     dp[i+1][j+1] = dp[i][j];
                 }else if(p[j] == '*'){
                     dp[i+1][j+1] = dp[i+1][j-1];
                     if(s[i] == p[j-1] || p[j-1] == '.'){
                         dp[i+1][j+1] = dp[i+1][j+1] || dp[i][j+1];
                     }
-                }else if(s[i] == p[j]){
-                    dp[i+1][j+1] = dp[i][j];
                 }
             }
         }
