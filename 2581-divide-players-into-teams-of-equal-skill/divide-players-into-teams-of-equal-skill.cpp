ab0ffe590955e01206418836ccd98bfd92801eb1
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-10-04 12:46:09 +0530

    Time: 49 ms (97.40%) | Memory: 56.1 MB (97.00%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -2,18 +2,32 @@ class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
         int n = skill.size();
-        if(n == 2){
-            return skill[0]*skill[1];
+        long long sum = 0;
+        int freq[1001] = {0};
+        int mn = 1000;
+        int mx = 1;
+        for(auto x : skill){
+            sum += x;
+            freq[x] += 1;
+            mn = min(mn, x);
+            mx = max(mx, x);
         }
+        if(sum %(n/2) != 0){
+            return -1;
+        }
+        int check = (mx + mn);
         long long ans = 0;
-        sort(skill.begin(), skill.end());
-        int check = skill[0] + skill[n-1];
-        for(int i = 0; i < n/2; i++){
-            if(skill[i] + skill[n-i-1] != check){
+        long i;
+        long j;
+        for(i = mn, j = mx; i < j; i++, j--){
+            long fi = freq[i];
+            long fj = freq[j];
+            if(i+j != check || fi != fj){
                 return -1;
             }
-            ans += skill[i] * skill[n-i-1];
+            ans += (i * j * fi);
         }
+        ans += (i==j && i*2 == check) ? freq[i] * i * i / 2LL : 0LL;
         return ans;
     }
 };
