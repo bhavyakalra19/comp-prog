631a5723c14d70bd5314c9ccc4b9808abf7a8817
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-10-21 23:00:54 +0530

    Time: 1803 ms (5.26%) | Memory: 516.5 MB (5.26%) - LeetSync

---
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int ans;
+    int n;
+    void getAns(string s, int idx, unordered_map<string,bool> mp, int num){
+        if(idx >= n){
+            ans = max(ans,num);
+            return;
+        }
+        string curr = "";
+        for(int i = idx; i < n; i++){
+            curr += s[i];
+            if(mp.find(curr) == mp.end() || mp[curr] == false){
+                mp[curr] = true;
+                getAns(s,i+1,mp,num+1);
+                mp[curr] = false;
+            }
+        }
+    }
+
+    int maxUniqueSplit(string s) {
+        ans = 0;
+        n = s.size();
+        string curr = "";
+        for(int i = 0; i < n; i++){
+            unordered_map<string,bool> mp;
+            string sub = s.substr(0,i+1);
+            mp[sub] = true;
+            getAns(s,i+1,mp,1);
+        }
+        return ans;
+    }
+};
