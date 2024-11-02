6d7c9154fa60e44f400f52baea95f8d0fa76f226
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-11-02 13:05:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (96.30%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        char start = sentence[0];
+        char curr;
+        int n = sentence.size();
+        int i = 0;
+        while(i < n){
+            while(i < n && sentence[i] != ' '){
+                curr = sentence[i];
+                i++;
+            }
+            i++;
+            if(i >= n){
+                break;
+            }else{
+                if(curr != sentence[i]){
+                    return false;
+                }
+            }
+        }
+        return start == curr;
+    }
+};
