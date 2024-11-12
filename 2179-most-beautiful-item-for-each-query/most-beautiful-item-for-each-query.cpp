2cd1d30b50b4012a16f9db08fa97e94a17fca219
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-11-12 13:47:51 +0530

    Time: 857 ms (5.74%) | Memory: 96.7 MB (48.31%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -3,11 +3,15 @@ public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         int n = items.size();
         sort(items.begin(), items.end());
+        vector<pair<int,int>> check;
+
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         int m = queries.size();
         for(int i = 0; i < m; i++){
+            check.push_back({queries[i],i});
             pq.push({queries[i],i});
         }
+        sort(check.begin(),check.end());
         vector<int> ans(m,0);
         int mx = 0;
         for(int i = 0; i < n; i++){
@@ -16,14 +20,22 @@ public:
             items[i][1] = mx;
         }
         int j = -1;
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
-            while(j + 1 < n && it.first >= items[j+1][0]){
+        // while(!pq.empty()){
+        //     auto it = pq.top();
+        //     pq.pop();
+        //     while(j + 1 < n && it.first >= items[j+1][0]){
+        //         j++;
+        //     }
+        //     if(j != -1){
+        //         ans[it.second] = items[j][1];
+        //     }
+        // }
+        for(int i = 0; i < m; i++ ){
+            while(j + 1 < n && check[i].first >= items[j+1][0]){
                 j++;
             }
             if(j != -1){
-                ans[it.second] = items[j][1];
+                ans[check[i].second] = items[j][1];
             }
         }
         return ans;
