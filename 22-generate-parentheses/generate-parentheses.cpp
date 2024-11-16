b48cbbe2debd9e03480bcd54a0e9b70b610555fd
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-11-16 13:47:02 +0530

    Time: 3 ms (70.08%) | Memory: 15.3 MB (60.16%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<string> ans;
-
-    void getAns(string s, int p, int c){
+    void getAns(string s, int p, int c, vector<string> &ans){
         if(p == 0 && c == 0){
             ans.push_back(s);
             return;
         }
         if(p){
-            getAns(s + '(', p-1,c+1);
+            getAns(s + '(', p-1,c+1,ans);
         }
         if(c){
-            getAns(s + ')', p, c-1);
+            getAns(s + ')', p, c-1,ans);
         }
     }
 
 
     vector<string> generateParenthesis(int n) {
-        getAns("",n,0);
+        vector<string> ans;
+        getAns("",n,0,ans);
         return ans;
     }
 };
