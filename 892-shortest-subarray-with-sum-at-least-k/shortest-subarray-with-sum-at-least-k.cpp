269ceb0b664b17a9023ef5282b5b7be67d0d37c0
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-11-17 14:05:51 +0530

    Time: 46 ms (49.83%) | Memory: 112.7 MB (48.92%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
-        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
-        pq.push({0,-1});
+        deque<pair<long long, int>> pq;
+        pq.push_back({0,-1});
         long long sum = 0;
         int ans = INT_MAX;
         for(int i = 0; i < n; i++){
             sum += nums[i];
-            pq.push({sum,i});
-            while(pq.top().first <= (sum - k)){
-                ans = min(ans, i - pq.top().second);
-                pq.pop();
+            while(!pq.empty() && sum <= pq.back().first){
+                pq.pop_back();
             }
-            if(ans == 1){
-                return ans;
+            pq.push_back({sum,i});
+            while(!pq.empty() && (sum - k) >= pq.front().first){
+                ans = min(ans, i - pq.front().second);
+                pq.pop_front();
             }
         }
         return ans == INT_MAX ? -1 : ans;
