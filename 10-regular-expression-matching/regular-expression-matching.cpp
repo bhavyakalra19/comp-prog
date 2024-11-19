9b61bd90a4538bda22d6423ca3ab15319007afea
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-11-19 14:06:49 +0530

    Time: 14 ms (24.61%) | Memory: 21.2 MB (12.02%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
+    int n;
+    int m;
+    bool getAns(string s, string p, int i, int j, vector<vector<int>> &dp){
+        if(i < 0 && j < m){
+            return true;
+        }
+        if(i < 0 && p[j] == '*'){
+            return getAns(s,p,i,j-2,dp);
+        }
+        if(i < 0 || j < m){
+            return false;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(p[j] == '*'){
+            bool a = getAns(s,p,i,j-2,dp);
+            if(s[i] == p[j-1] || p[j-1] == '.'){
+                a = getAns(s,p,i-1,j,dp) || a;
+            }
+            return dp[i][j] = a;
+        }else if(s[i] == p[j] || p[j] == '.'){
+            return dp[i][j] = getAns(s,p,i-1,j-1,dp);
+        }
+        return false;
+    }
+
     bool isMatch(string s, string p) {
-        int m = s.size();
-        int n = p.size();
-        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
-        dp[0][0] = true;
-        for(int i = 0; i < n; i++){
-            if(p[i] == '*'){
-                dp[0][i+1] = dp[0][i-1];
-            }
-        }
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(p[j] == '.' || s[i] == p[j]){
-                    dp[i+1][j+1] = dp[i][j];
-                }else if(p[j] == '*'){
-                    dp[i+1][j+1] = dp[i+1][j-1];
-                    if(s[i] == p[j-1] || p[j-1] == '.'){
-                        dp[i+1][j+1] = dp[i+1][j+1] || dp[i][j+1];
-                    }
-                }
-            }
-        }
-        return dp[m][n];
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return getAns(s,p,n-1,m-1,dp);
     }
 };
