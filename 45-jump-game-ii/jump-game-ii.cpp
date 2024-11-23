6c67f003ec1acc883c9ee514913faeb90cfcfc9f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-11-23 16:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (48.43%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int getAns(vector<int> &nums, int idx, vector<int> &dp){
-        if(idx >= nums.size() - 1){
-            return 0;
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int ans = 100000;
-        for(int i = 1; i <= nums[idx]; i++){
-            ans = min(ans, getAns(nums,idx + i, dp));
-        }
-        return dp[idx] = 1 + ans;
-    }
-
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return getAns(nums,0,dp);
+        int n = nums.size();
+        int jump = 0;
+        int far = 0;
+        int near = 0;
+        for(int i = 0; i < n - 1; i++){
+            far = max(far, i + nums[i]);
+            if(i == near){
+                near = far;
+                jump++;
+            }
+        }
+        return jump;
     }
 };
