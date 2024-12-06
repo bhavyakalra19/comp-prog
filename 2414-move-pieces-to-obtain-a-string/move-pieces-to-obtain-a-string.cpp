7214e10864c27ca525387a7ef2f80700bc8e53c7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-06 23:01:39 +0530

    Time: 9 ms (63.59%) | Memory: 21.8 MB (39.92%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     bool canChange(string start, string target) {
-        int i = 0, j = 0, n = start.size();
-	
-        while(i < n || j < n){
-            
-            while(i < n && start[i] == '_')
+        int n = start.size();
+        int i = 0;
+        int j = 0;
+        while(i < n && j < n){
+            while(i < n && start[i] == '_'){
                 i++;
-            while(j < n && target[j] == '_')
+            }
+            while(j < n && target[j] == '_'){
                 j++;
-           
-            if(i == n || j == n)
+            }
+            if(i < n && j < n){
+                if(start[i] == target[j]){
+                    if((start[i] == 'L' && j > i) || (start[i] == 'R' && j < i)){
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }else{
                 break;
-            if(start[i] != target[j])
-                return false;
-            if(start[i] == 'L')
-                if(i < j)
-                    return false;
-            if(start[i] == 'R')
-                if(i > j)
-                    return false;
+            }
             i++;
             j++;
         }
-
+        while(i < n && start[i] == '_'){
+            i++;
+        }
+        while(j < n && target[j] == '_'){
+            j++;
+        }
         return i == n && j == n;
     }
 };
