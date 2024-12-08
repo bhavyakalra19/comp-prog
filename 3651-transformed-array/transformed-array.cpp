1828434661c3e9a3b2a5699c5e8427fa02e43e46
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-08 16:53:42 +0530

    Time: 4 ms (88.89%) | Memory: 26 MB (11.11%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-        for(int i = 0; i < n; i++){
-            ans.push_back(nums[(i + (nums[i] % n) + n) % n]);
-        }
+        for(int i = 0; i < n; i++) ans.push_back(nums[(i + (nums[i] % n) + n) % n]);
         return ans;
     }
 };
