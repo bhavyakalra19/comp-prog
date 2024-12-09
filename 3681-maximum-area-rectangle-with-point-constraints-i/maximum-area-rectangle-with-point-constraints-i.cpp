81c858aedf6cd3538d63a91c85472f7f3ab6421f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-09 18:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 32.9 MB (92.31%) - LeetSync

--- 3681-maximum-area-rectangle-with-point-constraints-i/maximum-area-rectangle-with-point-constraints-i.cpp
+++ 3681-maximum-area-rectangle-with-point-constraints-i/maximum-area-rectangle-with-point-constraints-i.cpp
@@ -13,12 +13,7 @@ public:
             for(int j = i+1; j < n; j++){
                 int ux = points[j][0];
                 int uy = points[j][1];
-                string a1 = to_string(bx) + '-' + to_string(by);
-                string a2 = to_string(ux) + '-' + to_string(uy);
-                string a3 = to_string(ux) + '-' + to_string(by);
-                string a4 = to_string(bx) + '-' + to_string(uy);
-                if(a1 != a2 && a2 != a3 && a3 != a4 && a1 != a4 && a2 != a4 && a1 != a3 && check[bx][uy] == 1 && check[ux][by] == 1){
-                    
+                if(uy != by && ux != bx && check[bx][uy] == 1 && check[ux][by] == 1){
                     int lx = min(bx,ux);
                     int mx = max(bx,ux);
                     int ly = min(uy,by);
@@ -30,9 +25,6 @@ public:
                         }
                     }
                     if(count == 4){
-                        cout << bx << " " << by << endl;
-                        cout << ux << " " << uy << endl;
-                        cout << "------" << endl;
                         ans = max(ans, (mx - lx) * (my - ly));
                     }
                 }
