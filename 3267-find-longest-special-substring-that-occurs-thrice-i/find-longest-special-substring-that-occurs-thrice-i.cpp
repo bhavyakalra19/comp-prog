51ec6921b6f4f8a5cd8c6ed18fa7e14ef578866b
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-10 10:48:27 +0530

    Time: 4 ms (91.14%) | Memory: 10.6 MB (87.05%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -17,20 +17,21 @@ public:
         int ans = -1;
         for(auto m : mp){
             string str = m.first;
-            int cnt = m.second; 
+            int cnt = m.second;
+            int sz = str.size();
             if(cnt > 2){
-                ans = max(ans, (int) str.size());
+                ans = max(ans, sz);
             }else if(str.size() > 2){
                 if(cnt == 2){
-                    ans = max(ans, (int)str.size() - 1);
+                    ans = max(ans, sz - 1);
                 }else{
-                    ans = max(ans, (int)str.size() - 2);
+                    ans = max(ans, sz - 2);
                 }
                 string sub = str.substr(1);
                 if(mp.find(sub) != mp.end()){
-                    ans = max(ans, (int) sub.size());
+                    ans = max(ans, sz - 1);
                 }
-            }else if(str.size() == 2 && mp.find(str.substr(1)) != mp.end()){
+            }else if(sz == 2 && mp.find(str.substr(1)) != mp.end()){
                 ans = max(ans,1);
             }
         }
