7f64bdca9b53691b14d0c5da32ce73c17a0d3c0a
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-14 01:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (9.55%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int getAns(int m, int n, vector<vector<int>> &dp){
-        if(m == 0 && n == 0){
-            return 1;
-        }
-        if(m < 0 || n < 0){
-            return 0;
-        }
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
-        return dp[m][n] = getAns(m-1,n,dp) + getAns(m,n-1,dp);
-    }
+    // int getAns(int m, int n, vector<vector<int>> &dp){
+    //     if(m == 0 && n == 0){
+    //         return 1;
+    //     }
+    //     if(m < 0 || n < 0){
+    //         return 0;
+    //     }
+    //     if(dp[m][n] != -1){
+    //         return dp[m][n];
+    //     }
+    //     return dp[m][n] = getAns(m-1,n,dp) + getAns(m,n-1,dp);
+    // }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return getAns(m-1,n-1,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        dp[0][0] = 1;
+        dp[1][1] = 1;
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 && j == 1){
+                    continue;
+                }
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+        // return getAns(m-1,n-1,dp);
     }
 };
