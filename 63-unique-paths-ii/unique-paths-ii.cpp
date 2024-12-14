a5ebdd31436d88e6fdaf5b4d72b8d12e833a047b
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-14 12:57:28 +0530

    Time: 2 ms (13.71%) | Memory: 12 MB (9.58%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int getAns(vector<vector<int>> &obstacleGrid, int m, int n, int i, int j, vector<vector<int>> &dp){
-        if(i == m || j == n || obstacleGrid[i][j] == 1){
+    int getAns(vector<vector<int>> &check, int i, int j, vector<vector<int>> &dp){
+        if(i < 0 || j < 0 || check[i][j] == 1){
             return 0;
         }
-        if(i == m - 1 && j == n - 1){
+        if(i == 0 && j == 0){
             return 1;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        return dp[i][j] = getAns(obstacleGrid, m, n, i+1, j, dp) + getAns(obstacleGrid, m, n, i, j+1, dp);
+        return dp[i][j] = getAns(check,i-1,j,dp) + getAns(check,i,j-1,dp);
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return getAns(obstacleGrid, m, n, 0, 0, dp);
+
+        return getAns(obstacleGrid, m - 1, n - 1, dp);
     }
 };
