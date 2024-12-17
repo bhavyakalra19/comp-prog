5299f04d9b9de7c0871109feaa840da0a62c31f3
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-17 18:50:28 +0530

    Time: 2755 ms (5.06%) | Memory: 27.7 MB (59.07%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        int n = s.size();
+        vector<int> check(26,0);
+        for(auto a : s){
+            check[a - 'a'] += 1;
+        }
+        int i = 0;
+        string ans = "";
+        int prev = -1;
+        int j;
+        while(i < n){
+            j = 25;
+            int limit = repeatLimit;
+            while(j >= 0 && (check[j] == 0 || j == prev)){
+                if(prev == j && check[j] > 0){
+                    limit = 1;
+                }
+                j--;
+            }
+            cout << j << " " << prev << endl;
+            if(j < 0){
+                break;
+            }
+            for(int a = 0; a < limit && check[j] > 0; a++){
+                ans += j + 'a';
+                check[j] -= 1;
+                i++;
+                prev = j;
+            }
+        }
+        return ans;
+    }
+};
