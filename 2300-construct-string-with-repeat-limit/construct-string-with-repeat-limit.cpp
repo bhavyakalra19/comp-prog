947b1db9337bd3215943ff4f6f88dabf2134a31f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-17 23:54:27 +0530

    Time: 39 ms (60.76%) | Memory: 27.9 MB (45.83%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -3,30 +3,39 @@ public:
     string repeatLimitedString(string s, int repeatLimit) {
         int n = s.size();
         vector<int> check(26,0);
+        priority_queue<char> pq;
         for(auto a : s){
-            check[a - 'a'] += 1;
-        }
-        int i = 0;
-        string ans = "";
-        int prev = -1;
-        int j;
-        while(i < n){
-            j = 25;
-            int limit = repeatLimit;
-            while(j >= 0 && (check[j] == 0 || j == prev)){
-                if(prev == j && check[j] > 0){
-                    limit = 1;
-                }
-                j--;
+            if(check[a - 'a'] == 0){
+                pq.push(a);
             }
-            if(j < 0){
+            check[a - 'a']++;
+        }
+        char a = '\0';
+        string ans = "";
+        while(!pq.empty()){
+            bool ch = false;
+            if(pq.top() == a){
+                pq.pop();
+                ch = true;
+            }
+            if(pq.empty()){
                 break;
             }
-            for(int a = 0; a < limit && check[j] > 0; a++){
-                ans += j + 'a';
-                check[j] -= 1;
-                i++;
-                prev = j;
+            auto it = pq.top();
+            pq.pop();
+            for(int i = 0; i < repeatLimit && check[it - 'a']; i++){
+                ans += it;
+                check[it - 'a']--;
+                if(ch){
+                    break;
+                }
+            }
+            if(ch){
+                pq.push(a);
+            }
+            a = it;
+            if(check[it - 'a'] > 0){
+                pq.push(it);
             }
         }
         return ans;
