451351adf4d93451b28b734c6b19036b2d045be2
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-19 11:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (40.61%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.cpp
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int getCount(vector<int> nums, int check){
+    int getAns(vector<int> &nums, int ch){
+        int i = 0;
+        int j = 0;
+        int n = nums.size();
         int ans = 0;
-        int cnt = 0;
-        for(auto a : nums){
-            if(a <= check){
-                cnt += 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] <= ch){
+                ans += i - j + 1;
             }else{
-                cnt = 0;
+                j = i + 1;
             }
-            ans += cnt;
         }
         return ans;
     }
 
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-        return getCount(nums,right) - getCount(nums,left-1);
+        return getAns(nums,right) - getAns(nums, left - 1);
     }
 };
