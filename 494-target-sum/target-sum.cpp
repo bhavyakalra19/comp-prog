14fe76d364c0b47f443d7359b70c6041210459c2
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-25 17:14:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (61.61%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int getAns(vector<int> nums, int idx, int k, vector<vector<int>> &dp){
-        if(idx == 0){
-            if(nums[idx] == 0 && k == 0){
-                return 2;
-            }
-            if(k == 0 || k == nums[0]){
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[idx][k] != -1){
-            return dp[idx][k];
-        }
-        int nt = getAns(nums, idx - 1, k, dp);
-        int tk = 0;
-        if(nums[idx] <= k) tk = getAns(nums, idx - 1, k - nums[idx], dp);
-        return dp[idx][k] = nt + tk;
-    }
+    // int getAns(vector<int> nums, int idx, int k, vector<vector<int>> &dp){
+    //     if(idx == 0){
+    //         if(nums[idx] == 0 && k == 0){
+    //             return 2;
+    //         }
+    //         if(k == 0 || k == nums[0]){
+    //             return 1;
+    //         }
+    //         return 0;
+    //     }
+    //     if(dp[idx][k] != -1){
+    //         return dp[idx][k];
+    //     }
+    //     int nt = getAns(nums, idx - 1, k, dp);
+    //     int tk = 0;
+    //     if(nums[idx] <= k) tk = getAns(nums, idx - 1, k - nums[idx], dp);
+    //     return dp[idx][k] = nt + tk;
+    // }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         //s2 - s1 = target;
@@ -30,7 +30,6 @@ public:
             return 0;
         }
         target = (sum - target)/2;
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
         vector<int> prev(target+1,0);
         prev[0] = 1;
         if(nums[0] <= target){
