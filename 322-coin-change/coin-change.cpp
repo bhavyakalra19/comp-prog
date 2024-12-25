74efb2b5a0908f0dd43aeec652420d5322b311b4
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-25 17:38:20 +0530

    Time: 63 ms (15.01%) | Memory: 49.4 MB (21.36%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
-        int n = coins.size();
-        for(int i = 1; i <= amount; i++){
-            for(auto c : coins){
-                if(i - c < 0){
-                    continue;
-                }
-                dp[i] = min(dp[i], dp[i-c]);
+    int getAns(vector<int> &coins, int amount, int idx, vector<vector<int>> &dp){
+        if(idx == 0){
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
             }
-            if(dp[i] != INT_MAX){
-                dp[i] += 1;
-            }
+            return INT_MAX;
         }
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        if(dp[idx][amount] != -1){
+            return dp[idx][amount];
+        }
+        int nt = getAns(coins, amount, idx - 1, dp);
+        int tk = INT_MAX;
+        if(coins[idx] <= amount){
+            tk = min(getAns(coins, amount - coins[idx], idx, dp), getAns(coins, amount - coins[idx], idx - 1, dp));
+        }
+        if(tk != INT_MAX){
+            tk++;
+        }
+        return dp[idx][amount] = min(nt,tk);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int a = getAns(coins, amount, n-1,dp);
+        return a == INT_MAX ? -1 : a;
     }
 };
