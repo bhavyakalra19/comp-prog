48ab5f50dc3ffdeb6e24bd529231ccf1db498a43
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-26 10:11:41 +0530

    Time: 2 ms (90.56%) | Memory: 12.6 MB (61.14%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
+    // int getAns(vector<int> &nums, int idx, int k, vector<vector<int>> &dp){
+    //     if(idx == 0){
+    //         if(nums[0] == 0 && k == 0){
+    //             return 2;
+    //         }
+    //         if(k == 0 || nums[0] == k){
+    //             return 1;
+    //         }
+    //     }
+    // }
+
     int findTargetSumWays(vector<int>& nums, int target) {
         //s2 - s = target
         //s2 = totalSum - s1
         //totalSum - s1 - s1 = target
         //totalSum - target = 2s1
         //(totalSum - target)/2 = s1
-        int n = nums.size();
         int sum = 0;
         for(auto a : nums) sum += a;
-        if(sum - target < 0 || (sum - target) % 2 != 0){
-            return 0;
-        }
+        if(sum - target < 0 || (sum - target) % 2 != 0) return 0;
         target = (sum - target)/2;
         vector<int> prev(target+1,0);
         if(nums[0] <= target) prev[nums[0]] = 1;
         prev[0] = 1;
         if(nums[0] == 0) prev[0] = 2;
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < nums.size(); i++){
             for(int j = target; j >= 0; j--){
-                int nt = prev[j];
-                int tk = 0;
-                if(nums[i] <= j) tk = prev[j - nums[i]];
-                prev[j] = nt + tk;
+                prev[j] = prev[j] + ((nums[i] <= j) ? prev[j - nums[i]] : 0);
             }
         }
         return prev[target];
