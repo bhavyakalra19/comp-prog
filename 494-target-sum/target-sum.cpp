fc5b4a20f9e0db63df013e94cfd294bc1f5724e3
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-26 10:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (62.54%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-    // int getAns(vector<int> nums, int idx, int k, vector<vector<int>> &dp){
-    //     if(idx == 0){
-    //         if(nums[idx] == 0 && k == 0){
-    //             return 2;
-    //         }
-    //         if(k == 0 || k == nums[0]){
-    //             return 1;
-    //         }
-    //         return 0;
-    //     }
-    //     if(dp[idx][k] != -1){
-    //         return dp[idx][k];
-    //     }
-    //     int nt = getAns(nums, idx - 1, k, dp);
-    //     int tk = 0;
-    //     if(nums[idx] <= k) tk = getAns(nums, idx - 1, k - nums[idx], dp);
-    //     return dp[idx][k] = nt + tk;
-    // }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        //s2 - s1 = target;
-        //total - s1 - s1 = target;
-        //(total - target)/2 = s1;
-        int sum = 0;
+        //s2 - s = target
+        //s2 = totalSum - s1
+        //totalSum - s1 - s1 = target
+        //totalSum - target = 2s1
+        //(totalSum - target)/2 = s1
         int n = nums.size();
+        int sum = 0;
         for(auto a : nums) sum += a;
-        if(sum - target < 0 || (sum - target)%2 != 0){
+        if(sum - target < 0 || (sum - target) % 2 != 0){
             return 0;
         }
         target = (sum - target)/2;
         vector<int> prev(target+1,0);
+        if(nums[0] <= target) prev[nums[0]] = 1;
         prev[0] = 1;
-        if(nums[0] <= target){
-            prev[nums[0]] = 1;
-        }
         if(nums[0] == 0) prev[0] = 2;
         for(int i = 1; i < n; i++){
             for(int j = target; j >= 0; j--){
                 int nt = prev[j];
-                int tk =  0;
-                if(j >= nums[i]) tk = prev[j - nums[i]];
+                int tk = 0;
+                if(nums[i] <= j) tk = prev[j - nums[i]];
                 prev[j] = nt + tk;
             }
         }
         return prev[target];
-        // return getAns(nums,n-1,target,dp);
     }
-
 };
