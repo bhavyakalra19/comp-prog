8a2b922a79a9717049df36af8c6ca660473b7155
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-28 01:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (20.37%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int getAns(vector<int> &prices, int idx, int buy, vector<vector<int>> &dp){
-        if(idx >= prices.size()){
-            return 0;
-        }
-        if(dp[idx][buy] != -1){
-            return dp[idx][buy];
-        }
-        if(buy){
-            return dp[idx][buy] = max(getAns(prices,idx+1,buy,dp), -prices[idx] + getAns(prices,idx+1,0,dp));
-        }
-        return dp[idx][buy] = max(getAns(prices,idx+1,buy,dp), prices[idx] + getAns(prices,idx+2,1,dp));
-    }
+    // int getAns(vector<int> &prices, int idx, int buy, vector<vector<int>> &dp){
+    //     if(idx >= prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][buy] != -1){
+    //         return dp[idx][buy];
+    //     }
+    //     if(buy){
+    //         return dp[idx][buy] = max(getAns(prices,idx+1,buy,dp), -prices[idx] + getAns(prices,idx+1,0,dp));
+    //     }
+    //     return dp[idx][buy] = max(getAns(prices,idx+1,buy,dp), prices[idx] + getAns(prices,idx+2,1,dp));
+    // }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return getAns(prices,0,1,dp);
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j < 2; j++){
+                if(j == 1){
+                    dp[i][j] = max(dp[i+1][j], -prices[i] + dp[i+1][0]);
+                }else{
+                    dp[i][j] = max(dp[i+1][j], prices[i] + dp[i+2][1]);
+                }
+            }
+        }
+        return dp[0][1];
+        // return getAns(prices,0,1,dp);
     }
 };
