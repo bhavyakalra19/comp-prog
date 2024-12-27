b6bbe4c9c59ca167a0864151b4977a05e00c5ff5
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-27 16:41:27 +0530

    Time: 27 ms (73.93%) | Memory: 44.1 MB (25.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    int getAns(string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
-        if(j < 0){
-            return 1;
-        }
-        if(i < 0){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int a = getAns(s1,s2,i-1,j,dp);
-        int b = 0;
-        if(s1[i] == s2[j]){
-            b = getAns(s1,s2,i-1,j-1,dp);
-        }
-        return dp[i][j] = a + b;
-    }
+    // int getAns(string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
+    //     if(j < 0){
+    //         return 1;
+    //     }
+    //     if(i < 0){
+    //         return 0;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int a = getAns(s1,s2,i-1,j,dp);
+    //     int b = 0;
+    //     if(s1[i] == s2[j]){
+    //         b = getAns(s1,s2,i-1,j-1,dp);
+    //     }
+    //     return dp[i][j] = a + b;
+    // }
 
     int numDistinct(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return getAns(s1,s2,n-1,m-1,dp);
+        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+        for(int i = 0; i < n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                int a = dp[i][j+1];
+                int b = 0;
+                if(s1[i] == s2[j]){
+                    b = dp[i][j];
+                }
+                dp[i+1][j+1] = (long long)a + (long long)b;
+            }
+        }
+        return dp[n][m];
     }
 };
