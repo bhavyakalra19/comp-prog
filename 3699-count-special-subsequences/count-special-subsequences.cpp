800a47105276d0191475314ee8daac8f235d3757
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-29 13:16:28 +0530

    Time: 856 ms (66.67%) | Memory: 94 MB (40.00%) - LeetSync

--- 3699-count-special-subsequences/count-special-subsequences.cpp
+++ 3699-count-special-subsequences/count-special-subsequences.cpp
@@ -6,16 +6,16 @@ public:
         long long ans = 0;
         for(int i = 0; i < n - 6; i++){
             for(int j = i+2; j < n - 4; j++){
-                mp[(double) nums[i] / (double) nums[j]]++;
+                mp[(double) nums[i] /nums[j]]++;
             }
         }
 
         for(int i = n - 3; i >= 4; i--){
             for(int j = i+2; j < n; j++){
-                ans += mp[(double) nums[j] / (double) nums[i]];
+                ans += mp[(double) nums[j] /nums[i]];
             }
             for(int j = 0; j <= i - 4; j++){
-                mp[(double) nums[j] / (double) nums[i-2]]--;
+                mp[(double) nums[j] /nums[i-2]]--;
             }
         }
         return ans;
