dc129d11cbf4cfe597f69042f0a740727754655b
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-29 13:16:06 +0530

    Time: 876 ms (53.33%) | Memory: 94 MB (40.00%) - LeetSync

--- 3699-count-special-subsequences/count-special-subsequences.cpp
+++ 3699-count-special-subsequences/count-special-subsequences.cpp
@@ -3,22 +3,21 @@ public:
     long long numberOfSubsequences(vector<int>& nums) {
         int n = nums.size();
         unordered_map<double,int> mp;
-        for(int i = 0; i < n - 5; i++){
-            for(int j = i+2; j < n - 3; j++){
-                mp[(double)nums[i]/(double)nums[j]]++;
+        long long ans = 0;
+        for(int i = 0; i < n - 6; i++){
+            for(int j = i+2; j < n - 4; j++){
+                mp[(double) nums[i] / (double) nums[j]]++;
             }
         }
-        long long ans = 0;
+
         for(int i = n - 3; i >= 4; i--){
-            for(int j = 0; j < i - 2; j++){
-                mp[(double)nums[j]/(double)nums[i-1]]--;
-            }
             for(int j = i+2; j < n; j++){
-                ans += mp[(double)nums[j]/(double)nums[i]];
+                ans += mp[(double) nums[j] / (double) nums[i]];
+            }
+            for(int j = 0; j <= i - 4; j++){
+                mp[(double) nums[j] / (double) nums[i-2]]--;
             }
         }
         return ans;
     }
 };
-
-
