eca02157b92c3097211177618f6249f84df9770f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-29 17:22:49 +0530

    Time: 72 ms (69.83%) | Memory: 58.1 MB (56.94%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-    const long long mod = 1000000007; 
-    // int getAns(vector<vector<int>> &check, int i, int j, string &target, vector<vector<int>> &dp){
-    //     if(j == target.size()){
-    //         return 1;
-    //     }
-    //     if(i == check.size()){
-    //         return 0;
-    //     }
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-    //     long long nt = getAns(check, i+1,j, target, dp);
-    //     long long tk = 0;
-    //     if(check[i][target[j] - 'a'] > 0) tk = ((long long) check[i][target[j] - 'a'] * getAns(check,i+1,j+1,target,dp))%mod;
-    //     return dp[i][j] = (nt + tk) % mod;
-    // }
-
+    const long long mod = 1000000007;
     int numWays(vector<string>& words, string target) {
         int n = words[0].size();
         int t = target.size();
@@ -28,10 +12,6 @@ public:
                 check[i][words[j][i] - 'a']++;
             }
         }
-        // for(int i = 0; i <= n; i++){
-        //     dp[i][t] = 1;
-        // }
-        
         for(int i = n - 1; i >= 0; i--){
             prev[t] = 1;
             vector<int> curr(t+1,0);
@@ -44,32 +24,6 @@ public:
             prev = curr;
         }
         return prev[0];
-        // return getAns(check,0,0,target,dp);
     }
 };
-
-// i -> word.lenght;
-// j -> target.length;
-
-// 0
-// a 1
-// b 1
-// c 1
-
-// 1
-// a 2
-// b 1
-
-// 2 
-// b 1
-// c 2
-
-// 3 
-// a 2
-// b 1
-
-
-// 0 1 3 (2)
-// 0 2 3 (2)
-// 1 2 3 (4)
 
