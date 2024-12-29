b7dc74c246aa65e9a1e41dcf8590edb5396837a5
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 00:04:51 +0530

    Time: 88 ms (55.12%) | Memory: 95.1 MB (38.73%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    int getAns(vector<int> &prices, int fee, int idx, int buy, vector<vector<int>> &dp){
-        if(idx == prices.size()){
-            return 0;
-        }
-        if(dp[idx][buy] != -1){
-            return dp[idx][buy];
-        }
-        if(buy == 0){
-            int nt = getAns(prices, fee, idx + 1, buy, dp);
-            int tk = -prices[idx] - fee + getAns(prices, fee, idx + 1, 1, dp);
-            return dp[idx][buy] = max(nt, tk);
-        }
-        return dp[idx][buy] = max(getAns(prices, fee, idx + 1, buy, dp), prices[idx] + getAns(prices,fee,idx+1,0,dp));
-    }
+    // int getAns(vector<int> &prices, int fee, int idx, int buy, vector<vector<int>> &dp){
+    //     if(idx == prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][buy] != -1){
+    //         return dp[idx][buy];
+    //     }
+    //     if(buy == 0){
+    //         int nt = getAns(prices, fee, idx + 1, buy, dp);
+    //         int tk = -prices[idx] - fee + getAns(prices, fee, idx + 1, 1, dp);
+    //         return dp[idx][buy] = max(nt, tk);
+    //     }
+    //     return dp[idx][buy] = max(getAns(prices, fee, idx + 1, buy, dp), prices[idx] + getAns(prices,fee,idx+1,0,dp));
+    // }
 
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        return getAns(prices, fee, 0, 0, dp);
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        for(int i = n-1; i >= 0; i--){
+            for(int j = 0; j < 2; j++){
+                if(j == 0){
+                    dp[i][j] = max(dp[i+1][j], -prices[i] - fee + dp[i+1][1]);
+                }else{
+                    dp[i][j] = max(dp[i+1][j], prices[i] + dp[i+1][0]);
+                }
+            }
+        }
+        return dp[0][0];
+        // return getAns(prices, fee, 0, 0, dp);
     }
 };
