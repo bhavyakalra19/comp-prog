a7dd9ec9f3f30a238917be650f98134a931e5296
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-31 00:55:59 +0530

    Time: 280 ms (29.55%) | Memory: 296.4 MB (9.63%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int getAns(vector<int> &nums, int idx, int prev, vector<vector<int>> &dp){
-        if(idx == nums.size()){
-            return 0;
-        }
-        if(dp[idx][prev+1] != -1){
-            return dp[idx][prev+1];
-        }
-        int notTake = getAns(nums, idx + 1, prev, dp);
-        int take = 0;
-        if(prev == -1 || nums[idx] > nums[prev]){
-            take = 1 + getAns(nums, idx + 1, idx, dp);
-        }
-        return dp[idx][prev+1] = max(notTake, take);
-    }
+    // int getAns(vector<int> &nums, int idx, int prev, vector<vector<int>> &dp){
+    //     if(idx == nums.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][prev+1] != -1){
+    //         return dp[idx][prev+1];
+    //     }
+    //     int notTake = getAns(nums, idx + 1, prev, dp);
+    //     int take = 0;
+    //     if(prev == -1 || nums[idx] > nums[prev]){
+    //         take = 1 + getAns(nums, idx + 1, idx, dp);
+    //     }
+    //     return dp[idx][prev+1] = max(notTake, take);
+    // }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
