e6861826e9418f4c35ea14b269cfb484a49f09dc
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 23:04:01 +0530

    Time: 2 ms (98.43%) | Memory: 11.6 MB (71.43%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    int getAns(int &zero, int &one, int mx, vector<int> &dp){
-        if(mx < one && mx < zero){
-            return 1;
-        }
-        int a = 0;
-        if(dp[mx] != -1){
-            return dp[mx];
-        }
-        if(mx >= zero){
-            a += getAns(zero, one, mx - zero, dp);
-        }
-        if(mx >= one){
-            a = (a + getAns(zero, one, mx - one, dp)) % mod;
-        }
-        return dp[mx] = (a + 1)%mod;
-    }
+    // int getAns(int &zero, int &one, int mx, vector<int> &dp){
+    //     if(mx < one && mx < zero){
+    //         return 1;
+    //     }
+    //     int a = 0;
+    //     if(dp[mx] != -1){
+    //         return dp[mx];
+    //     }
+    //     if(mx >= zero){
+    //         a += getAns(zero, one, mx - zero, dp);
+    //     }
+    //     if(mx >= one){
+    //         a = (a + getAns(zero, one, mx - one, dp)) % mod;
+    //     }
+    //     return dp[mx] = (a + 1)%mod;
+    // }
 
     int countGoodStrings(int low, int high, int zero, int one) {
         vector<int> dp(high + 1,0);
