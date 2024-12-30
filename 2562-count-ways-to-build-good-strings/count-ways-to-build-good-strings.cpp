22c58b2d913e6add3b57fe3b22d503df8d7f5b61
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 22:46:02 +0530

    Time: 9 ms (37.18%) | Memory: 19.6 MB (32.29%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
+    int getAns(int &zero, int &one, int mx, vector<int> &dp){
+        if(mx < one && mx < zero){
+            return 1;
+        }
+        int a = 0;
+        if(dp[mx] != -1){
+            return dp[mx];
+        }
+        if(mx >= zero){
+            a += getAns(zero, one, mx - zero, dp);
+        }
+        if(mx >= one){
+            a = (a + getAns(zero, one, mx - one, dp)) % mod;
+        }
+        return dp[mx] = (a + 1)%mod;
+    }
+
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high + 1, 0);
-        for(int i = 0; i < min(zero,one); i++){
-            dp[i] = 1;
-        }
-        for(int i = min(one,zero); i <= high; i++){
-            int a = 0;
-            if(i >= zero){
-                a = a + dp[i - zero];
-            }
-            if(i >= one){
-                a = (a + dp[i - one]) % mod;
-            }
-            
-            dp[i] = (a + 1)% mod;
-        }
-        return (dp[high] - dp[low - 1] + mod)%mod;
+        vector<int> dp(high + 1, -1);
+        return (getAns(zero,one,high,dp)  - getAns(zero,one,low-1,dp) + mod)%mod;
     }
 };
