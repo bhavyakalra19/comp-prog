1ce150b141e8d83fd4711af098995d5b2ce2a28a
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 18:35:31 +0530

    Time: 5 ms (45.06%) | Memory: 8.6 MB (69.99%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,23 +1,5 @@
 class Solution {
 public:
-    // bool getAns(string &s1, string &s2, string &s3, int i, int j, vector<vector<int>> &dp){
-    //     if(i + j == s3.size()){
-    //         return true;
-    //     }
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-
-    //     bool a = false;
-    //     if(i < s1.size() && s1[i] == s3[i+j]){
-    //         a = getAns(s1,s2,s3,i+1,j,dp);
-    //     }
-    //     if(j < s2.size() && s2[j] == s3[i+j]){
-    //         a = a | getAns(s1,s2,s3,i,j+1,dp);
-    //     }
-    //     return dp[i][j] = a;
-    // }
-
     bool isInterleave(string s1, string s2, string s3) {
         int n1 = s1.size();
         int n2 = s2.size();
@@ -25,7 +7,6 @@ public:
         if(n1 + n2 != n3){
             return false;
         }
-        // vector<vector<bool>> dp(n1+1, vector<bool>(n2+1,false));
         vector<bool> prev(n2 + 1, false);
         for(int i = n1; i >= 0; i--){
             vector<bool> curr(n2 + 1, false);
@@ -43,6 +24,5 @@ public:
             prev = curr;
         }
         return prev[0]; 
-        // return getAns(s1,s2,s3,0,0,dp);
     }
 };
