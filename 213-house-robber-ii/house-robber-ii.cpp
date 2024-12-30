e9f7d9c774acca848d03151ac379671cfe761168
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 23:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (45.10%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -9,13 +9,14 @@ public:
         int prev1 = max(nums[0],nums[1]);
         int p2 = 0;
         int p1 = nums[1];
+        int curr;
         for(int i = 2; i < n; i++){
             if(i < n - 1){
-                int curr = max(prev1, nums[i] + prev2);
+                curr = max(prev1, nums[i] + prev2);
                 prev2 = prev1;
                 prev1 = curr;
             }
-            int curr = max(p1, nums[i] + p2);
+            curr = max(p1, nums[i] + p2);
             p2 = p1;
             p1 = curr;
         }
