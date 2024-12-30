6fb3eab0d6df2d611b3d5c6318bbceef2ea85a08
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 23:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (13.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    int getAns(vector<int> &nums, int idx, vector<int> &dp){
-        if(idx == 0){
-            return nums[0];
-        }
-        if(idx == 1){
-            return max(nums[0],nums[1]);
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int notTake = getAns(nums, idx-1,dp);
-        int take = nums[idx] + getAns(nums,idx-2,dp);
-        return dp[idx] = max(take,notTake);
-    }
+    // int getAns(vector<int> &nums, int idx, vector<int> &dp){
+    //     if(idx == 0){
+    //         return nums[0];
+    //     }
+    //     if(idx == 1){
+    //         return max(nums[0],nums[1]);
+    //     }
+    //     if(dp[idx] != -1){
+    //         return dp[idx];
+    //     }
+    //     int notTake = getAns(nums, idx-1,dp);
+    //     int take = nums[idx] + getAns(nums,idx-2,dp);
+    //     return dp[idx] = max(take,notTake);
+    // }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-        // for(int i = 0; i < n; i++){
-        //     int 
-        // }
-        return getAns(nums,n-1,dp);
+        vector<int> dp(n,0);
+        if(n == 1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        }
+        return dp[n-1];
+        // return getAns(nums,n-1,dp);
     }
 };
