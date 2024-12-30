5f3a948da92394962469bc8e8d44d5f443326f2d
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2024-12-30 17:53:43 +0530

    Time: 2 ms (68.91%) | Memory: 9.7 MB (34.56%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool getAns(string &s1, string &s2, string &s3, int i, int j, int k, vector<vector<int>> &dp){
+    bool getAns(string &s1, string &s2, string &s3, int i, int j, vector<vector<int>> &dp){
         if(i + j == s3.size()){
             return true;
         }
@@ -9,11 +9,11 @@ public:
         }
 
         bool a = false;
-        if(i < s1.size() && s1[i] == s3[k]){
-            a = getAns(s1,s2,s3,i+1,j,k+1,dp);
+        if(i < s1.size() && s1[i] == s3[i+j]){
+            a = getAns(s1,s2,s3,i+1,j,dp);
         }
-        if(j < s2.size() && s2[j] == s3[k]){
-            a = a | getAns(s1,s2,s3,i,j+1,k+1,dp);
+        if(j < s2.size() && s2[j] == s3[i+j]){
+            a = a | getAns(s1,s2,s3,i,j+1,dp);
         }
         return dp[i][j] = a;
     }
@@ -21,15 +21,19 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n1 = s1.size();
         int n2 = s2.size();
-        if(n1 + n2 > s3.size()){
+        int n3 = s3.size();
+        if(n1 + n2 > n3){
             return false;
         }
         vector<vector<int>> dp(n1+1, vector<int>(n2+1,-1));
+        // dp[n1][n2] = true;
         // for(int i = n1 - 1; i >= 0; i--){
         //     for(int j = n2 - 1; j >= 0; j--){
-                
+        //         if(i + j == n3){
+        //             if(s1[i] == s3[])
+        //         }
         //     }
         // }
-        return getAns(s1,s2,s3,0,0,0,dp);
+        return getAns(s1,s2,s3,0,0,dp);
     }
 };
