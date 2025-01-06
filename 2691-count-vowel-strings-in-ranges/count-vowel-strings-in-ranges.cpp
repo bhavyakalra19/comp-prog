c1c28ad3731787bcc5e12ea95bec27d336a21731
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 13:13:23 +0530

    Time: 20 ms (31.16%) | Memory: 74.5 MB (14.97%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    unordered_set<char> st{'a', 'e', 'i', 'o', 'u'};
-    bool checkString(string s){
-        int n = s.size() - 1;
-        if(st.find(s[0]) != st.end() && st.find(s[n]) != st.end()){
-            return true;
-        }
-        return false;
-    }
 
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         int n = words.size();
         vector<int> check(n+1);
         int sum = 0;
+        unordered_set<char> st{'a', 'e', 'i', 'o', 'u'};
         for(int i = 0; i < n; i++){
-            if(checkString(words[i])){
+            if(st.find(words[i][0]) != st.end() && st.find(words[i][words[i].size() - 1]) != st.end()){
                 sum += 1;
             }
             check[i+1] = sum;
