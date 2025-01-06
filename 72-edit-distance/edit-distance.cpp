79ba28e1a196492535c95a470306354490c17377
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 19:28:35 +0530

    Time: 63 ms (13.47%) | Memory: 83.4 MB (5.06%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int getAns(string &w1, string &w2, int i, int j, int &n, int &m, vector<vector<int>> &dp){
+    int n;
+    int m;
+    int getAns(string word1, string word2, int i, int j, vector<vector<int>> &dp){
         if(i == n){
             return m - j;
         }
@@ -10,16 +12,16 @@ public:
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        if(w1[i] == w2[j]){
-            return dp[i][j] = getAns(w1,w2,i+1,j+1,n,m,dp);
+        if(word1[i] == word2[j]){
+            return dp[i][j] = getAns(word1, word2 , i+1, j+1, dp);
         }
-        return dp[i][j] = 1 + min(getAns(w1,w2,i+1,j+1,n,m,dp), min(getAns(w1,w2,i,j+1,n,m,dp), getAns(w1,w2,i+1,j,n,m,dp)));
+        return dp[i][j] = 1 + min(getAns(word1,word2,i+1,j,dp), min(getAns(word1,word2,i+1,j+1,dp), getAns(word1,word2,i,j+1,dp)));
     }
 
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
+        n = word1.size();
+        m = word2.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return getAns(word1,word2,0,0,n,m,dp);
+        return getAns(word1,word2,0,0,dp);
     }
 };
