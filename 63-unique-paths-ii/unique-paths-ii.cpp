c51af5edc7b2c5a999f6452127b39bfcadce7113
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 22:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (21.26%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    // int getAns(vector<vector<int>> &check, int i, int j, vector<vector<int>> &dp){
-    //     if(i < 0 || j < 0 || check[i][j] == 1){
-    //         return 0;
-    //     }
-    //     if(i == 0 && j == 0){
-    //         return 1;
-    //     }
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-    //     return dp[i][j] = getAns(check,i-1,j,dp) + getAns(check,i,j-1,dp);
-    // }
+    int getAns(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
+        if(i == 0 & j == 0){
+            return 1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int a = 0;
+        int b = 0;
+        if(i - 1 >= 0 && !grid[i-1][j]){
+            a = getAns(grid,i-1,j,dp);
+        }
+        if(j - 1 >= 0 && !grid[i][j-1]){
+            b = getAns(grid,i,j-1,dp);
+        }
+        return dp[i][j] = a + b;
+    }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        // We only need prev row data and current row data so why use extra space
-        // vector<vector<int>> dp(m,vector<int>(n,-1));
-
-        vector<int> prev(n,0);
-        for(int i = 0; i < m; i++){
-            vector<int> curr(n,0);
-            for(int j = 0; j < n; j++){
-                if(obstacleGrid[i][j] == 1){
-                    curr[j] = 0;
-                }else if(i == 0 && j == 0){
-                    curr[j] = 1;
-                }else{
-                    int a = (i > 0) ? prev[j] : 0;
-                    int b = (j > 0) ? curr[j-1] : 0;
-                    curr[j] = a + b;
-                } 
-            }
-            prev = curr;
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        if(obstacleGrid[n-1][m-1]){
+            return 0;
         }
-        return prev[n-1];
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return getAns(obstacleGrid, n-1, m-1, dp);
     }
 };
