30a5fe72b32507ff3dde644b4a65f3384acc9785
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 20:09:42 +0530

    Time: 23 ms (93.93%) | Memory: 21.1 MB (14.49%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,63 +1,47 @@
 class Solution {
 public:
-    // int getAns(vector<vector<int>> &grid, int idx, int j1, int j2, int n, int m, vector<vector<vector<int>>> &dp){
-    //     if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m){
-    //         return INT_MIN;
-    //     }
-    //     if(idx == n - 1){
-    //         if(j1 == j2){
-    //             return grid[idx][j1];
-    //         }
-    //         return grid[idx][j1] + grid[idx][j2];
-    //     }
-    //     if(dp[idx][j1][j2] != -1){
-    //         return dp[idx][j1][j2];
-    //     }
-    //     int mx = INT_MIN;
-    //     for(int i1 = -1; i1 <= 1; i1++){
-    //         for(int i2 = -1; i2 <= 1; i2++){
-    //             mx = max(mx, getAns(grid, idx + 1, j1 + i1, j2 + i2, n, m, dp));
-    //         }
-    //     }
-    //     int val = ((j1 == j2) ? grid[idx][j1] : grid[idx][j1] + grid[idx][j2]);
-    //     return dp[idx][j1][j2] = (val + mx);
-    // }
+    int n;
+    int m;
+    int getAns(vector<vector<int>> &grid, int i, int j1, int j2, vector<vector<vector<int>>> &dp){
+        if(j1 < 0 || j1 == m || j2 < 0 || j2 == m){
+            return INT_MIN;
+        }
+        if(i == n-1){
+            if(j1 == j2){
+                return grid[i][j1];
+            }
+            return grid[i][j1] + grid[i][j2];
+        }
+        if(dp[i][j1][j2] != -1){
+            return dp[i][j1][j2];
+        }
+        int ans = INT_MIN;
+        for(int a = -1; a < 2; a++){
+            for(int j = -1; j < 2; j++){
+                ans = max(ans, getAns(grid,i+1, j1 + a, j2 + j,dp));
+            }
+        }
+        if(j1 == j2){
+            return dp[i][j1][j2] = grid[i][j1] + ans;
+        }
+        return dp[i][j1][j2] = grid[i][j1] + grid[i][j2] + ans;
+    }
 
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        // vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        // return getAns(grid,0,0,m-1,n,m,dp);
-
-        vector<vector<int>> front(m,vector<int>(m,-1));
-        for(int j1 = 0; j1 < m; j1++){
-            for(int j2 = 0; j2 < m; j2++){
-                if(j1 == j2){
-                    front[j1][j2] = grid[n-1][j1];
-                }else{
-                    front[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
-                }
-            }
-        }
-
-        for(int i = n-2; i >= 0; i--){
-            vector<vector<int>> curr(m,vector<int>(m,0));
-            for(int j1 = 0; j1 < m; j1++){
-                for(int j2 = 0; j2 < m; j2++){
-                    int val = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-                    int mx = -1e8;
-                    for(int i1 = -1; i1 <= 1; i1++){
-                        for(int i2 = -1; i2 <= 1; i2++){
-                            if((j1 + i1 >= 0) && (j1 + i1 < m) && (j2 + i2 >= 0) && (j2 + i2 < m)){
-                                mx = max(mx, front[j1 + i1][i2 + j2]);
-                            }
-                        }
-                    }
-                    curr[j1][j2] = val + mx;
-                }
-            }
-            front = curr;
-        }
-        return front[0][m-1];
+        n = grid.size();
+        m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m,vector<int>(m,-1)));
+        return getAns(grid, 0, 0, m-1, dp);
     }
 };
+// j == n-1 -> idx1 == idx1 ? grid[idx1][j] : grid[idx1][j] + grid[idx2][j] 
+
+// if idx1 + 1 < m 
+// if idx1 - 1 >= 0
+// idx1
+
+// if idx2 + 1 < m 
+// if idx2 - 1 >= 0
+// idx2
+
+// if(idx1 == idx2) else diff
