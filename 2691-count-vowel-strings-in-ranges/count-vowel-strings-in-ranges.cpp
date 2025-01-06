f6600a36d4a8bcbfa6969d556efa0f9620bdeea6
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 13:14:28 +0530

    Time: 24 ms (18.28%) | Memory: 74.5 MB (19.24%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -4,7 +4,6 @@ public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         int n = words.size();
         vector<int> check(n+1);
-        int sum = 0;
         unordered_set<char> st{'a', 'e', 'i', 'o', 'u'};
         for(int i = 0; i < n; i++){
             if(st.find(words[i][0]) != st.end() && st.find(words[i][words[i].size() - 1]) != st.end()){
