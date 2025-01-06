e72771b0633d81a858225ed2f3681a7363c2602e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 19:34:04 +0530

    Time: 69 ms (8.68%) | Memory: 83.2 MB (5.06%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int n;
-    int m;
     int getAns(string word1, string word2, int i, int j, vector<vector<int>> &dp){
-        if(i == n){
-            return m - j;
+        if(i < 0){
+            return j+1;
         }
-        if(j == m){
-            return n - i;
+        if(j < 0){
+            return i+1;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         if(word1[i] == word2[j]){
-            return dp[i][j] = getAns(word1, word2 , i+1, j+1, dp);
+            return dp[i][j] = getAns(word1, word2 , i-1, j-1, dp);
         }
-        return dp[i][j] = 1 + min(getAns(word1,word2,i+1,j,dp), min(getAns(word1,word2,i+1,j+1,dp), getAns(word1,word2,i,j+1,dp)));
+        return dp[i][j] = 1 + min(getAns(word1,word2,i-1,j,dp), min(getAns(word1,word2,i-1,j-1,dp), getAns(word1,word2,i,j-1,dp)));
     }
 
     int minDistance(string word1, string word2) {
-        n = word1.size();
-        m = word2.size();
+        int n = word1.size();
+        int m = word2.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return getAns(word1,word2,0,0,dp);
+        return getAns(word1,word2,n-1,m-1,dp);
     }
 };
