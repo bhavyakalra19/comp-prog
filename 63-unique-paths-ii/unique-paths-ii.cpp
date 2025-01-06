f22dab5c8166b4a225dfdb64a05d7a91b7aa7a6e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 22:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (52.30%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int getAns(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
-        if(i == 0 & j == 0){
-            return 1;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int a = 0;
-        int b = 0;
-        if(i - 1 >= 0 && !grid[i-1][j]){
-            a = getAns(grid,i-1,j,dp);
-        }
-        if(j - 1 >= 0 && !grid[i][j-1]){
-            b = getAns(grid,i,j-1,dp);
-        }
-        return dp[i][j] = a + b;
-    }
+    // int getAns(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
+    //     if(i == 0 & j == 0){
+    //         return 1;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     int a = 0;
+    //     int b = 0;
+    //     if(i - 1 >= 0 && !grid[i-1][j]){
+    //         a = getAns(grid,i-1,j,dp);
+    //     }
+    //     if(j - 1 >= 0 && !grid[i][j-1]){
+    //         b = getAns(grid,i,j-1,dp);
+    //     }
+    //     return dp[i][j] = a + b;
+    // }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
@@ -25,6 +25,24 @@ public:
             return 0;
         }
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return getAns(obstacleGrid, n-1, m-1, dp);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    int a = 0;
+                    int b = 0;
+                    if(i - 1 >= 0 && !obstacleGrid[i-1][j]){
+                        a = dp[i-1][j];
+                    }
+                    if(j - 1 >= 0 && !obstacleGrid[i][j-1]){
+                        b = dp[i][j-1];
+                    }
+                    dp[i][j] = a + b;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+        // return getAns(obstacleGrid, n-1, m-1, dp);
     }
 };
