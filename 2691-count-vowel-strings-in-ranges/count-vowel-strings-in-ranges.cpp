45c6aa023029952756b963e1221ece8a1c0010df
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 13:14:23 +0530

    Time: 19 ms (35.69%) | Memory: 74.5 MB (19.24%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -8,9 +8,10 @@ public:
         unordered_set<char> st{'a', 'e', 'i', 'o', 'u'};
         for(int i = 0; i < n; i++){
             if(st.find(words[i][0]) != st.end() && st.find(words[i][words[i].size() - 1]) != st.end()){
-                sum += 1;
+                check[i+1] = check[i] + 1;
+            }else{
+                check[i+1] = check[i];
             }
-            check[i+1] = sum;
         }
         vector<int> ans;
         for(auto q : queries){
