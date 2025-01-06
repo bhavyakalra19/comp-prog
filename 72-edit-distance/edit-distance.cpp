015238142ac15481e4998c6fb9235a69803365d2
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-06 19:41:35 +0530

    Time: 6 ms (64.39%) | Memory: 13.1 MB (34.94%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int getAns(string word1, string word2, int i, int j, vector<vector<int>> &dp){
-        if(i < 0){
-            return j+1;
-        }
-        if(j < 0){
-            return i+1;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(word1[i] == word2[j]){
-            return dp[i][j] = getAns(word1, word2 , i-1, j-1, dp);
-        }
-        return dp[i][j] = 1 + min(getAns(word1,word2,i-1,j,dp), min(getAns(word1,word2,i-1,j-1,dp), getAns(word1,word2,i,j-1,dp)));
-    }
+    // int getAns(string word1, string word2, int i, int j, vector<vector<int>> &dp){
+    //     if(i < 0){
+    //         return j+1;
+    //     }
+    //     if(j < 0){
+    //         return i+1;
+    //     }
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+    //     if(word1[i] == word2[j]){
+    //         return dp[i][j] = getAns(word1, word2 , i-1, j-1, dp);
+    //     }
+    //     return dp[i][j] = 1 + min(getAns(word1,word2,i-1,j,dp), min(getAns(word1,word2,i-1,j-1,dp), getAns(word1,word2,i,j-1,dp)));
+    // }
 
     int minDistance(string word1, string word2) {
         int n = word1.size();
