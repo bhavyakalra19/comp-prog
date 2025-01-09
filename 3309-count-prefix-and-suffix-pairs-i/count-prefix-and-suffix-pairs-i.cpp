6af4a6cabbea6395cb287b0dcff894ec7a712e9a
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-09 23:10:13 +0530

    Time: 15 ms (12.53%) | Memory: 34 MB (5.10%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -2,8 +2,7 @@ class Node{
     public:
         char data;
         unordered_map<char, Node*> mp;
-        vector<int> check;
-
+        unordered_map<int,int> check;
         Node(char a){
             data = a;
         }
@@ -24,84 +23,59 @@ class Trie{
                     temp->mp[s] = t;
                 }
                 temp = temp->mp[s];
-                temp->check.push_back(idx);
+                temp->check[idx] = 1;
             }
         }
 
-        vector<int> search(string word){
+        void insertBack(string word, int idx, int z){
+            int n = word.size();
             Node *temp = root;
+            for(int i = n-1; i >= 0; i--){
+                if(temp->mp.find(word[i]) == temp->mp.end()){
+                    Node *t = new Node(word[i]);
+                    temp->mp[word[i]] = t;
+                }
+                temp = temp->mp[word[i]];
+                temp->check[idx + z] = 1;
+            }
+        }
+
+        long long search(string word, int z){
+            Node *temp = root;
+            Node *back = root;
             for(auto a : word){
                 if(temp->mp.find(a) == temp->mp.end()){
-                    return {};
+                    return 0;
                 }
                 temp = temp->mp[a];
             }
-            return temp->check;
+            int n = word.size();
+            for(int i = n - 1; i >= 0; i--){
+                if(back->mp.find(word[i]) == back->mp.end()){
+                    return 0;
+                }
+                back = back->mp[word[i]];
+            }
+            int ans = 0;
+            for(auto m : temp->check){
+                if(back->check.find(m.first + z) != back->check.end()){
+                    ans++;
+                }
+            }
+            return ans;
         }
 };
-
-class BackTrie{
-    public:
-        Node *root;
-        BackTrie(){
-            root = new Node('\0');
-        }
-
-        void insert(string word, int idx){
-            reverse(word.begin(), word.end());
-            Node *temp = root;
-            for(auto s : word){
-                if(temp->mp.find(s) == temp->mp.end()){
-                    Node *t = new Node(s);
-                    temp->mp[s] = t;
-                }
-                temp = temp->mp[s];
-                temp->check.push_back(idx);
-            }
-        }
-
-        vector<int> search(string word){
-            reverse(word.begin(), word.end());
-            Node *temp = root;
-            for(auto a : word){
-                if(temp->mp.find(a) == temp->mp.end()){
-                    return {};
-                }
-                temp = temp->mp[a];
-            }
-            return temp->check;
-        }
-};
-
-int getAns(vector<int> check1, vector<int> check2){
-    int i = check1.size() - 1;
-    int j = check2.size() - 1;
-    int ans = 0;
-    while(i >= 0 && j >= 0){
-        if(check1[i] == check2[j]){
-            ans++;
-            i--;
-            j--;
-        }else if(check1[i] < check2[j]){
-            i--;
-        }else{
-            j--;
-        }
-    }
-    return ans;
-}
 
 class Solution {
 public:
-    int countPrefixSuffixPairs(vector<string>& words) {
+    long long countPrefixSuffixPairs(vector<string>& words) {
         Trie *t = new Trie();
-        BackTrie *b = new BackTrie();
         int n = words.size();
-        int ans = 0;
+        long long ans = 0;
         for(int i = n-1; i >= 0; i--){
-            ans += getAns(t->search(words[i]), b->search(words[i]));
+            ans += t->search(words[i],n);
             t->insert(words[i],i);
-            b->insert(words[i],i);
+            t->insertBack(words[i],i,n);
         }
         return ans;
     }
