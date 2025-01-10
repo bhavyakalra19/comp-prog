2b02c57067f12267e7acd568453caec3cb8e6494
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-10 10:40:53 +0530

    Time: 31 ms (5.09%) | Memory: 37.5 MB (5.00%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -1,13 +1,55 @@
+class Node{
+    public:
+        char data;
+        unordered_map<char,Node*> mp;
+        int count;
+        Node(char a){
+            data = a;
+            count = 0;
+        }
+};
+
+class Trie{
+    public:
+        Node *root;
+
+        Trie(){
+            root = new Node('\0');
+        }
+
+        void insert(string word, int n){
+            Node *temp = root;
+            n = min((int)(word.size()),n);
+            for(int i = 0; i < n; i++){
+                if(temp->mp.find(word[i]) == temp->mp.end()){
+                    Node *t = new Node(word[i]);
+                    temp->mp[word[i]] = t; 
+                }
+                temp = temp->mp[word[i]];
+                temp->count++;
+            }
+        }
+
+        int search(string word){
+            Node *temp = root;
+            for(auto a : word){
+                if(temp->mp.find(a) == temp->mp.end()){
+                    return 0;
+                }
+                temp = temp->mp[a];
+            }
+            return temp->count;
+        }
+};
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int ans = 0;
+        Trie *t = new Trie();
         int c = pref.size();
-        for(auto s : words){
-            if(s.substr(0,c) == pref){
-                ans++;
-            }
+        for(auto w : words){
+            t->insert(w,c);
         }
-        return ans;
+        return t->search(pref);
     }
 };
