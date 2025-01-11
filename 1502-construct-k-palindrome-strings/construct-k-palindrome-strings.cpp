56e669f3f1f616be40702df8ba8c46b6edc296aa
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-11 09:56:18 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (78.67%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        int n = s.size();
+        if(n < k){
+            return false;
+        }
+        if(n == k){
+            return true;
+        }
+        vector<int> check(26,0);
+        for(auto a : s){
+            check[a - 'a']++;
+        }
+        int count = 0;
+        for(int i = 0; i < 26; i++){
+            if(check[i] % 2 != 0){
+                count++;
+            }
+        }
+        return count <= k;
+    }
+};
