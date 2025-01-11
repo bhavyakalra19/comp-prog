27becdef4eeaa0e50d452df08ea391eeb03e6067
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-11 09:56:37 +0530

    Time: 7 ms (51.75%) | Memory: 14.9 MB (66.78%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -5,9 +5,6 @@ public:
         if(n < k){
             return false;
         }
-        if(n == k){
-            return true;
-        }
         vector<int> check(26,0);
         for(auto a : s){
             check[a - 'a']++;
