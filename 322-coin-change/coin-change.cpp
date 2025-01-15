6c70cab4f99e7dea26f8a8fcec9fe1b13207b99b
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-15 16:28:44 +0530

    Time: 16 ms (87.57%) | Memory: 17.8 MB (82.50%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    // int getAns(vector<int> &coins, int idx, int amount, vector<vector<int>> &dp){
-    //     if(amount == 0){
-    //         return 0;
-    //     }
-    //     if(idx == 0){
-    //         if(amount % coins[0] == 0){
-    //             return amount / coins[0];
-    //         }
-    //         return INT_MAX;
-    //     }
-    //     if(dp[idx][amount] != -1){
-    //         return dp[idx][amount];
-    //     }
-    //     int a = INT_MAX;
-    //     int b = getAns(coins, idx - 1, amount, dp);
-    //     if(coins[idx] <= amount){
-    //         a = getAns(coins, idx, amount - coins[idx], dp);
-    //     }
-    //     if(a != INT_MAX){
-    //         a += 1;
-    //     }
-    //     return dp[idx][amount] = min(a,b);
-    // }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
