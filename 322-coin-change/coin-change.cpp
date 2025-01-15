10d2d531b2f8775a85a01a4554367b3568ff89a7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-15 16:14:30 +0530

    Time: 43 ms (39.61%) | Memory: 49.3 MB (25.47%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
+    int getAns(vector<int> &coins, int idx, int amount, vector<vector<int>> &dp){
+        if(amount == 0){
+            return 0;
+        }
+        if(idx == 0){
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
+            }
+            return INT_MAX;
+        }
+        if(dp[idx][amount] != -1){
+            return dp[idx][amount];
+        }
+        int a = INT_MAX;
+        int b = getAns(coins, idx - 1, amount, dp);
+        if(coins[idx] <= amount){
+            a = getAns(coins, idx, amount - coins[idx], dp);
+        }
+        if(a != INT_MAX){
+            a += 1;
+        }
+        return dp[idx][amount] = min(a,b);
+    }
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> prev(amount+1,INT_MAX);
-        for(int i = 0; i <= amount; i++){
-            if(i % coins[0] == 0){
-                prev[i] = i / coins[0];
-            }
-        }
-        prev[0] = 0;
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j <= amount; j++){
-                int a = INT_MAX;
-                int b = prev[j];
-                if(coins[i] <= j){
-                    a = prev[j - coins[i]];
-                }
-                if(a != INT_MAX){
-                    a++;
-                }
-                prev[j] = min(a,b);
-            }
-        }
-        return prev[amount] != INT_MAX ? prev[amount] : -1;
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int a = getAns(coins, n-1, amount, dp);
+        return a == INT_MAX ? -1 : a;
     }
 };
-
