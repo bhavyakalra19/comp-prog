56923fcedb0a54e3f8d7da3a8bb34bf31401fde8
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-15 16:41:19 +0530

    Time: 7 ms (85.53%) | Memory: 10.1 MB (87.44%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int getAns(vector<int> &coins, int idx, int amount, vector<vector<int>> &dp){
-        if(amount == 0){
-            return 1;
-        }
-        if(idx == 0){
-            return amount % coins[0] == 0;
-        }
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
-        }
-        int a = 0;
-        int b = getAns(coins, idx - 1, amount, dp);
-        if(coins[idx] <= amount){
-            a = getAns(coins, idx, amount - coins[idx], dp);
-        }
-        return dp[idx][amount] = a + b;
-    }
 
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return getAns(coins, n-1, amount, dp);
+        vector<long long> prev(amount+1, 0);
+        prev[0] = 1;
+        for(int i = 0; i <= amount; i++){
+            if(i % coins[0] == 0){
+                prev[i] = 1;
+            }
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= amount; j++){
+                int a = 0;
+                int b = prev[j];
+                if(coins[i] <= j){
+                    a = prev[j-coins[i]];
+                }
+                prev[j] = (long long)a + (long long)b;
+            }
+        }
+        return prev[amount];
     }
 };
