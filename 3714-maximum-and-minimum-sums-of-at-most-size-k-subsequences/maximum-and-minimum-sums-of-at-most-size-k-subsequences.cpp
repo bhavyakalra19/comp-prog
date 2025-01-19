2905fc4022973ea6a4605174f87613d7d2dded99
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-19 16:22:13 +0530

    Time: 519 ms (64.71%) | Memory: 390.4 MB (5.88%) - LeetSync

--- 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
+++ 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    long long getAns(int n, int r, vector<vector<int>> &dp){
-        if(r == 0){
-            return 1;
-        }
-        if(n == 0){
-            return r == 0;
-        }
-        if(dp[n][r] != -1){
-            return dp[n][r];
-        }
-        long long a = getAns(n-1, r, dp);
-        long long b = getAns(n-1, r-1, dp);
-        return dp[n][r] = (a + b) % mod;
-    }
-
     int minMaxSums(vector<int>& nums, int k) {
         int n = nums.size();
         vector<vector<long long>> dp(n, vector<long long>(k,0));
