209a9eadc24aca1b18503195d2283bd25cf04a07
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-19 11:07:26 +0530

    Time: 442 ms (7.69%) | Memory: 281.4 MB (53.85%) - LeetSync

--- 3737-paint-house-iv/paint-house-iv.cpp
+++ 3737-paint-house-iv/paint-house-iv.cpp
@@ -8,14 +8,13 @@ public:
             for(int p1 = 0; p1 <= 3; p1++){
                 for(int p2 = 0; p2 <= 3; p2++){
                     for(int c1 = 0; c1 < 3; c1++){
-                        for(int c2 = 0; c2 < 3; c2++){
-                            if(c1 == c2){
-                                continue;
+                        if(c1 != p1){
+                            for(int c2 = 0; c2 < 3; c2++){
+                                if(c1 == c2 || c2 == p2){
+                                    continue;
+                                }
+                                curr[p1][p2] = min(curr[p1][p2],  (long long) cost[i][c1] + (long long) cost[n - i - 1][c2] + dp[c1][c2]);
                             }
-                            if(c1 == p1 || c2 == p2){
-                                continue;
-                            }
-                            curr[p1][p2] = min(curr[p1][p2],  (long long) cost[i][c1] + (long long) cost[n - i - 1][c2] + dp[c1][c2]);
                         }
                     }
                 }
