151f123571a65445debcdaa64b1cc3bfc50ebdcb
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-19 11:09:23 +0530

    Time: 403 ms (7.69%) | Memory: 281.4 MB (53.85%) - LeetSync

--- 3737-paint-house-iv/paint-house-iv.cpp
+++ 3737-paint-house-iv/paint-house-iv.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     long long minCost(int n, vector<vector<int>>& cost) {
         vector<vector<long long>> dp(4, vector<long long>(4, 0));
         for(int i = n/2 - 1; i >= 0; i--){
@@ -10,10 +9,9 @@ public:
                     for(int c1 = 0; c1 < 3; c1++){
                         if(c1 != p1){
                             for(int c2 = 0; c2 < 3; c2++){
-                                if(c1 == c2 || c2 == p2){
-                                    continue;
+                                if(c1 != c2 && c2 != p2){
+                                    curr[p1][p2] = min(curr[p1][p2],  (long long) cost[i][c1] + (long long) cost[n - i - 1][c2] + dp[c1][c2]);                 
                                 }
-                                curr[p1][p2] = min(curr[p1][p2],  (long long) cost[i][c1] + (long long) cost[n - i - 1][c2] + dp[c1][c2]);
                             }
                         }
                     }
