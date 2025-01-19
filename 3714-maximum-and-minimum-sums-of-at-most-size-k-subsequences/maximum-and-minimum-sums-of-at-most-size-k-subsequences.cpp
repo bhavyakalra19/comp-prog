70b65a2ab62a96fe4cb5ff904e5b3be0e88368f7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-19 16:22:04 +0530

    Time: 554 ms (58.82%) | Memory: 390.4 MB (5.88%) - LeetSync

--- 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
+++ 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
@@ -16,10 +16,17 @@ public:
         return dp[n][r] = (a + b) % mod;
     }
 
-
     int minMaxSums(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(k,-1));
+        vector<vector<long long>> dp(n, vector<long long>(k,0));
+        for(int i = 0; i < n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < k; j++){
+                dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % mod;
+            }
+        }
         long long ans = 0;
         for(int a : nums){
             ans += 2 * a;
@@ -28,8 +35,8 @@ public:
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n; i++){
             for(int j = 1; j < k; j++){
-                ans = (ans + (nums[i] * getAns(n - i - 1, j, dp) % mod)) % mod;
-                ans = (ans + (nums[i] * getAns(i,j,dp)) % mod) % mod;
+                ans = (ans + (nums[i] * dp[n - i - 1][j] % mod)) % mod;
+                ans = (ans + (nums[i] * dp[i][j]) % mod) % mod;
             }
         }
         return ans;
