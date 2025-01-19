0df9554de87fd819022bbb3abe733406b1419b2c
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-19 11:06:15 +0530

    Time: 448 ms (7.69%) | Memory: 281.6 MB (53.85%) - LeetSync

--- 3737-paint-house-iv/paint-house-iv.cpp
+++ 3737-paint-house-iv/paint-house-iv.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
 
     long long minCost(int n, vector<vector<int>>& cost) {
-        vector<vector<vector<long long>>> dp(n/2 + 1, vector<vector<long long>>(4, vector<long long>(4, LONG_MAX)));
-        for(int i = 0; i <= 3; i++){
-            for(int j = 0; j <= 3; j++){
-                dp[n/2][i][j] = 0;
-            }
-        }
+        // vector<vector<vector<long long>>> dp(n/2 + 1, vector<vector<long long>>(4, vector<long long>(4, LONG_MAX)));
+        vector<vector<long long>> dp(4, vector<long long>(4, 0));
         for(int i = n/2 - 1; i >= 0; i--){
-            long long ans = LONG_MAX;
+            vector<vector<long long>> curr(4, vector<long long>(4, LONG_MAX));
             for(int p1 = 0; p1 <= 3; p1++){
                 for(int p2 = 0; p2 <= 3; p2++){
                     for(int c1 = 0; c1 < 3; c1++){
@@ -20,13 +16,14 @@ public:
                             if(c1 == p1 || c2 == p2){
                                 continue;
                             }
-                            dp[i][p1][p2] = min(dp[i][p1][p2],  (long long) cost[i][c1] + (long long) cost[n - i - 1][c2] + dp[i+1][c1][c2]);
+                            curr[p1][p2] = min(curr[p1][p2],  (long long) cost[i][c1] + (long long) cost[n - i - 1][c2] + dp[c1][c2]);
                         }
                     }
                 }
             }
+            dp = curr;
         }
-        return dp[0][3][3];
+        return dp[3][3];
     }
 };
 
