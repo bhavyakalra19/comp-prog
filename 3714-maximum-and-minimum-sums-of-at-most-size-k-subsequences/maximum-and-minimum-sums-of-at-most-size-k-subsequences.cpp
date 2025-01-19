ec267cfa200453e0436015a65f3000fd9d28fa89
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-19 16:18:49 +0530

    Time: 1021 ms (11.76%) | Memory: 261.1 MB (29.41%) - LeetSync

--- 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
+++ 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/maximum-and-minimum-sums-of-at-most-size-k-subsequences.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    typedef long long ll;
-    vector<ll> f, factInv;
-
-    Solution() {
-        f.resize(100000);
-        factInv.resize(100000);
-        f[0] = 1;
-        for(int i = 1; i < 100000; i++){
-            f[i] = f[i-1] * i % mod;
+    long long getAns(int n, int r, vector<vector<int>> &dp){
+        if(r == 0){
+            return 1;
         }
-        
-        factInv[100000 - 1] = binpow(f[100000 - 1], mod - 2);
-        for(int i = 100000 - 2; i >= 0; i--){
-            factInv[i] = factInv[i+1] * (i+1) % mod;
+        if(n == 0){
+            return r == 0;
         }
-    }
-
-    ll binpow(ll a, ll b){
-        ll res = 1;
-        while (b) {
-            if (b % 2) {
-                res = (res * a) % mod;
-            }
-            a = (a * a) % mod;
-            b /= 2;
+        if(dp[n][r] != -1){
+            return dp[n][r];
         }
-        return res;
-    }
-
-    ll comb(ll n, ll r) {
-        if (r > n) return 0;
-        return f[n] * factInv[r] % mod * factInv[n - r] % mod;
+        long long a = getAns(n-1, r, dp);
+        long long b = getAns(n-1, r-1, dp);
+        return dp[n][r] = (a + b) % mod;
     }
 
 
     int minMaxSums(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(k,-1));
         long long ans = 0;
         for(int a : nums){
             ans += 2 * a;
@@ -46,8 +28,8 @@ public:
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n; i++){
             for(int j = 1; j < k; j++){
-                ans = (ans + (nums[i] * comb(n - i - 1, j) % mod)) % mod;
-                ans = (ans + (nums[i] * comb(i,j)) % mod) % mod;
+                ans = (ans + (nums[i] * getAns(n - i - 1, j, dp) % mod)) % mod;
+                ans = (ans + (nums[i] * getAns(i,j,dp)) % mod) % mod;
             }
         }
         return ans;
