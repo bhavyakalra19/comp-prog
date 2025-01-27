24b34fc82c4b197113519a925fa64704624a181c
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-27 22:03:34 +0530

    Time: 67 ms (59.31%) | Memory: 215.8 MB (43.07%) - LeetSync

--- 3674-count-non-decreasing-subarrays-after-k-operations/count-non-decreasing-subarrays-after-k-operations.cpp
+++ 3674-count-non-decreasing-subarrays-after-k-operations/count-non-decreasing-subarrays-after-k-operations.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     long long countNonDecreasingSubarrays(vector<int>& nums, long long k) {
-        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(), nums.end());
         int n = nums.size();
-        deque<int> dq;
-        int i = 0;
         long long res = 0;
-        for(int j = 0; j < n; j++){
-            while(!dq.empty() && nums[dq.front()] < nums[j]){
+        deque<int> dq;
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            while(!dq.empty() && nums[dq.front()] < nums[i]){
                 int r = dq.front();
                 dq.pop_front();
-                int l = dq.empty() ? i - 1 : dq.front();
-                k -= 1LL * (nums[j] - nums[r]) * (r - l);
+                int l = dq.empty() ? j - 1 : dq.front();
+                k -= 1LL *(nums[i] - nums[r]) * (r - l);
             }
-            dq.push_front(j);
+            dq.push_front(i);
             while(k < 0){
-                k += nums[dq.back()] - nums[i];
-                if(nums[dq.back()] == nums[i]){
+                k += nums[dq.back()] - nums[j];
+                if(nums[dq.back()] == nums[j]){
                     dq.pop_back();
                 }
-                ++i;
+                j++;
             }
-            res += j - i + 1;
+            res += i - j + 1;
         }
         return res;
     }
