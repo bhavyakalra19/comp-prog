f9acc15a0cf761f0d82dad486b1a7f26d1efaed7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-27 13:40:48 +0530

    Time: 95 ms (32.41%) | Memory: 80.2 MB (19.46%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        int n = numCourses;
+        vector<vector<int>> gp(n);
+        vector<set<int>> check(n);
+        vector<int> degree(n,0);
+        vector<bool> ans;
+        for(auto p : prerequisites){
+            gp[p[0]].push_back(p[1]);
+            degree[p[1]]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(degree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            for(auto g : gp[it]){
+                --degree[g];
+                check[g].insert(it);
+                for(auto c : check[it]){
+                    check[g].insert(c);
+                }
+                if(degree[g] == 0){
+                    q.push(g);
+                }
+            }
+        }
+
+        for(auto q : queries){
+            if(check[q[1]].find(q[0]) != check[q[1]].end()){
+                ans.push_back(true);
+            }else{
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
