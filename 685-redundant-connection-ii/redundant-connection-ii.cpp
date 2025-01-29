79fbb18fd85188b61ee74337ee8ae6fdbb4570e0
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-29 14:14:52 +0530

    Time: 3727 ms (5.13%) | Memory: 29.6 MB (9.52%) - LeetSync

---
+++ 685-redundant-connection-ii/redundant-connection-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    void getAns(vector<vector<int>> &gp ,int idx, int &st, int &en, vector<int> &vis, int &cn){
+        cout << idx << endl;
+        vis[idx] = true;
+        cn++;
+        for(auto g : gp[idx]){
+            if((vis[g]) || (idx == st && g == en)){
+                continue;
+            }
+            getAns(gp,g,st,en,vis,cn);
+        }
+    }
+
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>> gp(n+1);
+        vector<int> degree(n+1,0);
+        for(auto e : edges){
+            gp[e[0]].push_back(e[1]);
+            degree[e[1]]++;
+        }
+        int cn = 0;
+        for(int i = 1; i <= n; i++){
+            if(degree[i] == 0){
+                cn = i;
+                break;
+            }
+        }
+        for(int i = n - 1; i >= 0; i--){
+            vector<int> vis(n+1,false);
+            int cnt = 0;
+            if(cn != 0){
+                getAns(gp,cn,edges[i][0],edges[i][1],vis,cnt);
+                if(cnt == n){
+                    return edges[i];
+                }
+            }else if(degree[edges[i][1]] == 1){
+                getAns(gp,edges[i][1],edges[i][0],edges[i][1],vis, cnt);
+                if(cnt == n){
+                    return edges[i];
+                }
+            }
+        }
+        return {0,0};
+    }
+};
