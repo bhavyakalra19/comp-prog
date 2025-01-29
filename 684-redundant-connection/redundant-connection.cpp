2ffe461a1868e0f72e5d922479cd446a29968a60
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-29 09:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (69.21%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int find(vector<int> &check, int num){
-        if(check[num] == 0 || check[num] == num){
-            return check[num];
+    vector<int> check;
+
+    int findFunc(int a){
+        if(check[a] == a){
+            return a;
         }
-        return find(check,check[num]);
+        return findFunc(check[a]);
     }
 
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> check;
         for(int i = 0; i <= n; i++){
             check.push_back(i);
         }
-        for(int i = 0; i < n; i++){
-            int a = find(check,edges[i][0]);
-            int b = find(check,edges[i][1]);
+        for(auto e : edges){
+            int a = findFunc(e[0]);
+            int b = findFunc(e[1]);
             if(a == b){
-                return edges[i];
-            }else{
-                check[a] = b;
+                return e;
             }
+            check[a] = b;
         }
-        return edges[n-1];
-    }   
+        return {0,0};
+    }
 };
