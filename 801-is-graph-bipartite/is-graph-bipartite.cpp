2f9f1393ee774fcdf0bf0d1cb4930c59a5d8092d
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-01-29 16:44:21 +0530

    Time: 3 ms (16.83%) | Memory: 17.3 MB (56.74%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<bool> s1(n,false);
+        vector<bool> s2(n,false);
+        queue<int> q1;
+        queue<int> q2;
+        q1.push(0);
+        vector<bool> vis(n,false);
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                q1.push(i);
+            }
+            while(!q1.empty() || !q2.empty()){
+                while(!q1.empty()){
+                    auto it = q1.front();
+                    q1.pop();
+                    for(auto a : graph[it]){
+                        if(s1[a]){
+                            return false;
+                        }
+                        s2[a] = true;
+                        if(!vis[a]){
+                            vis[a] = true;
+                            q2.push(a);
+                        }
+                    }
+                }
+                while(!q2.empty()){
+                    auto it = q2.front();
+                    q2.pop();
+                    for(auto a : graph[it]){
+                        if(s2[a]){
+                            return false;
+                        }
+                        s1[a] = true;
+                        if(!vis[a]){
+                            vis[a] = true;
+                            q1.push(a);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
