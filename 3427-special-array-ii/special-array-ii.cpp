da391419379dc2ce3f50e8fd33c04aa0b0ab75c9
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-01 13:04:23 +0530

    Time: 43 ms (27.86%) | Memory: 136.2 MB (23.71%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -5,18 +5,18 @@ public:
         vector<int> check(n);
         check[0] = 0;
         for(int i = 1; i < n; i++){
-            if((nums[i] - nums[i-1]) % 2 == 0){
-                check[i] = 0;
+            if(nums[i] % 2 == nums[i-1] % 2){
+                check[i] = check[i-1];
             }else{
                 check[i] = check[i-1] + 1;
             }
         }
         vector<bool> ans;
         for(auto q : queries){
-            if(check[q[1]] - check[q[0]] != (q[1] - q[0])){
-                ans.push_back(false);
-            }else{
+            if((q[1] - q[0]) == (check[q[1]] - check[q[0]])){
                 ans.push_back(true);
+            }else{
+                ans.push_back(false);
             }
         }
         return ans;
