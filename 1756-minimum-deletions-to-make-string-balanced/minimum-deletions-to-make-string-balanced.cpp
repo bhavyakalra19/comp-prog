fea83951d6aa964b8669b08c814419e6314fb0b3
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-01 17:38:31 +0530

    Time: 24 ms (61.92%) | Memory: 40.7 MB (22.95%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -4,16 +4,17 @@ public:
         int n = s.size();
         vector<int> check(n+1,0);
         int sm = 0;
-        for(int i = n - 1; i >= 0; i--){
-            if(s[i] == 'a'){
-                sm += 1;
-            }
-            check[i] = sm;
-        }
+        // for(int i = n - 1; i >= 0; i--){
+        //     if(s[i] == 'a'){
+        //         sm += 1;
+        //     }
+        // }
         int next = 0;
         for(int i = n-1; i >= 0; i--){
             if(s[i] != 'a'){
-                next = min(1 + next, check[i+1]);
+                next = min(1 + next, sm);
+            }else{
+                sm++;
             }
         }
         return next;
