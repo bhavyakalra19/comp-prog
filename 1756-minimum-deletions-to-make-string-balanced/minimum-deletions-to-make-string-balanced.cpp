acb1db6beb16f44a0854a79b1e3fdc26e26b8a24
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-01 17:12:49 +0530

    Time: 55 ms (14.87%) | Memory: 55 MB (17.31%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    int getAns(string &s, int idx, vector<int> &dp, vector<int> &check){
-        if(idx == s.size()){
-            return 0;
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        if(s[idx] == 'a'){
-            return dp[idx] = getAns(s,idx+1,dp,check);
-        }
-        return dp[idx] = min(check[idx+1], 1 + getAns(s,idx+1,dp,check));
-    }
-
     int minimumDeletions(string s) {
         int n = s.size();
         vector<int> check(n+1,0);
