a9355d72ae351eedc24e4f0b8108f19a813d28f6
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-01 17:04:29 +0530

    Time: 704 ms (5.26%) | Memory: 274.5 MB (6.67%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    int getAns(string &s, int idx, int v, vector<vector<int>> &dp, vector<int> &check){
+        if(idx == s.size()){
+            return 0;
+        }
+        if(v == 1){
+            return check[idx];
+        }
+        if(dp[idx][v] != -1){
+            return dp[idx][v];
+        }
+
+        if(v == 0){
+           if(s[idx] == 'a'){
+                return dp[idx][v] = getAns(s,idx+1,v,dp,check);
+           }else{
+                return dp[idx][v] = min(getAns(s,idx+1,1,dp,check), 1 + getAns(s,idx+1,v,dp,check));
+           }
+        }
+        return 4;
+    }
+
     int minimumDeletions(string s) {
         int n = s.size();
-        int b = 0;
-        vector<int> f(n+1,0);
-        for(int i = 1; i <= n; ++i){
-            if(s[i-1] == 'b'){
-                ++b;
-                f[i] = f[i-1];
-            }else{
-                f[i] = min(f[i-1] + 1, b);
+        vector<int> check(n,0);
+        int sm = 0;
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == 'a'){
+                sm += 1;
             }
+            check[i] = sm;
         }
-        return f[n];
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return getAns(s,0,0,dp,check);
     }
 };
