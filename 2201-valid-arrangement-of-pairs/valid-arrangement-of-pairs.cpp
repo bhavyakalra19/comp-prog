66cc4945c03dbadb49f0e1a18b3307cddbfc1dfa
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-01 11:44:58 +0530

    Time: 610 ms (38.08%) | Memory: 453.4 MB (19.77%) - LeetSync

--- 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.cpp
+++ 2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    void dfs(unordered_map<int,stack<int>> &mp, vector<int> &curr, int idx){
-        while(!mp[idx].empty()){
-            int a = mp[idx].top();
-            mp[idx].pop();
-            // cout << idx << " " << a << endl;
-            dfs(mp,curr,a);
-        }
-        curr.push_back(idx);
-    }
+    // void dfs(unordered_map<int,stack<int>> &mp, vector<int> &curr, int idx){
+    //     while(!mp[idx].empty()){
+    //         int a = mp[idx].top();
+    //         mp[idx].pop();
+    //         dfs(mp,curr,a);
+    //     }
+    //     curr.push_back(idx);
+    // }
 
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
         unordered_map<int,stack<int>> mp;
@@ -27,19 +26,18 @@ public:
                 start = m.first;
             }
         }
-        // st.push(start);
-        // while(!st.empty()){
-        //     auto it = st.top();
-        //     if(!mp[it].empty()){
-        //         int c = mp[it].top();
-        //         mp[it].pop();
-        //         st.push(c);
-        //     }else{
-        //         st.pop();
-        //         curr.push_back(it);
-        //     }
-        // }
-        dfs(mp,curr,start);
+        st.push(start);
+        while(!st.empty()){
+            auto it = st.top();
+            if(!mp[it].empty()){
+                int c = mp[it].top();
+                mp[it].pop();
+                st.push(c);
+            }else{
+                st.pop();
+                curr.push_back(it);
+            }
+        }
         for(int i = curr.size() - 1; i > 0; i--){
             ans.push_back({curr[i],curr[i-1]});
         }
