95b12539793b020670e8227fb813bc8288651f36
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-04 22:43:07 +0530

    Time: 155 ms (18.04%) | Memory: 75.6 MB (12.02%) - LeetSync

--- 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.cpp
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
+    unordered_map<int,bool> mp;
+
+    int getAns(int i, int &a, int &b, int &x, vector<vector<int>> &dp, int val){
+        if(i == x){
+            return 0;
+        }
+        if(i < 0 || i > 6000 || mp.find(i) != mp.end()){
+            return 1e9;
+        }
+        if(dp[i][val] != -1){
+            return dp[i][val];
+        }
+        dp[i][val] = 1 + getAns(i + a, a, b, x, dp, 0);
+        if(val == 0){
+            dp[i][val] = min(dp[i][val], 1 + getAns(i - b, a, b, x, dp, 1));
+        }
+        return dp[i][val];
+    }
+
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        unordered_map<int,int> mp;
-        for(auto f : forbidden){
-            mp[f] = 1;
+        int mx = 7000;
+        for(auto c : forbidden){
+            mp[c] = true;
         }
-        vector<vector<int>> visit(2,vector<int>(6002));
-        visit[0][0] = 1;
-        visit[1][0] = 1;
-        queue<pair<int,int>> q;
-        int step = 0;
-        q.push({0,0});
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto it = q.front();
-                q.pop();
-                if(it.first == x){
-                    return step;
-                }
-                int bck = it.first - b;
-                int frw = it.first + a;
-                if(frw < 6002 && mp.find(frw) == mp.end() && visit[0][frw] == 0){
-                    visit[0][frw] = 1;
-                    q.push({frw, 0});
-                }
-                if((bck >= 0) && it.second == 0 && mp.find(bck) == mp.end() && visit[1][bck] == 0){
-                    visit[1][bck] = 1;
-                    q.push({bck, 1});
-                }
-                
-            }
-            step++;
-        }
-        return -1;
+        vector<vector<int>> dp(mx+1, vector<int>(2,-1));
+        int ans = getAns(0, a, b, x, dp, 0);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
