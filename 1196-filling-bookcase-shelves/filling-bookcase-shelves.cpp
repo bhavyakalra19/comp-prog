da2189dc927535e64b875a4e0f0b7ae0804b26cc
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-06 09:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (93.77%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int n;
-    int getAns(vector<vector<int>> &books, int idx, int &sw, vector<int> &dp){
-        if(idx == n) return 0;
-        int mh = 0;
-        int mw = 0;
-        int mn = INT_MAX;
-        if(dp[idx] != -1) return dp[idx];
-        for(int i = idx; i < n; i++){
-            if(mw + books[i][0] <= sw){
-                mh = max(mh, books[i][1]);
-                mw += books[i][0];
-                mn = min(mn, mh + getAns(books, i+1, sw, dp));
-            }else{
-                break;
-            }
-        }
-        return dp[idx] = mn; 
-    }
 
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        n = books.size();
-        vector<int> dp(n,-1);
-        return getAns(books, 0, shelfWidth, dp); 
+        int n = books.size();
+        vector<int> dp(n+1, 0);
+        for(int idx = n-1; idx >= 0; idx--){
+            int mh = 0;
+            int mw = 0;
+            int mn = INT_MAX;
+            for(int i = idx; i < n; i++){
+                if(mw + books[i][0] <= shelfWidth){
+                    mh = max(mh, books[i][1]);
+                    mw += books[i][0];
+                    mn = min(mn, mh + dp[i+1]);
+                }else{
+                    break;
+                }
+            }
+            dp[idx] = mn;
+        }
+        return dp[0]; 
     }
 };
