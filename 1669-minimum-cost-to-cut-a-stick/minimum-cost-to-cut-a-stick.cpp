e3e9736c8091588fd71b0d63c700e01966a5eb30
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-14 18:50:18 +0530

    Time: 41 ms (16.24%) | Memory: 15.3 MB (5.86%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -5,16 +5,17 @@ public:
         if(dp[i][j] != -1) return dp[i][j];
         long long ans = INT_MAX;
         for(int k = i; k <= j; k++){
-            ans = min(ans, (1LL * cuts[j+1] - cuts[i-1] + getAns(cuts, i, k-1, dp) + getAns(cuts, k+1,j, dp)));
+            ans = min(ans, 1LL * cuts[j+1] - cuts[i-1] + getAns(cuts, i, k-1, dp) + getAns(cuts, k+1, j, dp));
         }
         return dp[i][j] = ans;
     }
 
     int minCost(int n, vector<int>& cuts) {
+        int c = cuts.size();
         cuts.push_back(0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        vector<vector<long long>> dp(cuts.size(), vector<long long>(cuts.size(), -1));
-        return getAns(cuts,1,cuts.size()-2, dp);
+        vector<vector<long long>> dp(c+2, vector<long long>(c+2,-1));
+        return getAns(cuts, 1, c, dp);
     }
 };
