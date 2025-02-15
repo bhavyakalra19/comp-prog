c860e935931c0bba8fe58a9f0aa06539189211ad
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-15 23:56:36 +0530

    Time: 177 ms (26.67%) | Memory: 204.1 MB (16.67%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,31 +1,62 @@
 class Solution {
 public:
-    double separateSquares(vector<vector<int>>& squares) {
-        double st = INT_MAX;
-        double en = 0;
-        for(auto s : squares){
-            st = min(st,(double) s[1]);
-            en = max(en, (double)s[1] + s[2]);
-        }
-        double eps = 1e-5;
-        while(en - st > eps){
-            double y = (st + en)/2;
-            double up = 0;
-            double lw = 0;
-            for(const auto& s : squares){
-                double side = s[2];
-                double s1 = s[1];
-                if(s1 >= y){
-                    up += side * side;
-                }else if(s1 + side <= y){
-                    lw += side * side;
-                }else{
-                    up += side * ((s1 + side) - y);
-                    lw += side * (y - s1);
-                }
+    double getAns(vector<vector<int>> &nums, double y, int &cnt){
+        double up = 0;
+        double lw = 0;
+        for(auto &s : nums){
+            double side = s[2];
+            if(side == 0) continue;
+            if(s[1] >= y){
+                up += side * side;
+            }else if((double)(s[1] + s[2]) <= y){
+                lw += side * side;
+            }else{
+                cnt++;
+                double su1 = (double)(s[1] + s[2]) - y;
+                double sl1 = y - (double)s[1];
+                up += side * su1;
+                lw += side * sl1;
             }
-            up > lw ? st = y : en = y;
         }
-        return st;
+        return up - lw;
+    }
+
+    double separateSquares(vector<vector<int>>& squares) {
+        int mx = 0;
+        int mn = INT_MAX;
+        for(auto s : squares){
+            mn = min(mn, s[1]);
+            mx = max(mx, s[1] + s[2]);
+        }
+        double st = mn;
+        double en = mx;
+        double ms = INT_MAX;
+        double ans;
+        if(st == en) return st;
+        while(st < en){
+            double mid = (st + en) / (double) 2;
+            int cnt = 0;
+            double a = getAns(squares,mid,cnt);
+            if(abs(a) <= ms){
+                ms = abs(a);
+                ans = mid;
+            }
+            if(a == 0 && cnt == 0){
+                ans = -1;
+                for(auto s : squares){
+                    double y = s[1] + s[2];
+                    if(y <= mid){
+                        ans = max(ans, y);
+                    }
+                }
+                break;
+            }
+            if(a > 0){
+                st = mid + 0.00001;
+            }else{
+                en = mid - 0.00001;
+            }
+        }
+        return ans;
     }
 };
