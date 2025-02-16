bee387ee6e1468d8b9807fb1e4e4d7f7865581f9
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-16 16:01:44 +0530

    Time: 1613 ms (11.11%) | Memory: 452.7 MB (11.11%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -4,16 +4,17 @@ public:
     int m;
     int dx[4] = {-1,-1,1,1};
     int dy[4] = {-1,1,1,-1};
-    int getAns(vector<vector<int>> &grid, int i, int j, int ch, int a){
+    int getAns(vector<vector<int>> &grid, int i, int j, int ch, int a,vector<vector<vector<vector<int>>>> &dp){
         int ans = 1;
+        if(dp[i][j][ch][a] != -1) return dp[i][j][ch][a];
         if(ch == 1){
             int nx = i + dx[(a + 1)%4];
             int ny = j + dy[(a + 1)%4];
             if(nx >= 0 && nx < n && ny >= 0 && ny < m){
                 if(grid[i][j] == 0 && grid[nx][ny] == 2){
-                    ans = max(ans, 1 + getAns(grid, nx, ny, 0, (a+1)%4));
+                    ans = max(ans, 1 + getAns(grid, nx, ny, 0, (a+1)%4, dp));
                 }else if(grid[i][j] == 2 && grid[nx][ny] == 0){
-                    ans = max(ans, 1 + getAns(grid, nx, ny, 0, (a+1)%4));
+                    ans = max(ans, 1 + getAns(grid, nx, ny, 0, (a+1)%4, dp));
                 }
             }
         }
@@ -21,18 +22,18 @@ public:
         int ny = j + dy[a];
         if(nx >= 0 && nx < n && ny >= 0 && ny < m){
             if(grid[i][j] == 0 && grid[nx][ny] == 2){
-                ans = max(ans, 1 + getAns(grid, nx, ny, ch, a));
+                ans = max(ans, 1 + getAns(grid, nx, ny, ch, a, dp));
             }else if(grid[i][j] == 2 && grid[nx][ny] == 0){
-                ans = max(ans, 1 + getAns(grid, nx, ny, ch, a));
+                ans = max(ans, 1 + getAns(grid, nx, ny, ch, a, dp));
             }
         }
-        return ans;
+        return dp[i][j][ch][a] = ans;
     }
 
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(m, vector<vector<int>>(2,vector<int>(4,-1))));
         int ans = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
@@ -42,7 +43,7 @@ public:
                         int nx = i + dx[a];
                         int ny = j + dy[a];
                         if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 2){
-                             ans = max(ans, 1 + getAns(grid, nx, ny, 1, a));
+                             ans = max(ans, 1 + getAns(grid, nx, ny, 1, a, dp));
                         }
                     }
                 }
