b702ae4d14bd830b20419341b5d687114bb651fe
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-16 16:20:21 +0530

    Time: 166 ms (36.67%) | Memory: 201.6 MB (16.67%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,52 +1,31 @@
 class Solution {
 public:
-    double getAns(vector<vector<int>> &s, double y, int &cnt){
-        double up = 0;
-        double lw = 0;
-        for(int i = 0; i < s.size(); i++){
-            double side = s[i][2];
-            if(side == 0) continue;
-            if(s[i][1] >= y){
-                up += side * side;
-            }else if((double)(s[i][1] + s[i][2]) <= y){
-                lw += side * side;
-            }else{
-                cnt++;
-                double su1 = (double)(s[i][1] + s[i][2]) - y;
-                double sl1 = y - (double)s[i][1];
-                up += side * su1;
-                lw += side * sl1;
-            }
-        }
-        return up - lw;
-    }
-
     double separateSquares(vector<vector<int>>& squares) {
-        int mx = 0;
-        int mn = INT_MAX;
+        double st = INT_MAX;
+        double en = 0;
         for(auto s : squares){
-            mn = min(mn, s[1]);
-            mx = max(mx, s[1] + s[2]);
+            st = min(st,(double) s[1]);
+            en = max(en, (double)s[1] + s[2]);
         }
-        double st = mn;
-        double en = mx;
-        double ms = INT_MAX;
-        double ans;
-        if(st == en) return st;
-        while(st < en){
-            double mid = (st + en) / (double) 2;
-            int cnt = 0;
-            double a = getAns(squares,mid,cnt);
-            if(abs(a) <= ms){
-                ms = abs(a);
-                ans = mid;
+        double eps = 1e-5;
+        while(en - st > eps){
+            double y = (st + en)/2;
+            double up = 0;
+            double lw = 0;
+            for(auto &s : squares){
+                double side = s[2];
+                double s1 = s[1];
+                if(s1 >= y){
+                    up += side * side;
+                }else if(s1 + side <= y){
+                    lw += side * side;
+                }else{
+                    up += side * ((s1 + side) - y);
+                    lw += side * (y - s1);
+                }
             }
-            if(a > 0){
-                st = mid + 0.00001;
-            }else{
-                en = mid - 0.00001;
-            }
+            up > lw ? st = y : en = y;
         }
-        return ans;
+        return st;
     }
 };
