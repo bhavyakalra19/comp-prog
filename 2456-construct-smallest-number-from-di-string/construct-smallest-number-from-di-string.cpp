e177073e8445aa7542867b5e55ac39379a0538da
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-18 17:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.27%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -3,13 +3,13 @@ public:
     string ans = "";
     void getAns(string s, int idx, string &pattern, vector<bool> &check, int prev){
         if(ans != "") return;
-        if(idx == pattern.size()){
+        if(idx == pattern.size() + 1){
             ans = s;
             return;
         }
         for(int i = 1; i < 10; i++){
             if(check[i]){
-                if((pattern[idx] == 'I' && i > prev) || (pattern[idx] == 'D' && i < prev)){
+                if(idx == 0 || (pattern[idx - 1] == 'I' && i > prev) || (pattern[idx - 1] == 'D' && i < prev)){
                     check[i] = false;
                     getAns(s + to_string(i), idx + 1, pattern, check, i);
                     check[i] = true;
@@ -20,11 +20,11 @@ public:
 
     string smallestNumber(string pattern) {
         vector<bool> check(10,true);
-        for(int i = 1; i < 10; i++){
-            check[i] = false;
-            getAns(to_string(i), 0, pattern, check, i);
-            check[i] = true;
-        }
+        // for(int i = 1; i < 10; i++){
+        //     check[i] = false;
+            getAns("", 0, pattern, check, -1);
+        //     check[i] = true;
+        // }
         return ans;
     }
 };
