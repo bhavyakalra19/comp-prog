b75ee0342a1a20bfa13f2de175ba9d702e3898c0
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-21 19:02:45 +0530

    Time: 87 ms (92.45%) | Memory: 13.7 MB (79.15%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n,0));
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = 0; j < n; j++){
+        vector<vector<int>> dp(n+2, vector<int>(n+2,0));
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n; j++){
                 if(i > j){
                     continue;
                 }
-                int a = (i - 1 < 0) ? 1 : nums[i-1];
-                int b = (j + 1 >= n) ? 1 : nums[j+1];
+                int a = (i - 2 < 0) ? 1 : nums[i-2];
+                int b = (j >= n) ? 1 : nums[j];
                 int ans = INT_MIN;
                 for(int k = i; k <= j; k++){
-                    if(k + 1 == n && k - 1 < 0){
-                        ans = max(ans, a * b * nums[k] + 0 + 0);
-                    }else if(k - 1 < 0){
-                        ans = max(ans, a * b * nums[k] + 0 + dp[k+1][j]);
-                    }else if(k + 1 == n){
-                        ans = max(ans, a * b * nums[k] + dp[i][k-1] + 0);
-                    }else{
-                        ans = max(ans, a * b * nums[k] + dp[i][k-1] + dp[k+1][j]);
-                    }
+                    ans = max(ans, a * b * nums[k-1] + dp[i][k-1] + dp[k+1][j]);
+
                 }
                 dp[i][j] = ans;
             }
         }
-        return dp[0][n-1];
+        return dp[1][n];
     }
 };
