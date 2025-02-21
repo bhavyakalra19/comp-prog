fda7267df010aa7324b855770ee7fa6ce5e5ea9c
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-21 18:50:59 +0530

    Time: 103 ms (68.35%) | Memory: 13.5 MB (90.21%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int n;
-    int getAns(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int ans = INT_MIN;
-        int a = (i - 1 < 0) ? 1 : nums[i-1];
-        int b = (j + 1 >= n) ? 1 : nums[j+1];
-        for(int k = i; k <= j; k++){
-            ans = max(ans, a * b * nums[k] + getAns(nums, i , k - 1, dp) + getAns(nums, k + 1, j, dp));
-        }
-        return dp[i][j] = ans;
-    }
-
     int maxCoins(vector<int>& nums) {
-        n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return getAns(nums, 0, n-1, dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = 0; j < n; j++){
+                if(i > j){
+                    continue;
+                }
+                int a = (i - 1 < 0) ? 1 : nums[i-1];
+                int b = (j + 1 >= n) ? 1 : nums[j+1];
+                int ans = INT_MIN;
+                for(int k = i; k <= j; k++){
+                    if(k + 1 == n && k - 1 < 0){
+                        ans = max(ans, a * b * nums[k] + 0 + 0);
+                    }else if(k - 1 < 0){
+                        ans = max(ans, a * b * nums[k] + 0 + dp[k+1][j]);
+                    }else if(k + 1 == n){
+                        ans = max(ans, a * b * nums[k] + dp[i][k-1] + 0);
+                    }else{
+                        ans = max(ans, a * b * nums[k] + dp[i][k-1] + dp[k+1][j]);
+                    }
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][n-1];
     }
 };
