842b1350cce03dc227acfb45da635cf3609cf9a7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-24 16:32:15 +0530

    Time: 1191 ms (9.09%) | Memory: 18 MB (100.00%) - LeetSync

--- 3774-check-if-digits-are-equal-in-string-after-operations-ii/check-if-digits-are-equal-in-string-after-operations-ii.cpp
+++ 3774-check-if-digits-are-equal-in-string-after-operations-ii/check-if-digits-are-equal-in-string-after-operations-ii.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
-    int binsmall(int n, int k) {
-        if (k > n)
-            return 0;
-        int fact[5] = {1, 1, 2, 1, 4};
-        int numerator = fact[n];
-        int denominator = (fact[k] * fact[n - k]) % 5;
-        int deninv = 0;
-        for (int i = 0; i < 5; i++) {
-            if ((denominator * i) % 5 == 1) {
-                deninv = i;
-                break;
-            }
-        }
-        return (numerator * deninv) % 5;
-    }
+    int nCrModpDP(int n, int r, int p) { 
+        // The array C is going to store last row of 
+        // pascal triangle at the end. And last entry 
+        // of last row is nCr 
+        int C[r+1]; 
+        memset(C, 0, sizeof(C)); 
+    
+        C[0] = 1; // Top row of Pascal Triangle 
+    
+        // One by constructs remaining rows of Pascal 
+        // Triangle from top to bottom 
+        for (int i = 1; i <= n; i++) 
+        { 
+            // Fill entries of current row using previous 
+            // row values 
+            for (int j = min(i, r); j > 0; j--) 
+    
+                // nCj = (n-1)Cj + (n-1)C(j-1); 
+                C[j] = (C[j] + C[j-1])%p; 
+        } 
+        return C[r]; 
+    } 
+    
+    int nCrModpLucas(int n, int r, int p) { 
+    // Base case 
+    if (r==0) 
+        return 1; 
+    // Compute last digits of n and r in base p 
+    int ni = n%p, ri = r%p; 
+    
+    // Compute result for last digits computed above, and 
+    // for remaining digits.  Multiply the two results and 
+    // compute the result of multiplication in modulo p. 
+    return (nCrModpLucas(n/p, r/p, p) * // Last digits of n and r 
+            nCrModpDP(ni, ri, p)) % p;  // Remaining digits 
+    } 
 
-    int binmod5(int n, int k) {
-        int res = 1;
-        while (n > 0 || k > 0) {
-            int nd = n % 5;
-            int kd = k % 5;
-            if (kd > nd)
-                return 0;
-            res = (res * binsmall(nd, kd)) % 5;
-            n /= 5;
-            k /= 5;
-        }
-        return res;
-    }
-    int binmod2(int n, int k) {
-        while (k > 0) {
-            if ((k & 1) > (n & 1))
-                return 0;
-            n >>= 1;
-            k >>= 1;
-        }
-        return 1;
-    }
     bool hasSameDigits(string s) {
         int n = s.size();
         long long a = 0;
         long long b = 0;
         for(int i = 0; i < n-1; i++){
-            int m2 = binmod2(n-2, i);
-            int m5 = binmod5(n-2, i);
+            int m2 = nCrModpLucas(n-2, i, 2);
+            int m5 = nCrModpLucas(n-2, i, 5);
             int result;
             for(int i = 0; i < 10; i++){
                 if(i % 2 == m2 && i % 5 == m5){
