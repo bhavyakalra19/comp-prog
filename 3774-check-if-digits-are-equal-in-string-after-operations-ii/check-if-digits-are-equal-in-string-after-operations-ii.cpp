0edcc274656c9abe86adccf23559e417f50fcd59
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-24 16:33:53 +0530

    Time: 1177 ms (9.09%) | Memory: 18 MB (100.00%) - LeetSync

--- 3774-check-if-digits-are-equal-in-string-after-operations-ii/check-if-digits-are-equal-in-string-after-operations-ii.cpp
+++ 3774-check-if-digits-are-equal-in-string-after-operations-ii/check-if-digits-are-equal-in-string-after-operations-ii.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int nCrModpDP(int n, int r, int p) { 
-        // The array C is going to store last row of 
-        // pascal triangle at the end. And last entry 
-        // of last row is nCr 
         int C[r+1]; 
         memset(C, 0, sizeof(C)); 
-    
         C[0] = 1; // Top row of Pascal Triangle 
-    
-        // One by constructs remaining rows of Pascal 
-        // Triangle from top to bottom 
-        for (int i = 1; i <= n; i++) 
-        { 
-            // Fill entries of current row using previous 
-            // row values 
+        for (int i = 1; i <= n; i++) { 
             for (int j = min(i, r); j > 0; j--) 
-    
-                // nCj = (n-1)Cj + (n-1)C(j-1); 
                 C[j] = (C[j] + C[j-1])%p; 
-        } 
-        return C[r]; 
-    } 
-    
-    int nCrModpLucas(int n, int r, int p) { 
-    // Base case 
-    if (r==0) 
-        return 1; 
-    // Compute last digits of n and r in base p 
-    int ni = n%p, ri = r%p; 
-    
-    // Compute result for last digits computed above, and 
-    // for remaining digits.  Multiply the two results and 
-    // compute the result of multiplication in modulo p. 
-    return (nCrModpLucas(n/p, r/p, p) * // Last digits of n and r 
-            nCrModpDP(ni, ri, p)) % p;  // Remaining digits 
+            } 
+            return C[r]; 
+        }
+        int nCrModpLucas(int n, int r, int p) { 
+        if (r==0) return 1; 
+        int ni = n%p, ri = r%p; 
+        return (nCrModpLucas(n/p, r/p, p) * nCrModpDP(ni, ri, p)) % p;
     } 
 
     bool hasSameDigits(string s) {
