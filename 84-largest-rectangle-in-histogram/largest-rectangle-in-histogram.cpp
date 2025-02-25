13e951d7cd143d652d467acfee7ebdd766340a02
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-25 16:44:05 +0530

    Time: 19 ms (75.31%) | Memory: 79.6 MB (88.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
+        int ans = 0;
         stack<int> st;
         int i = 0;
-        int mx = 0;
         while(i < n){
-            if(st.empty() || heights[i] >= heights[st.top()]){
+            if(st.empty() || heights[i] > heights[st.top()]){
                 st.push(i);
                 i++;
             }else{
                 int topIdx = st.top();
                 st.pop();
-                int width = st.empty() ? i : i - 1 - st.top();
-                mx = max(mx, width * heights[topIdx]);
+                int width = i - (st.empty() ? -1 : st.top()) - 1;
+                ans = max(ans, width * heights[topIdx]);
             }
         }
+        int prev = st.top();
         while(!st.empty()){
             int topIdx = st.top();
             st.pop();
-            int width = st.empty() ? i : i - 1 - st.top();
-            mx = max(mx, width * heights[topIdx]);
+            int width = prev - (st.empty() ? -1 : st.top());
+            ans = max(ans, width * heights[topIdx]);
         }
-        return mx;
+        return ans;
     }
 };
