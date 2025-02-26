4f7c80bd0674ced4b79deacc00f5c8e49f3c4196
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-26 12:32:29 +0530

    Time: 1446 ms (36.82%) | Memory: 291.5 MB (44.76%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -3,17 +3,12 @@ public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         unordered_map<int,int> mp;
         map<int,int> below;
-        vector<int> points;
         for(auto &it : nums){
             mp[it]++;
             below[it - k] += 1;
             below[it] = below[it];
             below[it + k + 1] -= 1;
-            // points.push_back(it - k);
-            // points.push_back(it + k + 1);
-            // points.push_back(it);
         }
-        // sort(points.begin(), points.end());
         int ans = 1;
         int sm = 0;
         for(auto &m : below){
