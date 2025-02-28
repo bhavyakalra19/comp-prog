f36b113ffb9ec787aee48b974cec58edb15521cd
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-02-28 20:02:56 +0530

    Time: 23 ms (87.82%) | Memory: 31 MB (10.69%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        int n1 = str1.size();
+        int n2 = str2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1,0));
+        for(int i = 0; i < n1; i++){
+            for(int j = 0; j < n2; j++){
                 if(str1[i] == str2[j]){
                     dp[i+1][j+1] = 1 + dp[i][j];
                 }else{
@@ -13,20 +14,22 @@ public:
                 }
             }
         }
-        int i = n;
-        int j = m;
+        int i = n1;
+        int j = n2;
         string s = "";
         while(i > 0 && j > 0){
             if(str1[i-1] == str2[j-1]){
                 s = str1[i-1] + s;
                 i--;
                 j--;
-            }else if(dp[i-1][j] > dp[i][j-1]){
-                s = str1[i-1] + s;
-                i--;
             }else{
-                s = str2[j-1] + s;
-                j--;
+                if(dp[i][j] == dp[i-1][j]){
+                    s = str1[i-1] + s;
+                    i--;
+                }else{
+                    s = str2[j-1] + s;
+                    j--;
+                }
             }
         }
         while(i){
