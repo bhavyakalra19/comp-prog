e4d6736b05185b68910331c4040c4fa543d95648
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-02 10:39:11 +0530

    Time: 1 ms (27.27%) | Memory: 182.1 MB (0.00%) - LeetSync

--- 3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
+++ 3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
@@ -4,9 +4,11 @@ public:
         int n = bounds.size();
         for(int i = 0; i < n-1; i++){
             int cost = original[i+1] - original[i];
-            if(bounds[i][0] + cost > bounds[i+1][1] || bounds[i][1] + cost < bounds[i+1][0]) return 0;
-            bounds[i+1][0] = max(bounds[i+1][0], bounds[i][0] + cost);
-            bounds[i+1][1] = min(bounds[i+1][1], bounds[i][1] + cost);
+            int a = bounds[i+1][0];
+            int b = bounds[i+1][1];
+            if(bounds[i][0] + cost > b || bounds[i][1] + cost < a) return 0;
+            bounds[i+1][0] = max(a, bounds[i][0] + cost);
+            bounds[i+1][1] = min(b, bounds[i][1] + cost);
         }
         return bounds[n-1][1] - bounds[n-1][0] + 1;
     }
