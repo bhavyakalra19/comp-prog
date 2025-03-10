1844833f1a2a380418b4b1cbc965d7823e60d8aa
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-10 15:44:24 +0530

    Time: 63 ms (97.81%) | Memory: 30.4 MB (96.71%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -8,38 +8,29 @@ public:
         int n = word.size();
         vector<int> check(21,0);
         long long ans = 0;
-        int vc = 0;
-        int cc = 0;
-        int i = 0;
-        int j = 0;
-        int m = 0;
+        int vc = 0, cc = 0, i = 0, j = 0, m = 0;
         while(i < n){
             while(i < n && (vc != 5 || cc != k)){
                 int a = word[i] - 'a';
                 if(a == 0 || a == 4 || a == 8 || a == 14 || a == 20){
                     check[a]++;
                     if(check[a] == 1) vc++;
-                }else{
-                    cc++;
-                }
+                }else cc++;
+
                 i++;
                 while(cc > k){
                     int a = word[j] - 'a';
                     if(a == 0 || a == 4 || a == 8 || a == 14 || a == 20){
                         check[a]--;
                         if(check[a] == 0) vc--;
-                    }else{
-                        cc--;
-                    }
+                    }else cc--;
                     j++;
                 }
             }
             if(cc == k && vc == 5){
                 if(m < i){
                     m = i;
-                    while(m < n && checkVowel(word[m] - 'a')){
-                        m++;
-                    }
+                    while(m < n && checkVowel(word[m] - 'a')) m++;
                 }
                 int cnt = m - i + 1;
                 while(vc == 5 && cc == k){
@@ -48,9 +39,7 @@ public:
                     if(a == 0 || a == 4 || a == 8 || a == 14 || a == 20){
                         check[a]--;
                         if(check[a] == 0) vc--;
-                    }else{
-                        cc--;
-                    }
+                    }else cc--;  
                     j++;
                 }
             }
@@ -60,11 +49,3 @@ public:
 };
 
 
-//1. i = 0 j = 0
-// increase i till unique vowels = 5 and const = k
-//  a. we find substring
-    //count vowels in front of founded substring and add it to answer including this
-    //increase j till unique vowels == 5 and const. == k and add above value to answer
-
-//  b. our k count is increased
-    //increase j till our const != k 
