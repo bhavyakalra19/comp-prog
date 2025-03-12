60b8cc8ac55f2341d29fe2a16d96155db7487c3b
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-13 00:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (67.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int m = s1.size();
-        int n = s2.size();
-        if(m > n){
-            return false;
+        int n = s1.size();
+        int m = s2.size();
+        if(n > m) return false;
+        vector<int> check1(26,0);
+        vector<int> check2(26,0);
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(auto &a : s1){
+            int c = a - 'a';
+            if(check1[c] == 0) cnt1++;
+            check1[c]++;
         }
-        vector<int> check(26,0);
-        vector<int> curr(26,0);
-        for(int i = 0; i < m; i++){
-            check[s1[i] - 'a']++;
-            curr[s2[i] - 'a']++;
+        for(int i = 0; i < n; i++){
+            int a = s2[i] - 'a';
+            check2[a]++;
+            if(check2[a] == check1[a]) cnt2++;
         }
-        for(int i = m; i < n; i++){
-            if(check == curr){
-                return true;
-            }
-            curr[s2[i - m] - 'a']--;
-            curr[s2[i] - 'a']++;
+        if(cnt1 == cnt2) return true;
+
+        for(int i = n; i < m; i++){
+            int a = s2[i] - 'a';
+            check2[a]++;
+            if(check2[a] == check1[a]) cnt2++;
+
+            a = s2[i - n] - 'a';
+            if(check2[a] == check1[a]) cnt2--;
+            check2[a]--;
+            if(cnt2 == cnt1) return true;
         }
-        return check == curr;
+        return false;
     }
 };
