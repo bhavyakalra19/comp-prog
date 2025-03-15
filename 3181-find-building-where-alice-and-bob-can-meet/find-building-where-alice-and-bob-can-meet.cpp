4e0239adf5f51e00417ba230a95e6bac20c91a62
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-15 17:40:04 +0530

    Time: 110 ms (63.66%) | Memory: 288.8 MB (33.33%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
         int n = heights.size();
+        vector<vector<pair<int,int>>> check(n);
         int q = queries.size();
         vector<int> result(q,-1);
-        vector<vector<pair<int,int>>> check(n);
         for(int i = 0; i < q; i++){
             int a = queries[i][0];
             int b = queries[i][1];
-            if(a > b){
-                swap(a,b);
-            }
+            if(a > b) swap(a,b);
+
             if(a == b || heights[a] < heights[b]){
                 result[i] = b;
             }else{
