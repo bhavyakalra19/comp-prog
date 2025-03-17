caae562bd08b86bd9c2f5f55091d757cb3f3976e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-17 21:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (68.43%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         int sz = n / 2;
         int oc = 0;
-        unordered_map<int,int> mp;
+        vector<int> mp(501,0);
         for(auto &a : nums){
-            if(mp.find(a) == mp.end() || mp[a] % 2 == 0){
+            if(mp[a] % 2 == 0){
                 oc++;
             }else{
                 oc--;
