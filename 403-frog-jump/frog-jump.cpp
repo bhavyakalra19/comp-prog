a1db939760dc1c5e03f9c7e69c0e452e855ba7dc
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-22 18:59:17 +0530

    Time: 240 ms (13.83%) | Memory: 414.3 MB (5.03%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int n;
+    int getAns(vector<int> &stones, int idx, int jump, vector<vector<int>> &dp){
+        if(idx == n-1) return true;
+        bool check = false;
+        if(dp[idx][jump] != -1) return dp[idx][jump];
+        
+        int i = idx;
+        int j1 = jump - 1;
+        if(j1 > 0){
+            while(i < n && stones[i] < (stones[idx] + j1)){
+                i++;
+            }
+            if(i < n && stones[i] == stones[idx] + j1){
+                check = check | getAns(stones, i, j1, dp);
+            }
+        }
+        int j2 = jump;
+        while(i < n && stones[i] < (stones[idx] + j2)){
+            i++;
+        }
+        if(i < n && stones[i] == stones[idx] + j2){
+            check = check | getAns(stones, i, j2, dp);
+        }
+
+    
+        int j3 = jump + 1;
+        while(i < n && (stones[i] < (stones[idx] + j3))){
+            i++;
+        }
+        if(i < n && (stones[i] == (stones[idx] + j3))){
+            check = check | getAns(stones, i, j3, dp);
+        }
+        return dp[idx][jump] = check;
+    }
+
+    bool canCross(vector<int>& stones) {
+        n = stones.size();
+        vector<vector<int>> dp(n, vector<int>(10000, -1));
+        if(stones[1] > 1) return false;
+        int a = getAns(stones, 1, 1, dp);
+        return a;
+    }
+};
