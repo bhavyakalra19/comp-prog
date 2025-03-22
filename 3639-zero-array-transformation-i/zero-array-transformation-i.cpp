afdcbf3b82d4f4e16f7bc9948c48a0d6011b863e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-22 12:14:47 +0530

    Time: 7 ms (66.85%) | Memory: 292.9 MB (69.32%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> line(n+1,0);
-        for(auto q : queries){
-            line[q[0]] += 1;
-            line[q[1] + 1] -= 1;
+        vector<int> cnt(n+1,0);
+        for(auto &q: queries){
+            cnt[q[0]]++;
+            cnt[q[1] + 1]--;
         }
-        int s = 0;
+        int sm = 0;
         for(int i = 0; i < n; i++){
-            s += line[i];
-            nums[i] -= s;
-            if(nums[i] > 0){
-                return false;
-            }
+            sm += cnt[i];
+            if(sm < nums[i]) return false; 
         }
         return true;
     }
