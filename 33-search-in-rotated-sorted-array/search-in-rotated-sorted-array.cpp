39265944c07457f46a8a29cf35199e004d5ffc86
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-25 00:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.37%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,17 +5,17 @@ public:
         int st = 0;
         int en = n-1;
         while(st <= en){
-            int mid = (st + en)/2;
+            int mid = st + (en - st) / 2;
             if(nums[mid] == target){
                 return mid;
-            }if(nums[mid] >= nums[st]){
+            }else if(nums[mid] >= nums[st]){
                 if(target >= nums[st] && target < nums[mid]){
-                    en = mid-1;
+                    en = mid - 1;
                 }else{
-                    st = mid+1;
+                    st = mid + 1;
                 }
             }else{
-                if(target <= nums[en] && target >= nums[mid]){
+                if(target <= nums[en] && target > nums[mid]){
                     st = mid + 1;
                 }else{
                     en = mid - 1;
@@ -25,3 +25,4 @@ public:
         return -1;
     }
 };
+
