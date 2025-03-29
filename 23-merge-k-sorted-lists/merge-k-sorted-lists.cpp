216457ddd83b05951655c3634ad549f3c4db055f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-03-29 13:36:51 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (40.48%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,35 +11,20 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
         int n = lists.size();
-        ListNode *temp = NULL;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        for(int i= 0; i < n; i++){
-            if(lists[i]){
-                pq.push({lists[i]->val, i});
-            }
+        for(int i = 0; i < n; i++){
+            if(lists[i] != NULL) pq.push({lists[i]->val, lists[i]});
         }
-        if(pq.empty()){
-            return temp;
-        }
-        auto it = pq.top();
-        pq.pop();
-        temp = lists[it.second];
-        lists[it.second] = lists[it.second]->next;
-        if(lists[it.second] != NULL){
-            pq.push({lists[it.second]->val,it.second});
-        }
+        ListNode *temp = new ListNode(-1);
         ListNode *head = temp;
         while(!pq.empty()){
-            auto it = pq.top();
+            ListNode *n = pq.top().second;
             pq.pop();
-            temp->next = lists[it.second];
+            temp->next = n;
             temp = temp->next;
-            lists[it.second] = lists[it.second]->next;
-            if(lists[it.second] != NULL){
-                pq.push({lists[it.second]->val,it.second});
-            }
+            if(n->next) pq.push({n->next->val, n->next});
         }
-        return head;
+        return head->next;
     }
 };
