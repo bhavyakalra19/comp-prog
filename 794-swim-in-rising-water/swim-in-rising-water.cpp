185d9f213f55d1268b3b0fd952e3f5afb5392cbd
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-05 01:09:52 +0530

    Time: 3 ms (88.85%) | Memory: 12.8 MB (81.11%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -4,27 +4,26 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        vector<vector<int>> check(n, vector<int>(m,INT_MAX));
         pq.push({grid[0][0], {0,0}});
-        int dx[4] = {1,0,-1,0};
-        int dy[4] = {0,-1,0,1};
-        vector<vector<bool>> visit(n,vector<bool>(m,0));
-        visit[0][0] = true;
+        int dx[4] = {0,1,0,-1};
+        int dy[4] = {1,0,-1,0};
+        check[0][0] = grid[0][0];
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
+            int cw = it.first;
             int cx = it.second.first;
             int cy = it.second.second;
-            int cw = it.first;
             for(int i = 0; i < 4; i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx == n-1 && ny == m-1) return max(cw, grid[nx][ny]);
-                if(nx < n && nx >= 0 && ny < m && ny >= 0 && !visit[nx][ny]){
-                    visit[nx][ny] = true;
-                    pq.push({max(cw, grid[nx][ny]), {nx, ny}});
+                if(nx >= 0 && nx < n && ny >= 0 & ny < m && max(cw, grid[nx][ny]) < check[nx][ny]){
+                    check[nx][ny] = max(cw, grid[nx][ny]);
+                    pq.push({check[nx][ny], {nx, ny}});
                 }
             }
         }
-        return 0;
+        return check[n-1][m-1];
     }
 };
