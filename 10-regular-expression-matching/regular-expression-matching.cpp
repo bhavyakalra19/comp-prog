d1d53cd6632dd687b2a08cbb86d93e4366c80607
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-04 22:52:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (30.26%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    int n;
-    int m;
-    bool getAns(string s, string p, int i, int j, vector<vector<int>> &dp){
-        if(i < 0 && j < m){
-            return true;
-        }
-        if(i < 0 && p[j] == '*'){
-            return getAns(s,p,i,j-2,dp);
-        }
-        if(i < 0 || j < m){
-            return false;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(p[j] == '*'){
-            bool a = getAns(s,p,i,j-2,dp);
-            if(s[i] == p[j-1] || p[j-1] == '.'){
-                a = getAns(s,p,i-1,j,dp) || a;
+    bool getAns(string &s, string &p, int i, int j, vector<vector<int>> &dp){
+        if(i < 0){
+            while(j >= 0 && p[j] == '*'){
+                j -= 2;
             }
-            return dp[i][j] = a;
-        }else if(s[i] == p[j] || p[j] == '.'){
-            return dp[i][j] = getAns(s,p,i-1,j-1,dp);
+            return j < 0;
         }
-        return false;
+        if(j < 0) return false;
+        if(dp[i][j] != -1) return dp[i][j];
+        bool a = false;
+        if(s[i] == p[j] || p[j] == '.'){
+            a = getAns(s, p, i-1, j-1, dp);
+        }else if(p[j] == '*'){
+            a = a | getAns(s, p, i, j-2, dp);
+            if(p[j-1] == s[i] || p[j-1] == '.'){
+                a = a | getAns(s, p, i-1, j, dp);
+            }
+        }
+        return dp[i][j] = a;
     }
 
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return getAns(s,p,n-1,m-1,dp);
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        return getAns(s, p, n-1, m-1, dp);
     }
 };
