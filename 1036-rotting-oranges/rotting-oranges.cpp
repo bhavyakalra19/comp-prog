531bd92dfb9d79a829be215d157ede85d7d30135
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-05 00:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (66.78%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,39 +3,40 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int dx[4] = {0, -1, 0, 1};
-        int dy[4] = {1, 0, -1, 0};
-        int ans = 0;
-        int cnt = 0;
         queue<pair<int,int>> q;
+        int cnt = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2) q.push({i,j});
-                if(grid[i][j] == 1) cnt += 1;
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                }else if(grid[i][j] == 1){
+                    cnt++;
+                }
             }
         }
         if(cnt == 0) return 0;
+        int ans = 0;
+        int dx[4] = {1,0,-1,0};
+        int dy[4] = {0,-1,0,1};
         while(!q.empty()){
-            ans++;
             int size = q.size();
+            ans++;
             while(size--){
                 auto it = q.front();
                 q.pop();
                 int cx = it.first;
                 int cy = it.second;
-                grid[cx][cy] = 0;
                 for(int i = 0; i < 4; i++){
                     int nx = cx + dx[i];
                     int ny = cy + dy[i];
-                    if(nx < n && nx >= 0 && ny < m && ny >= 0 && grid[nx][ny] == 1){
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1){
                         cnt--;
-                        if(cnt == 0) return ans;
-                        grid[nx][ny] = 0;
-                        q.push({nx,ny});
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
                     }
                 }
             }
         }
-        return -1;
+        return cnt == 0 ? ans - 1 : -1;
     }
 };
