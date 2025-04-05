0fa469eaeb64c4a0fe4e53b26060c60dd7265336
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-05 15:16:10 +0530

    Time: 3 ms (91.19%) | Memory: 10.6 MB (78.93%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.cpp
+++ 787-sliding-puzzle/sliding-puzzle.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     int slidingPuzzle(vector<vector<int>>& board) {
-        unordered_map<string,int> mp;
-        string br = "";
-        string ans = "123450";
-        queue<string> q;
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 3; j++){
-                br += to_string(board[i][j]);
+        vector<vector<int>> gp{{1,3}, {0,2,4}, {1,5}, {0,4}, {1,3,5}, {2, 4}};
+        unordered_set<string> mp;
+        string main = "123450";
+        string check = "";
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                check += to_string(board[i][j]);
             }
         }
-        mp[br] = 1;
-        vector<vector<int>> steps{{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{2,4}};
-        q.push(br);
-        int step = 0;
+        if(check == main) return 0;
+        queue<string> q;
+        q.push(check);
+        int ans = 0;
         while(!q.empty()){
             int size = q.size();
+            ans++;
             while(size--){
                 auto it = q.front();
                 q.pop();
-                if(it == ans){
-                    return step;
-                }
-                int ch = 0;
-                while(it[ch] != '0'){
-                    ch++;
-                }
-                for(auto a : steps[ch]){
-                    string next = it;
-                    swap(next[ch],next[a]);
-                    if(mp.find(next) == mp.end()){
-                        mp[next] = 1;
-                        q.push(next);
+                int idx = 0;
+                while(it[idx] != '0') idx++;
+                for(auto &a : gp[idx]){
+                    string str = it;
+                    swap(str[idx], str[a]);
+                    if(str == main) return ans;
+                    if(mp.find(str) == mp.end()){
+                        q.push(str);
+                        mp.insert(str);
                     }
                 }
             }
-            step++;
         }
         return -1;
     }
