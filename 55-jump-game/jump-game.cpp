f1b35f63cf216fed1b1b4ff13f85be242a6caf89
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-08 14:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (64.00%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums.size();
         int i = 0;
         int mx = 0;
-        if(n == 1) return true;
         while(i < n - 1){
             mx = max(mx, i + nums[i]);
             if(mx <= i) return false;
