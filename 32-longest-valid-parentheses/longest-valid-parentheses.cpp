f24ad426d262ad7ee830e4dcba5ebe78f1498a3f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-11 17:39:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (82.13%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        int mx = 0;
         int n = s.size();
+        stack<int> st;
+        int ans = 0;
         for(int i = 0; i < n; i++){
             if(s[i] == '('){
-                st.push(1);
+                st.push(i);
             }else{
-                if(!st.empty() && st.top() == 1){
+                if(!st.empty() && s[st.top()] == '('){
                     st.pop();
-                    st.push(2);
-                }else if(!st.empty() && st.top() > 1){
-                   auto it = st.top();
-                   st.pop();
-                   if(!st.empty() && st.top() == 1){
-                    st.pop();
-                    st.push(it + 2);
-                   }else{
-                    st.push(-1);
-                   }
+                    int dist = i - (st.empty() ? -1 : st.top());
+                    ans = max(ans, dist);
                 }else{
-                    st.push(-1);
-                }
-                if(st.top() > 1){
-                    auto it = st.top();
-                    st.pop();
-                    if(!st.empty() && st.top() > 1){
-                        auto its = st.top();
-                        st.pop();
-                        st.push(it + its);
-                    }else{
-                        st.push(it);
-                    }
+                    st.push(i);
                 }
             }
-            mx = max(mx,st.top());
         }
-        return mx == 1 ? 0 : mx;
+        return ans;
     }
 };
