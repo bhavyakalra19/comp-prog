f3b241c4b855b963a268f6ef5bb2a24a02bf661e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-12 22:31:53 +0530

    Time: 3 ms (65.14%) | Memory: 10.1 MB (92.69%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    bool isValid(vector<string> &check, int a, int b, int &n){
-        //check col
-        for(int i = 0; i < a; i++) if(check[i][b] == 'Q') return false;
-        
-        //check diagnal1
-        int i = a;
-        int j = b;
+    bool validate(vector<string> &check, int r, int c, int &n){
+        //check upper row
+        for(int i = 0; i < r; i++){
+            if(check[i][c] == 'Q'){
+                return false;
+            }
+        }
+        //check left diagnol
+        int i = r;
+        int j = c;
         while(i >= 0 && j >= 0){
             if(check[i][j] == 'Q') return false;
             i--;
             j--;
         }
-        //check diagnol2
-        i = a;
-        j = b;
+        //check right diagnol
+        i = r;
+        j = c;
         while(i >= 0 && j < n){
             if(check[i][j] == 'Q') return false;
             i--;
@@ -23,27 +26,25 @@ public:
         return true;
     }
 
-
-    void getAns(vector<string> &check, int idx, int &n, vector<vector<string>> &ans){
+    void getAns(vector<string> &check, int idx, vector<vector<string>> &ans, int &n){
         if(idx == n){
             ans.push_back(check);
             return;
         }
         for(int i = 0; i < n; i++){
-            if(isValid(check, idx, i, n)){
+            if(validate(check, idx, i, n)){
                 check[idx][i] = 'Q';
-                getAns(check, idx + 1, n, ans);
+                getAns(check, idx + 1, ans, n);
                 check[idx][i] = '.';
             }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
+        string st(n,'.');
+        vector<string> check(n, st);
         vector<vector<string>> ans;
-        string s = "";
-        for(int i = 0; i < n; i++) s += '.';
-        vector<string> check(n,s);
-        getAns(check, 0, n, ans);
+        getAns(check, 0, ans, n);
         return ans;
     }
 };
