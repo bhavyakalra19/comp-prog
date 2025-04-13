f79d4ab8b2122321eccd7734d29ba49ea105ed70
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-13 17:04:37 +0530

    Time: 95 ms (66.71%) | Memory: 288.5 MB (33.42%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
         int n = heights.size();
-        vector<vector<pair<int,int>>> check(n);
         int q = queries.size();
-        vector<int> result(q,-1);
+        vector<int> ans(q,-1);
+        vector<vector<pair<int,int>>> check(n);
         for(int i = 0; i < q; i++){
             int a = queries[i][0];
             int b = queries[i][1];
             if(a > b) swap(a,b);
 
-            if(a == b || heights[a] < heights[b]){
-                result[i] = b;
+            if(a == b || heights[b] > heights[a]){
+                ans[i] = b;
             }else{
                 check[b].push_back({heights[a], i});
             }
         }
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         for(int i = 0; i < n; i++){
-            for(auto a : check[i]) pq.push(a);
+            for(auto &a : check[i]){
+                pq.push(a);
+            }
             while(!pq.empty() && pq.top().first < heights[i]){
-                result[pq.top().second] = i;
+                ans[pq.top().second] = i;
                 pq.pop();
             }
         }
-        return result;
+        return ans;
     }
 };
