9952d4d9e44a5857d58dbb4b08a2f394737127d7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-16 22:30:11 +0530

    Time: 23 ms (84.57%) | Memory: 30.7 MB (99.15%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -13,24 +13,24 @@ public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         string ans = "";
-        queue<TreeNode*> q;
         if(!root) return ans;
+        queue<TreeNode*> q;
         q.push(root);
-        ans += to_string(root->val) + " ";
+        ans += to_string(root->val);
         while(!q.empty()){
             root = q.front();
             q.pop();
             if(root->left){
+                ans += " " + to_string(root->left->val);
                 q.push(root->left);
-                ans += to_string(root->left->val) + " ";
             }else{
-                ans += "# ";
+                ans += " #";
             }
             if(root->right){
+                ans += " " + to_string(root->right->val);
                 q.push(root->right);
-                ans += to_string(root->right->val) + " ";
             }else{
-                ans += "# ";
+                ans += " #";
             }
         }
         return ans;
@@ -38,69 +38,68 @@ public:
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        queue<TreeNode*> q;
-        int n = data.size();
-        if(n <= 1) return NULL;
+        if(data.size() == 0) return NULL;
+        
         int i = 0;
+        int n = data.size();
         int sum = 0;
-        int mul = 1;
+        int sign = 1;
         if(data[i] == '-'){
-            mul = -1;
+            sign = -1;
             i++;
         }
         while(i < n && data[i] != ' '){
             sum *= 10;
-            sum += data[i] - '0';
+            sum += (data[i] - '0');
             i++;
         }
-        TreeNode *temp = new TreeNode(mul * sum);
         i++;
-        TreeNode *head = temp;
-        q.push(temp);
-        while(i < n && !q.empty()){
+        TreeNode *root = new TreeNode(sum * sign);
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode *temp;
+        while(i < n){
             temp = q.front();
             q.pop();
             if(data[i] == '#'){
                 i += 2;
             }else{
                 int sum = 0;
-                int mul = 1;
+                int sign = 1;
                 if(data[i] == '-'){
-                    mul = -1;
+                    sign = -1;
                     i++;
                 }
                 while(i < n && data[i] != ' '){
                     sum *= 10;
-                    sum += data[i] - '0';
+                    sum += (data[i] - '0');
                     i++;
                 }
-                TreeNode *t = new TreeNode(mul * sum);
-                temp->left = t;
-                q.push(t);
                 i++;
+                temp->left = new TreeNode(sum * sign);
+                q.push(temp->left);
             }
 
             if(data[i] == '#'){
                 i += 2;
             }else{
                 int sum = 0;
-                int mul = 1;
+                int sign = 1;
                 if(data[i] == '-'){
-                    mul = -1;
+                    sign = -1;
                     i++;
                 }
                 while(i < n && data[i] != ' '){
                     sum *= 10;
-                    sum += data[i] - '0';
+                    sum += (data[i] - '0');
                     i++;
                 }
-                TreeNode *t = new TreeNode(mul * sum);
-                temp->right = t;
-                q.push(t);
                 i++;
+                temp->right = new TreeNode(sum * sign);
+                q.push(temp->right);
             }
         }
-        return head;
+        return root;
     }
 };
 
