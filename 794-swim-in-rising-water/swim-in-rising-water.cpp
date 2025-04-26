71f4f382ceeb11812c4ae996fb396cd745fd00a2
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-26 14:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (87.75%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = grid[0].size();
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        vector<vector<int>> check(n, vector<int>(m,INT_MAX));
-        pq.push({grid[0][0], {0,0}});
-        int dx[4] = {0,1,0,-1};
-        int dy[4] = {1,0,-1,0};
+        vector<vector<int>> check(n, vector<int>(n, INT_MAX));
         check[0][0] = grid[0][0];
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+        pq.push({grid[0][0],{0,0}});
+        int dx[4] = {0,-1,0,1};
+        int dy[4] = {1,0,-1,0};
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
             int cw = it.first;
             int cx = it.second.first;
             int cy = it.second.second;
+            if(cx == n-1 && cy == n-1) return cw;
             for(int i = 0; i < 4; i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 & ny < m && max(cw, grid[nx][ny]) < check[nx][ny]){
-                    check[nx][ny] = max(cw, grid[nx][ny]);
-                    pq.push({check[nx][ny], {nx, ny}});
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n && max(cw, grid[nx][ny]) < check[nx][ny]){
+                    int nw = max(cw, grid[nx][ny]);
+                    check[nx][ny] = nw;
+                    pq.push({nw, {nx, ny}});
                 }
             }
         }
-        return check[n-1][m-1];
+        return -1;
     }
 };
