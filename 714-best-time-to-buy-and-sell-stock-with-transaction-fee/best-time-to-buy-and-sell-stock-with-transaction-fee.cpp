ab45b5db7310fbe45caa22f064353850597e8459
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-27 17:35:50 +0530

    Time: 94 ms (51.77%) | Memory: 81.1 MB (57.18%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int getAns(vector<int> &prices, int idx, int &fee, int buy, vector<vector<int>> &dp){
-        if(idx == prices.size()){
-            return 0;
-        }
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-        if(buy == 1){
-            return dp[idx][buy] = max(getAns(prices, idx + 1, fee, buy, dp), getAns(prices, idx + 1, fee, 0, dp) - prices[idx] - fee);
-        }
-        return dp[idx][buy] = max(getAns(prices, idx + 1, fee, buy, dp), prices[idx] + getAns(prices, idx + 1, fee, 1, dp));
-    }
 
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return getAns(prices, 0, fee, 1, dp);
+        vector<int> prev(2, 0);
+        for(int idx = n-1; idx >= 0; idx--){
+            vector<int> curr(2, 0);
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy == 1){
+                    curr[buy] = max(prev[buy], prev[0] - prices[idx] - fee);
+                }else{
+                    curr[buy] = max(prev[buy], prices[idx] + prev[1]);
+                }
+            }
+            prev = curr;
+        }
+        return prev[1];
     }
 };
