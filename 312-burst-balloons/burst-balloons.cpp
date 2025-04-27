9afdcef02727c904c0a6d4efe65e1c732aed50a7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-04-27 23:02:37 +0530

    Time: 188 ms (48.89%) | Memory: 13.7 MB (89.65%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int getAns(vector<int> &nums, int st, int en, vector<vector<int>> &dp){
+    int getAns(vector<int> &nums, int st, int en, vector<vector<int>>&dp){
         if(st > en){
             return 0;
         }
         if(dp[st][en] != -1) return dp[st][en];
-        int a = st - 1 < 0 ? 1 : nums[st-1];
-        int b = en + 1 == nums.size() ? 1 : nums[en + 1];
-        int ans = INT_MIN;
+        int left = (st - 1) < 0 ? 1 : nums[st-1];
+        int right = (en + 1) == nums.size() ? 1 : nums[en + 1];
+        int ans = 0;
         for(int i = st; i <= en; i++){
-            ans = max(ans, (a * b * nums[i]) + getAns(nums, st, i - 1, dp) + getAns(nums, i + 1, en, dp));
+            ans = max(ans, nums[i] * left * right + getAns(nums, st, i - 1, dp) + getAns(nums, i + 1, en, dp));
         }
         return dp[st][en] = ans;
     }
 
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         return getAns(nums, 0, n-1, dp);
     }
 };
