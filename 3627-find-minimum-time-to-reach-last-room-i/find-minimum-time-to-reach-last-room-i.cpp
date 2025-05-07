bc0b3c321d57400920e634e0e2dd4c855e09a51e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-05-07 13:54:27 +0530

    Time: 19 ms (76.17%) | Memory: 28.7 MB (89.51%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int m = moveTime.size();
-        int n = moveTime[0].size();
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        vector<vector<int>> check(n, vector<int>(m, INT_MAX));
+        check[0][0] = 0;
+        int dx[4] = {0,1,0,-1};
+        int dy[4] = {-1,0,1,0};
         priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
-        vector<vector<int>> check(m,vector<int>(n,INT_MAX));
-        int dx[4] = {-1,0,1,0};
-        int dy[4] = {0,1,0,-1};
         pq.push({0, {0,0}});
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
+            int cw = it.first;
             int cx = it.second.first;
             int cy = it.second.second;
-            if(cx == m-1 && cy == n-1){
-                return it.first;
-            }
-            int cw = it.first;
             for(int i = 0; i < 4; i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx >= m || nx < 0 || ny >= n || ny < 0){
-                    continue;
-                }
-                int nw = max(cw, moveTime[nx][ny]) + 1;
-                if(nw < check[nx][ny]){
-                    check[nx][ny] = nw;
-                    pq.push({nw,{nx,ny}});
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m){
+                    int nw = moveTime[nx][ny];
+                    if((max(nw, cw) + 1) < check[nx][ny]){
+                        check[nx][ny] = max(nw, cw) + 1;
+                        pq.push({check[nx][ny],{nx, ny}});
+                    }
                 }
             }
         }
-        return -1;
+        return check[n-1][m-1];
     }
 };
