d27559823a6dd8ab4917ff2056c879b6423b35b6
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-05-20 10:14:42 +0530

    Time: 153 ms (10.46%) | Memory: 325.6 MB (11.30%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> cnt(n+1,0);
-        for(auto &q: queries){
-            cnt[q[0]]++;
-            cnt[q[1] + 1]--;
+        vector<int> check(n+1, 0);
+        for(auto q : queries){
+            check[q[0]]--;
+            check[q[1] + 1]++;
         }
-        int sm = 0;
+        int a = 0;
         for(int i = 0; i < n; i++){
-            sm += cnt[i];
-            if(sm < nums[i]) return false; 
+            a += check[i];
+            if(a + nums[i] > 0) return false;
         }
         return true;
     }
