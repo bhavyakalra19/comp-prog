f2df62ac3a5587f2a6ca875fd89006ba45f152b7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-05-22 11:59:18 +0530

    Time: 107 ms (81.44%) | Memory: 224.3 MB (64.82%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        int q = queries.size();
         sort(queries.begin(), queries.end());
         priority_queue<int> pq;
         int j = 0;
-        int sm = 0;
-        int ans = 0;
+        int q = queries.size();
+        int sum = 0;
         vector<int> check(n+1, 0);
         for(int i = 0; i < n; i++){
-            sm -= check[i];
             while(j < q && queries[j][0] == i){
-                pq.push(queries[j][1]);
-                j++;
+                pq.push(queries[j++][1]);
             }
-            while(sm < nums[i]){
-                if(pq.empty() || pq.top() < i) return -1;
-                int top = pq.top();
-                pq.pop();
-                sm++;
-                ans++;
-                check[top + 1]++;
+            while(sum < nums[i]){
+                if(pq.empty() || pq.top() < i){
+                    return -1;
+                }else{
+                    sum++;
+                    int top = pq.top();
+                    check[top]++;
+                    pq.pop();
+                }
             }
+            sum -= check[i];
         }
         return pq.size();
     }
