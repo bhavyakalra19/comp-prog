6a7480e8fd7e053bd879221dc0e8fd5f79d63b83
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-05-31 12:01:22 +0530

    Time: 99 ms (95.48%) | Memory: 59.6 MB (65.58%) - LeetSync

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> buildList(const vector<vector<int>>& edges) {
-        vector<vector<int>> adj(edges.size() + 1);
-        for (auto &e : edges) {
+    void createGraph(vector<vector<int>> &edges, vector<vector<int>> &adj){
+        for(auto &e : edges){
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
-        return adj;
     }
-    
-    int dfs(const vector<vector<int>>& adj, int u, int p, int k) {
-        if (k < 0) return 0;
-        int cnt = 1;
-        for (int v : adj[u])
-            if (v != p) cnt += dfs(adj, v, u, k-1);
-        return cnt;
-    }
-    
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        auto adj1 = buildList(edges1), adj2 = buildList(edges2);
-        int n = adj1.size(), m = adj2.size(), maxiB = 0;
-        vector<int> res(n);
 
-        for (int i = 0; i < m; i++) maxiB = max(maxiB, dfs(adj2, i, -1, k - 1));
-        for (int i = 0; i < n; i++) res[i] = dfs(adj1, i, -1, k) + maxiB;
-        return res;
+    int dfs(vector<vector<int>> &adj, int par, int curr, int k){
+        if(k == 0) return 1;
+        int a = 1;
+        for(auto &c : adj[curr]){
+            if(c != par){
+                a += dfs(adj, curr, c, k-1);
+            }
+        }
+        return a;
+    }
+
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        int n = edges1.size() + 1;
+        int m = edges2.size() + 1;
+        vector<vector<int>> adj1(n);
+        vector<vector<int>> adj2(m);
+        createGraph(edges1, adj1);
+        createGraph(edges2, adj2);
+        vector<int> ans;
+        int mx = 0;
+        if(k > 0){
+            for(int i = 0; i < m; i++){
+                mx = max(mx, dfs(adj2, i, i, k-1));
+            }
+        }
+        for(int i = 0; i < n; i++){
+            ans.push_back(dfs(adj1, i, i, k) + mx);
+        }
+        return ans;
     }
 };
