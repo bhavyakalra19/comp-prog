89bbeb58687d96f844318468d46118395d009fb7
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-06-07 18:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (40.96%) - LeetSync

--- 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     int n;
-    int mx;
-    // bool getAns(long long curr, vector<int> &nums, long long &target, long long check){
-    //     if(curr == target){
-    //         long long rem = 1;
-    //         for(int i = 0; i < n; i++){
-    //             if(((check >> i) & 1) != 1){
-    //                 rem *= nums[i];
-    //             }
-    //         }
-    //         return rem == target;
-    //     }
-    //     if(check == mx || curr > target) return false;
-    //     for(int i = 0; i < n; i++){
-    //         if(!((check >> i) & 1)){
-    //             if(getAns(curr * nums[i], nums, target, (check | (1 << i)))) return true;
-    //         }
-    //     }
-    //     return false;
-    // }
 
     bool getAns(long long curr, long long rem, long long target, int idx, vector<int> &nums){
         if(curr == rem && rem == target && idx == n) return true;
@@ -31,7 +12,6 @@ public:
 
     bool checkEqualPartitions(vector<int>& nums, long long target) {
         n = nums.size();
-        mx = (1 << n) - 1;
         long long curr = 1;
         long long check = 0;
         return getAns(1, 1, target, 0, nums);
