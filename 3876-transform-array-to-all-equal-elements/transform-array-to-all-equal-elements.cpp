926edc3c8f0f9520d492863597588088ad1bc6c8
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-06-10 13:35:25 +0530

    Time: 16 ms (44.95%) | Memory: 167.1 MB (6.21%) - LeetSync

---
+++ 3876-transform-array-to-all-equal-elements/transform-array-to-all-equal-elements.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool checkEqual(vector<int> &nums, int k){
+        int ans = 0;
+        for(int i = 0; i < nums.size() - 1; i += 2){
+            ans += nums[i+1] - nums[i];
+        }
+        return k >= ans;
+    }
+
+    bool canMakeEqual(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> pos;
+        vector<int> neg;
+        int ps = 0;
+        int ns = 0;
+         for(int i = 0; i < n; i++){
+            if(nums[i] == 1){
+                pos.push_back(i);
+                ps++;
+            }else{
+                neg.push_back(i);
+                ns++;
+            }
+        }
+        if(ps == n || ns == n) return true;
+        if((ps % 2 != 0) && (ns % 2 != 0)) return false;
+        if((ps % 2 == 0) && (ns % 2 == 0)){
+            return checkEqual(neg, k) | checkEqual(pos, k);
+        }else if(ps % 2 == 0){
+            return checkEqual(pos, k);
+        }
+        return checkEqual(neg, k);
+    }
+};
+
+//if both odd return false
+//if one is odd go for another one 
+// if both is even check for both
+
+
+// -1 1 1 1 1 1 -1
+
+// -1 1 1 1 1 -1 check for both 1 and -1
