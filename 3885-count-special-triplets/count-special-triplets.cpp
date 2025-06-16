c9a2573dcb7ff7f444991111cfa70cd6c132ef24
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-06-16 17:01:03 +0530

    Time: 767 ms (63.64%) | Memory: 403.1 MB (65.85%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> count;
+        unordered_map<int,int> pairs;
+        int ans = 0;
+        int zc = 0;
+        int za = 0;
+        int mod = 1e9 + 7;
+        for(int i = 0; i < n; i++){
+            // if(nums[i] == 0){
+            //     zc++;
+            //     if(zc > 2){
+            //         za++;
+            //         za = za % mod;
+            //     }
+            //     ans = (ans + za) % mod;
+            // }else{
+                if(nums[i] % 2 == 0){
+                    ans = (ans + pairs[nums[i] / 2]) % mod;
+                }
+                pairs[nums[i]] = (pairs[nums[i]] + count[nums[i] * 2]) % mod;
+                count[nums[i]]++;
+            // }
+        }
+        return ans;
+    }
+};
+
+// 0 0 0 0 0 0 0
