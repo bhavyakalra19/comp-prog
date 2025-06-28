5bc5e8f5a95fac8e5e9563f01a26c4abf10ddada
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-06-28 12:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (43.60%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
         int n = nums.size();
         vector<pair<int,int>> check;
-        for(int i = 0; i < n; i++){
-            check.push_back({nums[i], i});
-        }
+        for(int i = 0; i < n; i++) check.push_back({nums[i], i});
         sort(check.begin(), check.end(), [](const pair<int,int> &a, const pair<int,int> &b){
             return a.first > b.first;
         });
@@ -13,9 +11,7 @@ public:
             return a.second < b.second;
         });
         vector<int> ans;
-        for(int i = 0; i < k; i++){
-            ans.push_back(check[i].first);
-        }
+        for(int i = 0; i < k; i++) ans.push_back(check[i].first);
         return ans;
     }
 };
