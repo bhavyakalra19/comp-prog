170cfa5774ca677284bc4bd44380aa2cfa964fb9
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-07-09 10:11:34 +0530

    Time: 788 ms (7.38%) | Memory: 125.7 MB (51.19%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -3,23 +3,29 @@ public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
         int n = startTime.size();
         vector<int> check;
-        check.push_back(startTime[0] - 0);
-        for(int i = 1; i < n; i++){
-            check.push_back(startTime[i] - endTime[i-1]);
+        int prev = 0;
+        for(int i = 0; i < n; i++){
+            check.push_back(startTime[i] - prev);
+            prev = endTime[i];
         }
-        if(n > 1){
-            check.push_back(eventTime - endTime[n-1]);
+        check.push_back(eventTime - prev);
+        for(auto c : check){
+            cout << c << " ";
         }
+        cout << endl;
+        int i = 0;
+        int j = 0;
+        int mx = 0;
         int ans = 0;
-        int sm = 0;
-        for(int i = 0; i < check.size(); i++){
-            sm += check[i];
-            if(i >= k){
-                ans = max(ans,sm);
-                sm -= check[i-k];
-            }
+        while(j < k+1){
+            mx += check[j++];
         }
-        ans = max(ans,sm);
+        ans = max(ans, mx);
+        while(j < check.size()){
+            mx += check[j++];
+            mx -= check[i++];
+            ans = max(ans, mx);
+        }
         return ans;
     }
 };
