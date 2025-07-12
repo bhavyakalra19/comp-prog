e554fee35cb695a63e79347341fb5bba25614021
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-07-12 14:44:49 +0530

    Time: 1660 ms (6.25%) | Memory: 285 MB (6.25%) - LeetSync

---
+++ 2028-the-earliest-and-latest-rounds-where-players-compete/the-earliest-and-latest-rounds-where-players-compete.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>> mp;
+    void dfs(int &curr, int i, int j, vector<int> &nums, int &firstPlayer, int &secondPlayer){
+        while(i <= j && (curr & (1 << i))){
+            i++;
+        }
+        while(i <= j && (curr & (1 << j))){
+            j--;
+        }
+        if(i >= j){
+            nums.push_back(curr);
+            return;
+        }
+        if(i != firstPlayer && i != secondPlayer){
+            curr |= (1 << i);
+            dfs(curr, i+1, j-1, nums, firstPlayer, secondPlayer);
+            curr ^= (1 << i);
+        }
+        if(j != secondPlayer && j != firstPlayer){
+            curr |= (1 << j);
+            dfs(curr, i+1, j-1, nums, firstPlayer, secondPlayer);
+            curr ^= (1 << j);
+        }
+    }
+    
+    bool checkStatus(int &n, int &firstPlayer, int &secondPlayer, int &curr){
+        int i = 1;
+        int j = n;
+        int fc = 0;
+        int sc = 0;
+        while(i < firstPlayer){
+            if((curr & (1 << i)) == 0){
+                fc++;
+            }
+            i++;
+        }
+        while(j > secondPlayer){
+            if((curr & (1 << j)) == 0){
+                sc++;
+            }
+            j--;
+        }
+        if(fc == sc){
+            return 1;
+        }
+        return false;
+    }
+
+    int getMin(int &n, int &firstPlayer, int &secondPlayer, int &curr){
+        if(checkStatus(n, firstPlayer, secondPlayer, curr)){
+            return 1;
+        }
+        
+        // if(mp.find(curr) == mp.end()){
+            vector<int> nums;
+            dfs(curr, 1, n, nums, firstPlayer, secondPlayer); 
+        //     mp[curr] = nums;
+        // }
+        int ans = 1e9;
+        for(auto a : nums){
+            ans = min(ans, 1 + getMin(n, firstPlayer, secondPlayer, a));
+        }
+        return ans;
+    }
+
+    int getMax(int &n, int &firstPlayer, int &secondPlayer, int &curr){
+        if(checkStatus(n, firstPlayer, secondPlayer, curr)){
+            return 1;
+        }       
+        // if(mp.find(curr) == mp.end()){
+            vector<int> nums;
+            dfs(curr, 1, n, nums, firstPlayer, secondPlayer); 
+        //     mp[curr] = nums;
+        // }
+        int ans = -1e9;
+        for(auto a : nums){
+            ans = max(ans, 1 + getMax(n, firstPlayer, secondPlayer, a));
+        }
+        return ans;
+    }
+
+
+    vector<int> earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
+        int a = (1 << (n + 1)) - 1;
+        int mx = ceil(log2(n));
+        int curr = 0;
+        int a1 = getMin(n, firstPlayer, secondPlayer, curr);
+        curr = 0;
+        int b1 = getMax(n, firstPlayer, secondPlayer, curr);
+        return {a1,b1};
+    }
+};
