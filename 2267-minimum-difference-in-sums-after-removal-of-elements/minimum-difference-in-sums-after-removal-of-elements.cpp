10c09e6cbffe6906a5526035a878d56a69ffdbf4
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-07-19 12:10:43 +0530

    Time: 90 ms (90.89%) | Memory: 154.8 MB (44.05%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    static long long minimumDifference(vector<int>& nums) {
-        const int n=nums.size()/3;
-        vector<long long> diff(n+1,0);
-        priority_queue<int> pqL(nums.begin(), nums.begin()+n);
-        priority_queue<int, vector<int>, greater<int>> pqR(nums.begin()+2*n, nums.end());
-        long long sum=accumulate(nums.begin(), nums.begin()+n, 0LL);
-        for(int i=n; i<=2*n; i++){
-            diff[i-n]=sum;
-        //    cout<<sum<<",";
-            int x=nums[i];
-            if (x>=pqL.top()) continue;
-            sum+=x-pqL.top();
-            pqL.pop();
-            pqL.push(x);
+    long long minimumDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> min_arr(n, 0);
+        vector<long long> max_arr(n, 0);
+        priority_queue<int> pq;
+        priority_queue<int, vector<int>, greater<int>> mq;
+        int a = n/3;
+        int b = 2 * a;
+        long long sum = 0;
+        for(int i = 0; i < n/3; i++){
+            sum += nums[i];
+            pq.push(nums[i]);
         }
-    //    cout<<endl;
-        sum=accumulate(nums.begin()+2*n, nums.end(), 0LL);
-        for(int i=2*n-1; i>=n-1; i--){
-            diff[i-n+1]-=sum;
-        //    cout<<sum<<",";
-            int x=nums[i];
-            if (x<=pqR.top()) continue;
-            sum+=x-pqR.top();
-            pqR.pop();
-            pqR.push(x);
+        min_arr[a - 1] = sum; 
+        for(int i = n/3; i < b; i++){
+            if(nums[i] < pq.top()){
+                sum -= pq.top();
+                pq.pop();
+                sum += nums[i];
+                pq.push(nums[i]);
+            }
+            min_arr[i] = sum;
         }
-        return *min_element(diff.begin(), diff.end());
+        sum = 0;
+        for(int i = n-1; i >= b; i--){
+            sum += nums[i];
+            mq.push(nums[i]);
+        }
+        max_arr[b] = sum;
+        for(int i = ((2 * n)/ 3) - 1; i >= n/3; i--){
+            if(nums[i] > mq.top()){
+                sum -= mq.top();
+                mq.pop();
+                sum += nums[i];
+                mq.push(nums[i]);
+            }
+            max_arr[i] = sum;
+        }
+        long long mn = LONG_MAX;
+        for(int i = a-1; i < b; i++){
+            mn = min(mn, min_arr[i] - max_arr[i+1]);
+        }
+        return mn;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
