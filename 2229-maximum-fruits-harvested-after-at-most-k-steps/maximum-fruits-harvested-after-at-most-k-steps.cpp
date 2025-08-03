f4d2751b8dacb6a64c9792b0c4e2a73755f4f9a1
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-08-03 14:11:21 +0530

    Time: 21 ms (42.98%) | Memory: 134.7 MB (37.02%) - LeetSync

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -10,35 +10,29 @@ public:
         int jSt = 0;
         vector<int> rightCost(k + 1, 0);
         //check for fruits which are reachable from starPos in left side
-        while(j < n && fruits[j][0] < startPos - k){
-            j++;
-        }
+        while(j < n && fruits[j][0] < startPos - k) j++;
+
         jSt = j;
 
         //calculating total cost from left most reachable point to startPos inclusive
-        while(j < n && fruits[j][0] <= startPos){
-            cost += fruits[j++][1];
-        }
+        while(j < n && fruits[j][0] <= startPos) cost += fruits[j++][1];
+
         //calculating prefix Sum for right most Reachable Part
         if(j < n){
             for(int i = startPos + 1; i <= startPos + k; i++){
-                int point = i - startPos;
-                if(j < n && (fruits[j][0] == i)){
-                    rightCost[point] += fruits[j++][1];
-                }
-                rightCost[point] += rightCost[point - 1];
+                if(j < n && (fruits[j][0] == i)) rightCost[i - startPos] += fruits[j++][1];
+                rightCost[i - startPos] += rightCost[i - startPos - 1];
             }
         }
         j = jSt;
+
         for(int i = startPos - k; i <= startPos; i++){
             int left = startPos - i;
             ans = max(ans, cost + rightCost[(int)((k - left) / 2)]);
-            if(left * 2 <= k){
-                ans = max(ans, cost + rightCost[k - (left * 2)]);
-            }
-            if(j < n && fruits[j][0] == i){
-                cost -= fruits[j++][1];
-            }
+            
+            if(left * 2 <= k) ans = max(ans, cost + rightCost[k - (left * 2)]);
+            
+            if(j < n && fruits[j][0] == i) cost -= fruits[j++][1];
         }
         return ans;
     }
