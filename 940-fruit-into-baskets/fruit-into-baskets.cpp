a7d8a915a43e31d8b4d5512c278329f49c5d2fd8
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-08-04 11:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 69.8 MB (99.71%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int prev = 0;
+        int fn = fruits[0];
+        int sn = -1;
+        int i = 0;
+        int ans = 0;
+        int j = 0;
+        while(i < n){
+            if(fruits[i] != fn && fruits[i] != sn){
+                if(sn == -1){
+                    sn = fruits[i];
+                }else{
+                    sn = fruits[i];
+                    ans = max(ans, i - j);
+                    j = prev;
+                    fn = fruits[j];
+                }
+            }
+            if(i > 0 && fruits[i] != fruits[i-1]){
+                prev = i;
+            }
+            i++;
+        }
+        ans = max(ans, n - j);
+        return ans;
+    }
+};
+
+// 11 2222 1111 22222
+
+// int next = (diff start + part of herd)
+// if(nums[i] != nums[i-1]){
+//     next = i
+// }
+// int lastPointer = i;
