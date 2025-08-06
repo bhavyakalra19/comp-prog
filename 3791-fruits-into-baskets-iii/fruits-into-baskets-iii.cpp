a2a16a32468782ccdc8cec731307417ed10c39ee
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-08-06 10:42:56 +0530

    Time: 368 ms (33.98%) | Memory: 308 MB (5.01%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -2,16 +2,14 @@ class Node{
     public:
         int st;
         int en;
-        int leftMax;
-        int rightMax;
+        int mx;
         Node *left;
         Node *right;
 
     Node(int l, int r){
         st = l;
         en = r;
-        leftMax = 0;
-        rightMax = 0;
+        mx = 0;
         left = NULL;
         right = NULL;
     }
@@ -22,34 +20,30 @@ public:
     Node* createTree(vector<int> &baskets, int left, int right){
         if(left == right){
             Node *root = new Node(left, right);
-            root->leftMax = baskets[left];
-            root->rightMax = baskets[left];
+            root->mx = baskets[left];
             return root;
         }
         int mid = (left + right)/2;
         Node *root = new Node(left, right);
         root->left = createTree(baskets, left, mid);
         root->right = createTree(baskets, mid + 1, right);
-        root->leftMax = max(root->left->leftMax, root->left->rightMax);
-        root->rightMax = max(root->right->leftMax, root->right->rightMax);
+        root->mx = max(root->left->mx, root->right->mx);
         return root;
     }
     
     int getAns(Node *&root, int curr){
         if(root->left == root->right){
-            int val = root->leftMax;
-            root->leftMax = 0;
-            root->rightMax = 0;
+            int val = root->mx;
+            root->mx = 0;
             return val;
         }
         int a;
-        if(root->leftMax >= curr){
+        if(root->left->mx >= curr){
             a = getAns(root->left, curr);
-            root->leftMax = max(root->left->leftMax, root->left->rightMax);
         }else{
             a = getAns(root->right, curr);
-            root->rightMax = max(root->right->leftMax, root->right->rightMax);
         }
+        root->mx = max(root->left->mx, root->right->mx);
         return a;
     }
 
@@ -58,7 +52,7 @@ public:
         Node *root = createTree(baskets, 0, n-1);
         int ans = 0;
         for(int i = 0; i < n; i++){
-            if(fruits[i] > root->leftMax && fruits[i] > root->rightMax){
+            if(fruits[i] > root->mx){
                 ans++;
             }else{
                 getAns(root, fruits[i]);
