aa4c26c4ebece0aac8d1afd78e2189c55f519ac9
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-08-16 18:52:33 +0530

    Time: 2363 ms (5.03%) | Memory: 331.4 MB (10.08%) - LeetSync

---
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
+        vector<vector<int>> pre(n + 1);
+        vector<int> post(n + 2, 0);
+        vector<long long> count(n + 1, 0);
+        post[n+1] = 1e9;
+        int j = 0;
+        for(auto &a : conflictingPairs){
+            if(a[0] > a[1]){
+                swap(a[0], a[1]);
+            }
+            pre[a[0]].push_back(a[1]);
+            post[a[1]]++;
+        } 
+        int top1 = 0;
+        int top2 = 0;
+        long long ans = 0;
+        bool op = true;
+        for(int i = 1; i <= n; i++){
+            if(op){
+                int j = i+1;
+                int cnt = 0;
+                while(j <= n + 1 && cnt < 2){
+                    if(post[j] > 0){
+                        if(cnt == 0){
+                            top1 = j;
+                            top2 = j;
+                        }else{
+                            top2 = j;
+                        }
+                        cnt += post[j];
+                    }
+                    j++;
+                }
+                op = false;
+            }
+            ans += top1 - i;
+            if(top1 != n+1) count[top1] += top2 - top1;
+            //increase curr
+            for(auto &a : pre[i]){
+                post[a]--;
+                if(((a == top1 || a == top2) && post[a] == 0) || (top1 == top2)){
+                    op = true;
+                }
+            }
+        }
+        long long mx = 0;
+        for(int i = 1; i <= n; i++){
+            mx = max(mx, count[i]);
+        }
+        return ans + mx;
+    }
+};
