572d260f56b27d533557f1a17a3c807d77576dc6
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2025-10-25 13:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.15%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int calculateSum(int n){
+        return (n * (n + 1)) / 2;
+    }
+
+    int totalMoney(int n) {
+        int sevenSum = calculateSum(7);
+        int total = n / 7;
+        int start = 1 + total;
+        int newSum = calculateSum(total - 1) * 7;
+        total = sevenSum * total;
+        total += newSum;
+        for(int i = 0; i < n % 7; i++){
+            total += start;
+            start++;
+        }
+        return total;
+    }
+};
