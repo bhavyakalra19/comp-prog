1c89f2e38092dbd5b8011bc4d8fea680b800e92f
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-06-21 00:43:04 +0530

    Time: 163 ms (5.55%) | Memory: 134.9 MB (5.56%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.cpp
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     int maxBuilding(int n, vector<vector<int>>& res) {
-        // int curr_mx = n;
-        // int curr = 0;
-        // int mx = 0;
-        // vector<int> ans(n, 0);
-        // unordered_map<int, int> check;
-        // for(auto &a : res){
-        //     check[a[0] - 1] = a[1];
-        // }
-        // for(int i = n-1; i > 0; i--){
-        //     if(check.find(i) != check.end() && check[i] <= curr_mx){
-        //         curr_mx = check[i];
-        //     }else{
-        //         curr_mx = min(curr_mx + 1, n);
-        //     }
-        //     ans[i] = curr_mx;
-        // }
-        // for(int i = 1; i < n; i++){
-        //     curr = min(curr+1, ans[i]);
-        //     mx = max(mx, curr);
-        // }
-        // return mx;
-
-        // n is too large instead of this update the restrictions from left to right and then right to left.
         res.push_back({1, 0});
         res.push_back({n, n});
         sort(res.begin(), res.end());
