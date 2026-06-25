7723c12d9963956e509783e862a0c3f0ec5f0654
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-06-25 23:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (26.80%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -8,21 +8,26 @@ public:
         // target -> +1, others -> -1
         //find all prefix sum encounters lesser than curr as there will be more target values there -> very obvious 
         int mn = n;
-        int ans = 0;
+        long long ans = 0;
         check[n] = 1;
+        long long ps = 0;
         for(int i = 0; i < n; i++){
             if(nums[i] == target){
+
+                ps += check[curr];
+
                 curr += 1;
             }else{
                 curr -= 1;
-                mn = min(mn, curr);
+                ps -= check[curr];
             }
-            for(int j = mn; j < curr; j++){
-                ans += check[j];
-            }
+ 
             check[curr]++;
+            ans +=ps;
         }
         return ans;
     }
 };
 // n n-1, n, n+1 , n
+
+// 0 -1, -2, -3 -2 -3
