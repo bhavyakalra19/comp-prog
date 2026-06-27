37569794c86d49f25d3ae01c9d20d518a2c47037
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-06-27 13:20:50 +0530

    Time: 860 ms (5.14%) | Memory: 113.1 MB (88.57%) - LeetSync

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int checkBinarySearch(vector<int> &nums, int st, int en, long long check){
+        int ans = -1;
+        while(st <= en){
+            int mid = (st + en)/2;
+            if(nums[mid] < check){
+                st = mid + 1;
+            }else if(nums[mid] == check){
+                ans = mid;
+                en = mid - 1;
+            }else{
+                en = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    int maximumLength(vector<int>& nums) {
+        unordered_set<int> st;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < n && nums[i] == 1){
+            i++;
+        }
+        int mx = (i % 2 == 0 && i > 0) ? i - 1 : i;
+        cout << "i -> " << i << endl;
+
+        while(i < n){
+            int check = nums[i];
+            if(st.find(check) != st.end()){
+                while(i < n && nums[i] == check){
+                    i++;
+                }
+            }else{
+                int j = i;
+                cout << nums[j] << " ";
+                int ans = 2;
+                while(j + 1 < n && nums[j] == nums[j+1]){
+                    st.insert(nums[j]);
+                    int mid = checkBinarySearch(nums, j+2, n-1, 1LL * nums[j] * nums[j]);
+                    if(mid == -1){
+                        break;
+                    }
+                    cout << nums[mid] << " ";
+                    ans += 2;
+                    j = mid;
+                }
+                mx = max(mx, ans - 1);
+                i++;
+            }
+            cout << endl;
+        }
+        return mx;
+    }
+};
+
+
+// 2 2 4 4 16 
