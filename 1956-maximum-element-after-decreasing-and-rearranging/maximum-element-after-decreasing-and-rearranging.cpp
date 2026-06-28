dcac64282de60fc499843cb03a199cfde4694e98
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-06-28 11:17:56 +0530

    Time: 5 ms (79.66%) | Memory: 57.4 MB (4.52%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,19 +1,50 @@
-class Solution {
-public:
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int n = arr.size();
-        arr[0] = 1;
-        for(int i = 1; i < n; i++){
-            if(arr[i] - arr[i-1] > 1){
-                arr[i] = arr[i-1] + 1;
-            }
-        }
-        return arr[n-1];
-    }
-};
-
+// class Solution {
+// public:
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         sort(arr.begin(), arr.end());
+//         int n = arr.size();
+//         arr[0] = 1;
+//         for(int i = 1; i < n; i++){
+//             if(arr[i] - arr[i-1] > 1){
+//                 arr[i] = arr[i-1] + 1;
+//             }
+//         }
+//         return arr[n-1];
+//     }
+// }; 
+//above method is of sorting o(n * logn) time and O(1) space
 //decrease 
 
 // rearrange
+
+//below method by hashmap -> o(n) time and O(n) space
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+       
+        int n = arr.size();
+        vector<int> mp(n+1, 0);
+        for(int i = 0; i < n; i++){
+            mp[min(arr[i], n)]++;
+        }
+
+        int i = 0;
+        int curr = 1;
+        int used_smaller = 0;
+        while(i < n){
+            int used = min(mp[curr], used_smaller);
+            mp[curr] -= used;
+            used_smaller -= used;
+            if(mp[curr] > 0){
+                i += mp[curr];
+            }else{
+                i++;
+                used_smaller++;
+            }
+            curr++;
+        }
+        return curr - 1;
+    }
+};
 
