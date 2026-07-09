e6a351354a6af03db375a6fbc3e07fb27d0e3015
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-07-09 18:26:38 +0530

    Time: 36 ms (76.25%) | Memory: 151.5 MB (74.35%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,56 +1,59 @@
 class Solution {
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-        const int kMod = 1e9 + 7;
-        const int n = s.length();
+        int n = s.size();
+        vector<long long> nums(n+1);
+        vector<int> power10(n+1);
+        vector<long long> sums(n+1);
+        
+        int mod = 1e9 + 7;
+        long long sum = 0;
+        int ten = 0;
+        long long num = 0;
+        nums[0] = 0;
+        power10[0] = 0;
+        sums[0] = 0;
 
-        vector<int> idx(n+1);
-        vector<long long> val(n+1);
-        vector<long long> total(n+1);
-        vector<long long> pow10(n+1, 1);
-
-        for (int i = 1; i <= n; ++i) {
-            pow10[i] = (pow10[i-1] * 10) % kMod;
-        }
-
-        int c = 0;
-
-        for (int i = 0; i < n; ++i) {
-            const int d = s[i] - '0';
-            if (0 != d) {
-                ++c;
-                val[c] = (val[c-1]*10 + d) % kMod;
-                total[c] = total[c-1] + d;
+        for(int i = 0; i < n; i++){
+            if(s[i] != '0'){
+                int x = s[i] - '0';
+                sum = (sum + x) % mod;
+                num = (num * 10 + x) % mod;
+                ten += 1;
             }
-            idx[i+1] = c;
+            nums[i+1] = num;
+            sums[i+1] = sum;
+            power10[i+1] = ten;
+        }
+        int mx = power10[n];
+        vector<int> power(mx+1);
+        power[0] = 1;
+        for(int i = 1; i <= mx; i++){
+            power[i] = (1LL * power[i-1] * 10) % mod;
         }
 
         vector<int> ans;
-        ans.reserve(queries.size());
+        for(auto &q : queries){
+            int i = q[0];
+            int j = q[1] + 1;
+            long long curr_sum = (sums[j] - sums[i] + mod) % mod;
 
-        for (const auto& q: queries) {
-            const int l = q[0];
-            const int r = q[1];
-
-            const int a = idx[l];
-            const int b = idx[r+1];
-
-            if (a == b) {
-                ans.push_back(0);
-                continue;
-            }
-
-            const int len = b-a;
-
-            long long num = (val[b] - val[a] * pow10[len]) % kMod;
-            if (num < 0) {
-                num += kMod;
-            }
-
-            long long sum = total[b] - total[a];
-            ans.push_back((num * sum) % kMod);
+            int k = power[power10[j] - power10[i]];
+            int curr = ((nums[j] - (nums[i] * k) % mod + mod) % mod * curr_sum + mod) % mod;
+            ans.push_back(curr);
         }
-
         return ans;
-    }
+    }  
 };
+
+// 0 0 0
+// 1 1 10
+// 1 1 10
+// 12 3 100
+// 12 3 100
+// 123 6 1000
+// 123 6 1000
+// 123 6 1000
+
+
+// int power = (nums[j] - (nums[i] * (power[j+1] / power[i]))) * sum;
