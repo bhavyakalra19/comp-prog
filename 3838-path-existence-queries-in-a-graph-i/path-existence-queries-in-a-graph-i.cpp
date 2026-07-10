da943c182323fb3740f2bdbef9ee7c70856f7f7e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-07-10 19:15:29 +0530

    Time: 4 ms (93.65%) | Memory: 224.2 MB (66.30%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    //union find
-    vector<int> check;
-
-    int findFunc(int a){
-        if(check[a] == a){
-            return a;
-        }
-        return check[a] = findFunc(check[a]);
-    }
-
-    void unionFunc(int a, int b){
-        int f1 = findFunc(a);
-        int f2 = findFunc(b);
-        check[f1] = f2;
-    }
-
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        //find table initialized
-        check.resize(n);
-        for(int i = 0; i < n; i++){
-            check[i] = i;
+        int curr = 1;
+        vector<int> check(n);
+        check[0] = 1;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i-1] > maxDiff){
+                curr++;
+            }
+            check[i] = curr;
         }
         vector<bool> ans;
-
-        //main logic -> why check all nodes check for the last one if it reachable to next one
-        for(int i = 0; i < n-1; i++){
-            if(nums[i+1] - nums[i] <= maxDiff){
-                unionFunc(i, i+1);
-            }
-        }
-
         for(auto &a : queries){
-            int f1 = findFunc(a[0]);
-            int f2 = findFunc(a[1]);
-            if(f1 != f2){
+            if(check[a[0]] != check[a[1]]){
                 ans.push_back(false);
             }else{
                 ans.push_back(true);
