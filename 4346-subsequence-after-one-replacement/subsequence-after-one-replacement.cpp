6c9684582d7894be9f3d394f310be046321acca6
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-07-12 14:10:05 +0530

    Time: 27 ms (19.84%) | Memory: 42 MB (10.89%) - LeetSync

---
+++ 4346-subsequence-after-one-replacement/subsequence-after-one-replacement.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canMakeSubsequence(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<int> prefix(n, -1);
+        vector<int> suffix(n, -1);
+        if(n == 1) return true;
+        int i = 0;
+        int j = 0;
+        while(i < n && j < m){
+            if(s[i] == t[j]){
+                prefix[i] = j;
+                i++;
+                j++;
+                if(i == n-1 && j < m) return true;
+            }else{
+                j++;
+            }
+        }
+
+        i = n-1;
+        j = m-1;
+        while(j >= 0 && i >= 0){
+            if(s[i] == t[j]){
+                suffix[i] = j;
+                i--;
+                j--;
+                if(i == 0 && j >= 0) return true;
+            }else{
+                j--;
+            }
+        }
+
+        for(int i = 1; i < n-1; i++){
+            int a = prefix[i-1];
+            int b = suffix[i+1];
+            // cout << a << " " << b << endl;
+            if(a != -1 && b != -1 && b - a > 1) return true;
+        }
+
+        return false;
+    }
+};
