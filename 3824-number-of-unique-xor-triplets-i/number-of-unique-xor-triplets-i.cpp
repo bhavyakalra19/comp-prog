811fac037b9397dc74b60894131ed9362f9c344e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-07-23 20:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 198.7 MB (85.00%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -7,6 +7,14 @@ public:
         while(x <= n){
             x = x << 1;
         }
+        // before 2 we can't get 0 and 
+        //after that take the number which has most highest bit set
+        // set all the bit of that number as we can get that number via xor of numbers before that
+        // suppose you have 4 -> 100 -> you can maximum get 111 which is 7 via -> 4 ^ 2 ^ 1 
+        // so take the number with highes left bit set and set all bit from right to left most bit 
+        // or get a number greater than that and -1 
+        // but we have to add zero as well so remove the -1 
+        // and thats the final answer greater than the bit we can find
         return x;
     }
 };
