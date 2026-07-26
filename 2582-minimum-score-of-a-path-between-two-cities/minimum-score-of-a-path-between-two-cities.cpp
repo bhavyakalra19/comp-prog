a5fbd0da7550b0760f9339fa88b54b5f736d2586
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-07-26 14:49:04 +0530

    Time: 75 ms (33.40%) | Memory: 175 MB (5.02%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> graph(n + 1);
-        for(auto &a : roads){
-            graph[a[0]].push_back({a[1], a[2]});
-            graph[a[1]].push_back({a[0], a[2]});
+        vector<vector<pair<int,int>>> g(n + 1);
+
+        for (auto &e : roads) {
+            g[e[0]].push_back({e[1], e[2]});
+            g[e[1]].push_back({e[0], e[2]});
         }
 
-        vector<int> check(n+1, INT_MAX);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({INT_MAX, 1});
-        while(!pq.empty()){
-            auto it = pq.top();
-            pq.pop();
+        vector<int> vis(n + 1, 0);
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
 
-            int cx = it.second;
-            int cw = it.first;
+        int ans = INT_MAX;
 
-            if(check[cx] != cw) continue;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
 
-            for(auto &a : graph[cx]){
-                int nx = a.first;
-                int nw = min(cw, a.second);
-
-                if(check[nx] > nw){
-                    check[nx] = nw;
-                    pq.push({nw, nx});
+            for (auto &[v, w] : g[u]) {
+                ans = min(ans, w);
+                if (!vis[v]) {
+                    vis[v] = 1;
+                    q.push(v);
                 }
             }
         }
-        return check[n];
+
+        return ans;
     }
 };
