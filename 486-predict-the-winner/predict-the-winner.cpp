00163ffae0d4bc13c9b5b38bf37bea40981b4a40
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-01 14:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (32.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int getAns(int i, int j, int &n, vector<int> &nums, int x){
+    vector<vector<int>> dp;
+    int getAns(int i, int j, int &n, vector<int> &nums){
         if(i == j){
             return nums[i];
         }
-        if(dp[i][j][x] != -1) return dp[i][j][x];
-        int a = nums[i] - getAns(i+1, j, n, nums, x ^ 1);
-        int b = nums[j] - getAns(i, j-1, n, nums, x ^ 1);
-        return dp[i][j][x] = max(a, b);
+        if(dp[i][j] != -1) return dp[i][j];
+        int a = nums[i] - getAns(i+1, j, n, nums);
+        int b = nums[j] - getAns(i, j-1, n, nums);
+        return dp[i][j] = max(a, b);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        return getAns(0, n-1, n, nums, 0) >= 0;
+        dp.resize(n, vector<int>(n, -1));
+
+        for(int i = 0; i < n; i++){
+            dp[i][i] = nums[i];
+        }
+
+        for(int j = 1; j < n; j++){
+            for(int i = j-1; i >= 0; i--){
+                int pickLeft = nums[i] - dp[i+1][j];
+                int pickRight = nums[j] - dp[i][j-1];
+                dp[i][j] = max(pickLeft, pickRight);
+            }
+        }
+        return dp[0][n-1] >= 0;
+
+        // return getAns(0, n-1, n, nums) >= 0;
     }
 };
