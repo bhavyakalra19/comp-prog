0bf01947794bc1de3800f2b2bad03ee7991645c4
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-01 14:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (54.28%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    // vector<vector<int>> dp;
     // int getAns(int i, int j, int &n, vector<int> &nums){
     //     if(i == j){
     //         return nums[i];
@@ -11,22 +11,44 @@ public:
     //     return dp[i][j] = max(a, b);
     // }
 
+    // bool predictTheWinner(vector<int>& nums) {
+    //     int n = nums.size();
+    //     dp.resize(n, vector<int>(n, -1));
+
+    //     for(int i = 0; i < n; i++){
+    //         dp[i][i] = nums[i];
+    //     }
+    //     // i = j - 1 
+    //     for(int j = 1; j < n; j++){
+    //         for(int i = j-1; i >= 0; i--){
+    //             int pickLeft = nums[i] - dp[i+1][j];
+    //             int pickRight = nums[j] - dp[i][j-1];
+    //             dp[i][j] = max(pickLeft, pickRight);
+    //         }
+    //     }
+
+    //     return dp[0][n-1] >= 0;
+
+    //     // return getAns(0, n-1, n, nums) >= 0;
+    // }
+
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n, vector<int>(n, -1));
+        vector<int> dp(n);
 
         for(int i = 0; i < n; i++){
-            dp[i][i] = nums[i];
+            dp[i] = nums[i];
         }
-
+        // i = j - 1 so why dont we pick single array
         for(int j = 1; j < n; j++){
             for(int i = j-1; i >= 0; i--){
-                int pickLeft = nums[i] - dp[i+1][j];
-                int pickRight = nums[j] - dp[i][j-1];
-                dp[i][j] = max(pickLeft, pickRight);
+                int pickLeft = nums[i] - dp[i+1];
+                int pickRight = nums[j] - dp[i];
+                dp[i] = max(pickLeft, pickRight);
             }
         }
-        return dp[0][n-1] >= 0;
+
+        return dp[0] >= 0;
 
         // return getAns(0, n-1, n, nums) >= 0;
     }
