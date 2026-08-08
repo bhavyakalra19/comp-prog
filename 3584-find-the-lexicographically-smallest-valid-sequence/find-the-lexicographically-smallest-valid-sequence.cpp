3cc8f9c8be548684d7378882bc260a8f8eaa9a78
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-08 10:04:26 +0530

    Time: 47 ms (52.13%) | Memory: 115.5 MB (29.79%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
         int n = word1.size();
-        vector<int> prefix(n, 0);
+        // vector<int> prefix(n, 0);
         vector<int> suffix(n, 0);
         int m = word2.size();
         int j = 0;
         int cnt = 0;
-        for(int i = 0; i < n; i++){
-            prefix[i] = cnt;
-            if(j < m && word1[i] == word2[j]){
-                j++;
-                cnt++;
-            }
-        }
+        // for(int i = 0; i < n; i++){
+        //     prefix[i] = cnt;
+        //     if(j < m && word1[i] == word2[j]){
+        //         j++;
+        //         cnt++;
+        //     }
+        // }
         j = m-1;
         cnt = 0;
         for(int i = n-1; i >= 0; i--){
@@ -31,7 +31,7 @@ public:
                 j++;
                 ans.push_back(i);
                 if(j == m) break;
-            }else if ((suffix[i] + prefix[i]) >= (m-1) && check){
+            }else if ((suffix[i] + j) >= (m-1) && check){
                 j++;
                 ans.push_back(i);
                 check = false;
