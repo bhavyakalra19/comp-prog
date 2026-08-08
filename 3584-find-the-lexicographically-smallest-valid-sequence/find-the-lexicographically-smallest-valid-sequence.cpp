26a9df547af7d1cb829bbe171fc9c266ebf86336
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-08 10:04:51 +0530

    Time: 49 ms (40.43%) | Memory: 115.4 MB (31.92%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
         int n = word1.size();
-        // vector<int> prefix(n, 0);
         vector<int> suffix(n, 0);
         int m = word2.size();
-        int j = 0;
+        int j = m-1;
         int cnt = 0;
-        // for(int i = 0; i < n; i++){
-        //     prefix[i] = cnt;
-        //     if(j < m && word1[i] == word2[j]){
-        //         j++;
-        //         cnt++;
-        //     }
-        // }
-        j = m-1;
-        cnt = 0;
         for(int i = n-1; i >= 0; i--){
             suffix[i] = cnt;
             if(j >= 0 && word1[i] == word2[j]){
