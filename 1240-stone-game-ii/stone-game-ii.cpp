80bad8f6379a4839d8899e64a1f92bf4260ac364
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-09 10:50:50 +0530

    Time: 11 ms (54.67%) | Memory: 13.6 MB (64.48%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int n;
-    int getAns(vector<int> &piles, int i, int m, vector<vector<int>> &dp){
-        if(i + 2 * m >= n){
-            return piles[i];
-        }
-        if(dp[i][m] != -1) return dp[i][m];
-        int ans = INT_MAX;
-        for(int j = i; j < i + 2 * m; j++){
-            int nm = max(m, j - i + 1);
-            ans = min(ans, getAns(piles, j + 1, nm, dp));
-        }
-        return dp[i][m] = piles[i] - ans;
-    }
+    // int getAns(vector<int> &piles, int i, int m, vector<vector<int>> &dp){
+    //     if(i + 2 * m >= n){
+    //         return piles[i];
+    //     }
+    //     if(dp[i][m] != -1) return dp[i][m];
+    //     int ans = INT_MAX;
+    //     for(int j = i; j < i + 2 * m; j++){
+    //         int nm = max(m, j - i + 1);
+    //         ans = min(ans, getAns(piles, j + 1, nm, dp));
+    //     }
+    //     return dp[i][m] = piles[i] - ans;
+    // }
 
     int stoneGameII(vector<int>& piles) {
         n = piles.size();
