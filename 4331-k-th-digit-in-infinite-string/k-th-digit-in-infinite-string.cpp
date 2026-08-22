b2ef483709b71d0221ae66c3560c0d0f164095f0
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-22 12:06:57 +0530

    Time: 10 ms (5.31%) | Memory: 8.7 MB (93.86%) - LeetSync

---
+++ 4331-k-th-digit-in-infinite-string/k-th-digit-in-infinite-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    long long getNum(long long start, long long sm, long long left){
+        // 9 + 1
+        // if left = 8 -> 8 / sm 
+        long long check_digit = start + ((left - 1) / sm) + 1;
+        int place = check_digit % 10;
+        check_digit /= 10;
+        if(check_digit % 2 == 0){
+            // b is even increasing order
+            check_digit *= 10;
+            check_digit += place;
+        }else{
+            // b is odd decreasing order
+            check_digit *= 10;
+            check_digit += (9 - place);
+        }
+        int digit_num = sm - (left % sm == 0 ? sm : left % sm);
+        cout << digit_num << endl;
+        cout << check_digit << endl;
+        while(digit_num > 0){
+            check_digit /= 10;
+            digit_num--;
+        }
+        cout << check_digit % 10 << endl;
+        return check_digit % 10;
+    }
+    
+    int kthDigit(long long k) {
+        long long sm = 2;
+        long long start = 9;
+        long long total = 9;
+        long long actual_start = 9;
+        if(k <= 9) return k;
+        while(total + (start * 10 * sm) < k){
+            total += start * 10 * sm;
+            start *= 10;
+            sm += 1;
+            actual_start = actual_start * 10 + 9;
+        }
+        return getNum(actual_start, sm, k - total);
+    }
+};
+// 1 - 9 -> 1 -> 9 * 1
+// 10 - 99 -> 2 -> 90 * 2
+// 100 - 999 -> 3 -> 900 * 3
+
+
+// 6 -> 3 + 9 => 12th -> 
+
+// 5 -> digit -> k % sm;
