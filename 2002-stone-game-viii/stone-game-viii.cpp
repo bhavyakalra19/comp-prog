6ddf014e6b5cc2ce5dcef03d4847842d3b924142
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-24 13:07:36 +0530

    Time: 110 ms (49.20%) | Memory: 87.3 MB (84.19%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    long long getAns(vector<int> &stones, int i, int &n, vector<long long> &dp){
-        if(i == n-1){
-            return stones[n-1];
-        }
-        if(dp[i] != LONG_MIN) return dp[i];
-        // remove for loop here by simply take stones and remove the bob's / alice max or skip choosing stone and pick in future as in any position we are just taking ps[i]
-        long long ans = max(getAns(stones, i+1, n, dp), stones[i] - getAns(stones, i+1, n, dp));
-        return dp[i] = ans;
-    }
+    // long long getAns(vector<int> &stones, int i, int &n, vector<long long> &dp){
+    //     if(i == n-1){
+    //         return stones[n-1];
+    //     }
+    //     if(dp[i] != LONG_MIN) return dp[i];
+    //     // remove for loop here by simply take stones and remove the bob's / alice max or skip choosing stone and pick in future as in any position we are just taking ps[i]
+    //     long long ans = max(getAns(stones, i+1, n, dp), stones[i] - getAns(stones, i+1, n, dp));
+    //     return dp[i] = ans;
+    // }
 
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
         for(int i = 1; i < n; i++){
             stones[i] += stones[i-1];
         }
-        vector<long long> dp(n, LONG_MIN);
-        int a = getAns(stones, 1, n, dp);
-        return a;
+        // vector<long long> dp(n, LONG_MIN);
+        // int a = getAns(stones, 1, n, dp);
+        
+
+        // we only need dp[i+1] so remove array
+        int dp = stones[n-1];
+        for(int i = n-2; i >= 1; i--){
+            dp = max(dp, stones[i] - dp);
+        }
+        return dp;
     }
 };
 
