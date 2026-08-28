80e96a3b7b3d7c474ac3f6605b2659d5cf9170b5
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-29 01:14:52 +0530

    Time: 59 ms (64.90%) | Memory: 14.4 MB (48.66%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int n;
-
-    int getAns(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
-        if(i == n){
-            return 0;
-        }
-        // skip taking 
-        // taking then use newer j
-        if(dp[i][j] != INT_MIN) return dp[i][j];
-
-        int ans = getAns(nums, i+1, j, dp);
-        if(j == n || nums[i] > nums[j]) ans = max(ans, 1 + getAns(nums, i+1, i, dp));
-        return dp[i][j] = ans;
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, INT_MIN));
-        return getAns(nums, 0, n, dp);
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int mx = 1;
+        for(int i = 1; i < n; i++){
+            int ans = 0;
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    ans = max(ans, dp[j]);
+                }
+            }
+            dp[i] = ans + 1;
+            mx = max(mx, dp[i]);
+        }
+        return mx;
     }
 };
