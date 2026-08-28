e003292a7d5a1bf1de40871735f0bae9d398300e
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-08-29 01:05:08 +0530

    Time: 563 ms (16.59%) | Memory: 296.4 MB (27.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
-    vector<int> tree;
+    int n;
 
-    int addNum(int curr, int st, int en, int idx, int val){
-        if(st == en){
-            return tree[curr] = max(tree[curr], val);
+    int getAns(vector<int> &nums, int i, int j, vector<vector<int>> &dp){
+        if(i == n){
+            return 0;
         }
-        int mid = (st + en) / 2;
-        int a = (idx <= mid) ? addNum(curr * 2 + 1, st, mid, idx, val) : tree[curr * 2 + 1];
-        int b = (idx > mid) ? addNum(curr * 2 + 2, mid + 1, en, idx, val) : tree[curr * 2 + 2];
-        return tree[curr] = max(a, b);
-    }
+        // skip taking 
+        // taking then use newer j
+        if(dp[i][j] != INT_MIN) return dp[i][j];
 
-    int getSum(int curr, int st, int en, int idx){
-        if(idx >= en){
-            return tree[curr];
-        }
-        if(st > idx) return 0;
-        int mid = (st + en) / 2;
-        int a = getSum(curr * 2 + 1, st, mid, idx);
-        int b = getSum(curr * 2 + 2, mid + 1, en, idx);
-        return max(a, b);
+        int ans = getAns(nums, i+1, j, dp);
+        if(j == n || nums[i] > nums[j]) ans = max(ans, 1 + getAns(nums, i+1, i, dp));
+        return dp[i][j] = ans;
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        int ans = 1;
-        int n = nums.size();
-        int mn = 10000;
-        int mx = -10000;
-        for(auto &a : nums){
-            mn = min(mn, a);
-            mx = max(mx, a);
-        }
-        int total = mx - mn + 1;
-        tree.resize(total * 4, 0);
-
-        addNum(0, 0, total - 1, nums[0] - mn, 1);
-        for(int i = 1; i < n; i++){
-            int val = nums[i] - mn;
-            int curr_mx = 0;
-            if(val != 0) curr_mx = max(curr_mx, getSum(0, 0, total - 1, val - 1));
-            ans = max(ans, curr_mx + 1);
-            addNum(0, 0, total - 1, val, curr_mx + 1);
-        }
-        return ans;
+        n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, INT_MIN));
+        return getAns(nums, 0, n, dp);
     }
 };
-
-// we can create segment tree or fenwick tree for logn getting 
