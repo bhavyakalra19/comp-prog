0b1ed70a779528e68f7ad1a756d594a475362784
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-09-01 00:40:30 +0530

    Time: 653 ms (8.61%) | Memory: 565.5 MB (10.06%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     int n;
-    
-    int getAns(vector<vector<int>> &grid, int r1, int c1, int r2, int c2, vector<vector<vector<vector<int>>>> &dp){
-        if((r1 == n-1 && c1 == n-1) || (r2 == n-1 && c2 == n-1)){
-            return grid[n-1][n-1];
+    int m;
+    bool vis;
+    vector<vector<vector<vector<int>>>> dp;
+    int getAns(vector<vector<int>> &grid, int i, int j, int x, int y){
+        //both will reach same position at same time so need to to worry about grid state
+        if(i == n || j == m || x == n || y == m || grid[i][j] == -1 || grid[x][y] == -1){
+            return -1e9;
         }
-        if(r1 == n || c1 == n || r2 == n || c2 == n) return INT_MIN;
-        if(grid[r1][c1] == -1 || grid[r2][c2] == -1) return dp[r1][c1][r2][c2] = INT_MIN;
-        if(dp[r1][c1][r2][c2] != -1) return dp[r1][c1][r2][c2];
-        int sum = (((r1 == r2) && (c1 == c2)) ? grid[r1][c1] : (grid[r1][c1] + grid[r2][c2]));
-        int cherry1 = max(getAns(grid, r1 + 1, c1, r2 + 1, c2, dp), getAns(grid, r1 + 1, c1, r2, c2 + 1, dp));
-        int cherry2 = max(getAns(grid, r1, c1 + 1, r2 + 1, c2, dp), getAns(grid, r1, c1 + 1, r2, c2 + 1, dp));
-        int cherry = max(cherry1, cherry2);
-        if(cherry == INT_MIN) return dp[r1][c1][r2][c2] = INT_MIN;
-        return dp[r1][c1][r2][c2] = cherry + sum;
+        if(i == n-1 && j == m-1){
+            return grid[n-1][m-1];
+        }
+        if(dp[i][j][x][y] != -1) return dp[i][j][x][y];
+
+        int cherry = 0;
+        if(grid[i][j] == 1){
+            cherry++;
+        }
+        if(grid[x][y] == 1 && !(i == x && j == y)){
+            cherry++;
+        }
+        int ans = -1e9;
+        ans = max(ans, getAns(grid, i+1, j, x+1, y));
+        ans = max(ans, getAns(grid, i, j+1, x+1, y));
+        ans = max(ans, getAns(grid, i+1, j, x, y+1));
+        ans = max(ans, getAns(grid, i, j+1, x, y+1));
+        return dp[i][j][x][y] = cherry + ans;
     }
 
-    int cherryPickup(vector<vector<int>>& grid){
+    int cherryPickup(vector<vector<int>>& grid) {
+        // solution is this that there are two bots starting from 0,0 we can go only two paths top to bottom and then bottom to up
+        // but how we can find which way has highest instead add two bots from 0,0 and start traversing them to bottom.
         n = grid.size();
-        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(n, vector<vector<int>>(n, vector<int>(n,-1))));
-        int a = getAns(grid, 0, 0, 0, 0, dp);
-        return a == INT_MIN ? 0 : a;
+        m = grid[0].size();
+        dp.resize(n, vector<vector<vector<int>>>(m, vector<vector<int>>(n, vector<int>(m, -1))));
+        if(grid[n-1][m-1] == -1) return 0;
+        int ans = getAns(grid, 0, 0, 0, 0);
+        if(ans < 0) return 0;
+        return ans;
     }
 };
