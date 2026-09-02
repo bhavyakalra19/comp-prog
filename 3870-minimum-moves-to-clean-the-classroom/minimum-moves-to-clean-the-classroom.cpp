fe2ea25cbe53f7613b705800970716034cc78631
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-09-02 18:25:25 +0530

    Time: 1989 ms (8.00%) | Memory: 330.2 MB (46.00%) - LeetSync

--- 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
@@ -1,59 +1,78 @@
 class Solution {
 public:
     int minMoves(vector<string>& classroom, int energy) {
-        int n = classroom.size();
-        int m = classroom[0].size();
-        int trash = 0;
-        int x = 0;
-        int y = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(classroom[i][j] == 'L'){
-                    classroom[i][j] = '0' + trash;
-                    trash++;
-                }else if(classroom[i][j] == 'S'){
-                    x = i;
-                    y = j;
+        	int n = classroom.size();
+		int m = classroom[0].size();
+		
+		// get start positions and number of litters and the total mask
+		int x;
+		int y;
+		int litters = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				if(classroom[i][j] == 'S'){
+					x = i;
+					y = j;
+				}
+				if(classroom[i][j] == 'L'){
+                    char c = (litters + '0');
+                    classroom[i][j] = c;
+                    litters++;
                 }
-            }
-        }
-        if(trash == 0) return 0;
-        int mx = (1 << trash) - 1;
-        vector<vector<vector<int>>> check(n, vector<vector<int>>(m, vector<int>(mx + 1, -1)));
-        check[x][y][0] = energy;
-        int ans = INT_MAX;
-        queue<vector<int>> q;
-        q.push({x, y, energy, 0, 0});
-        int dx[4] = {0,1,0,-1};
-        int dy[4] = {-1,0,1,0};
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            int cx = it[0];
-            int cy = it[1];
-            int ce = it[2];
-            int cm = it[3];
-            int cs = it[4];
-            if(cm == mx) return cs;
-            if(ce == 0 && classroom[cx][cy] != 'R') continue;
-            for(int i = 0; i < 4; i++){
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                int ne = ce - 1;
-                int nm = cm;
-                if(nx < 0 || nx == n || ny < 0 || ny == m || classroom[nx][ny] == 'X') continue;
-                if(classroom[nx][ny] == 'R'){
-                    ne = energy;
-                }else if(classroom[nx][ny] != '.' && classroom[nx][ny] != 'S'){
-                    int num = classroom[nx][ny] - '0';
-                    nm = nm | (1 << num);
-                }
-                if(check[nx][ny][nm] < ne){
-                    check[nx][ny][nm] = ne;
-                    q.push({nx, ny, ne, nm, cs + 1});
-                }
-            }
-        }
-        return -1;
-    }
+			}
+		}
+        if(litters == 0) return 0;
+		int total_mask = (1 << litters) - 1;
+		queue<vector<int>> q; // x, y, mask, energy
+
+        // cout << total_mask << endl;
+		
+		int moves = 1;
+		q.push({x, y, 0, energy});
+		
+		vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(total_mask + 1, -1)));
+		dp[x][y][0] = energy;
+
+		vector<int> dx = {0, 1, 0, -1};
+		vector<int> dy = {1, 0, -1, 0};
+		
+		
+		while(!q.empty()){
+			int size = q.size();
+			while(size--){
+				auto it = q.front();
+                q.pop();
+				int curr_x = it[0];
+				int curr_y = it[1];
+				int curr_mask = it[2];
+				int curr_energy = it[3];
+
+				for(int i = 0; i < 4; i++){
+					int nx = curr_x + dx[i];
+					int ny = curr_y + dy[i];
+					int new_energy = curr_energy - 1;
+					int new_mask = curr_mask;
+
+					if(nx < 0 || nx == n || ny < 0 || ny == m || classroom[nx][ny] == 'X') continue;
+				
+					if(classroom[nx][ny] == 'R'){
+                        new_energy = energy;
+                    }else if(classroom[nx][ny] != '.' && classroom[nx][ny] != 'S'){
+                        int b = (int)(classroom[nx][ny] - '0');
+                        cout << b << endl;
+                        new_mask = new_mask | (1 << b);
+                    }
+                   
+					if(new_mask == total_mask) return moves;
+					
+					if(dp[nx][ny][new_mask] < new_energy && new_energy > 0) {
+						dp[nx][ny][new_mask] = new_energy;
+						q.push({nx, ny, new_mask, new_energy});
+					}
+				}
+			}
+			moves++;
+		}
+		return -1;
+    }	
 };
