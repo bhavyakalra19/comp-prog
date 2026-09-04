5cd5ea14ff88051d7f0b0b775816fa2171cd0255
Bhavya Kalra <54973097+bhavyakalra19@users.noreply.github.com>
2026-09-04 23:10:05 +0530

    Time: 7 ms (39.60%) | Memory: 12.1 MB (39.60%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -4,21 +4,30 @@ public:
         int a = 0;
         int b = 0;
         int c = 0;
-        vector<int> num;
-        num.push_back(1);
-        for(int i = 0; i < n; i++){
-            int check = min(num[a]*2, min(num[b]*3, num[c]*5));
-            if(check == num[a]*2){
-                a += 1;
+        int curr = 1;
+        vector<int> nums;
+        nums.push_back(1);
+        for(int i = 1; i < n; i++){
+            int mn = min(nums[a] * 2, min(nums[b] * 3, nums[c] * 5));
+            if(nums[a] * 2 == mn){
+                a++;
             }
-            if(check == num[b]*3){
-                b += 1;
+            if(nums[b] * 3 == mn){
+                b++;
             }
-            if(check == num[c]*5){
-                c += 1;
+            if(nums[c] * 5 == mn){
+                c++;
             }
-            num.push_back(check);
+            nums.push_back(mn);
         }
-        return num[n-1];
+        return nums[n-1];
     }
 };
+
+// a = 3
+// b = 2
+// c = 1
+
+// 2
+// 3
+// 4
